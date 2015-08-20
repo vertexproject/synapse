a9b3bb722176acf68c9530b4869d68c118fd63ac
invisigoth <invisigoth@kenshoto.com>
2015-08-20 17:02:43 -0400

    un bit rotted server CLI and added way to load cortex with name

--- synapse/tools/server.py
+++ synapse/tools/server.py
@@ -3,17 +3,25 @@ import argparse
 import importlib
 
 import synapse.link as s_link
-import synapse.telepath as s_telepath
+import synapse.cortex as s_cortex
+import synapse.daemon as s_daemon
 
 def main(argv):
 
     p = argparse.ArgumentParser(prog='server')
     p.add_argument('--initmod',help='python module name for daemon init callback')
+    p.add_argument('--cortex', action='append', default=[], help='cortex name,url to share for RMI')
     p.add_argument('linkurl',nargs='+',help='link urls to bind/listen')
 
     opts = p.parse_args(argv)
 
-    daemon = s_telepath.Daemon()
+    daemon = s_daemon.Daemon()
+
+    for nameurl in opts.cortex:
+        name,url = nameurl.split(',',1)
+        core = s_cortex.openurl(url)
+        daemon.addSharedObject(name,core)
+
     for url in opts.linkurl:
         link = s_link.chopLinkUrl(url)
         daemon.runLinkServer(link)
@@ -28,7 +36,11 @@ def main(argv):
         # call back the daemon init module
         meth(daemon)
 
-    daemon.wait()
+    try:
+        daemon.wait()
+    except KeyboardInterrupt as e:
+        print('ctrl-c caught: shutting down')
+        daemon.fini()
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
