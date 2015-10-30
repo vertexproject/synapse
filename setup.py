10eab9bcd14091628def5c9c520d657d09c544b3
invisigoth <invisigoth@kenshoto.com>
2015-10-30 10:07:12 -0400

    udpated setup.py to use synapse.version programatically

--- setup.py
+++ setup.py
@@ -13,10 +13,12 @@ from setuptools import setup,find_packages
 # python3.4 setup.py register
 # python3.4 setup.py bdist_wheel upload
 # python3.4 -m pip install
+import synapse
+import synapse.common as s_common
 
 setup(
     name='viv-synapse',
-    version='0.0.5', # sync with synapse.__version__
+    version=s_common.verstr( synapse.version ),
     description='Synapse Distributed Computing Framework',
     author='Invisigoth Kenshoto',
     author_email='invisigoth.kenshoto@gmail.com',
