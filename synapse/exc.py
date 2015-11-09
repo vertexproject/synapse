c6834da28c0752aca9f4d7179fc3f5960c8783ba
invisigoth <invisigoth@kenshoto.com>
2015-11-09 11:02:45 -0500

    added unified exc module

---
+++ synapse/exc.py
@@ -0,0 +1,14 @@
+
+class BadUrl(Exception):pass
+class BadPropValu(Exception):pass
+class BadPySource(Exception):pass
+
+class NoSuchMod(Exception):pass
+class NoSuchProp(Exception):pass
+class NoSuchProto(Exception):pass
+class NoSuchPath(Exception):pass
+
+class PropNotFound(Exception):pass
+
+class HitTimeMax(Exception):pass
+class HitRetryMax(Exception):pass
