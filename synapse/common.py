6d469714f7d11d07f35db0de26188d39d5678548
invisigoth <invisigoth@kenshoto.com>
2015-11-09 20:40:28 -0500

    unified function for return/raise from ret msgs

--- synapse/common.py
+++ synapse/common.py
@@ -3,6 +3,7 @@ import time
 import msgpack
 import functools
 import threading
+import traceback
 
 from binascii import hexlify
 
@@ -43,6 +44,28 @@ def verstr(vtup):
     Convert a version tuple to a string.
     '''
     return '.'.join([ str(v) for v in vtup ])
+
+retcls = {
+    'NoSuchObj':NoSuchObj,
+    'NoSuchMeth':NoSuchMeth,
+}
+
+def retmesg(mesg):
+    '''
+    Introspect the message for "return" convention
+    '''
+    err = mesg[1].get('err')
+    if err != None:
+        errmsg = mesg[1].get('errmsg')
+
+        cls = retcls.get(err)
+        if cls:
+            raise cls(errmsg)
+
+        errtb = mesg[1].get('errtb')
+        raise CallError(err,errmsg,errtb)
+
+    return mesg[1].get('ret')
 
 def excinfo(e):
     '''
