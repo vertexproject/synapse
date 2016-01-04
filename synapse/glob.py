9fc8c3a7f373fb86963346fd327ce8f60d965966
invisigoth <invisigoth.kenshoto@gmail.com>
2016-01-04 11:27:44 -0500

    change global instance Lock to RLock

--- synapse/glob.py
+++ synapse/glob.py
@@ -1,6 +1,9 @@
 import threading
 
-lock = threading.Lock()
+lock = threading.RLock()
+
+# A global / default PkiStor used by getUserPki()
+pki = None
 
 # A global Plex instance for managing socket i/o.
 plex = None
