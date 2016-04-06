3a0e152081f30ed0c34e5d51ae5bd3ce991ca3d6
Adam Heinz <adam.heinz@fireeye.com>
2016-04-06 09:09:42 -0400

    Don't use pytest.

---
+++ synapse/tests/test_dmon.py
@@ -0,0 +1,17 @@
+
+from synapse.tests.common import SynTest
+from synapse.tools.dmon import getArgParser
+
+
+class TestArgParser(SynTest):
+    def test_getArgParser_logLevel(self):
+        for level in ['debug', 'info', 'warning', 'error', 'critical']:
+            p = getArgParser()
+            args = p.parse_args(['--log-level', level])
+            self.eq(args.log_level, level)
+
+    def test_getArgParser_logLevel_exception(self):
+        for level in ['all', 'notice']:
+            with self.assertRaises(SystemExit):
+                p = getArgParser()
+                p.parse_args(['--log-level', level])
