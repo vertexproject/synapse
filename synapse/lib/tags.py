1ec2016a5d44e4cd27faedafd76af9de868357c9
invisigoth <invisigoth.kenshoto@gmail.com>
2016-04-20 10:09:04 -0400

    added and updated tags lib module to replace aspects top level module

---
+++ synapse/lib/tags.py
@@ -0,0 +1,132 @@
+import time
+import collections
+
+'''
+Tools to help implement a hierarchical tagging system.
+'''
+
+def iterTagDown(tag,div='.'):
+    '''
+    Yield tags from top to bottom.
+
+    Example:
+
+        iterTagDown('foo.bar.baz') -> ('foo','foo.bar','foo.bar.baz')
+
+    '''
+    parts = tag.split(div)
+    for i in range(len(parts)):
+        yield div.join(parts[0:i+1])
+
+def iterTagUp(tag,div='.'):
+    '''
+    Yield tags from top to bottom.
+
+    Example:
+
+        iterTagUp('foo.bar.baz') -> ('foo.bar.baz','foo.bar','foo')
+
+    '''
+    parts = tag.split(div)
+    psize = len(parts)
+    for i in range(psize):
+        yield div.join( parts[:psize-i] )
+
+def getTufoSubs(tufo, tag):
+    '''
+    Return a list of tufo props for the given tag (and down).
+    '''
+    form = tufo[1].get('tufo:form')
+    pref = '*|%s|%s.' % (form,tag)
+    props = [ '*|%s|%s' % (form,tag) ]
+    props.extend([ p for p in tufo[1].keys() if p.startswith(pref) ])
+    return props
+
+def genTufoRows(tufo, tag, valu=None):
+    # gen rows for a cortex
+    now = int(time.time())
+    if valu == None:
+        valu = int(time.time())
+
+    iden = tufo[0]
+    form = tufo[1].get('tufo:form')
+    props = [ '*|%s|%s' % (form,tag) for tag in iterTagDown(tag) ]
+    return [ (iden,prop,valu,now) for prop in props if tufo[1].get(prop) == None ]
+
+def getTufoTags(tufo):
+    '''
+    Return a dict of tag:val for tags on tufo.
+
+    Example:
+
+        tags = getTufoTags(tufo)
+
+    '''
+    return { choptag(p):v for (p,v) in tufo[1].items() if p[0] == '*' }
+
+def choptag(prop):
+    '''
+    Chop a tag property to return tag.
+
+    Example:
+
+        bazfaz = choptag('*|foo:bar|baz.faz')
+
+    '''
+    return prop.split('|',2)[2]
+
+class ByTag:
+    '''
+    A dictionary style put/get API using tags.
+    '''
+    def __init__(self):
+        self.byval = {}
+        self.bytag = collections.defaultdict(set)
+
+    def put(self, item, tags):
+        '''
+        Add an item for ByTag lookup with tags.
+
+        Example:
+
+            btag.put( woot, ('woots.woot0', 'foo') )
+
+        '''
+        self.byval[item] = tags
+        for tag in tags:
+            for name in iterTagDown(tag):
+                self.bytag[name].add(item)
+
+    def get(self, tag):
+        '''
+        Retrieve items by a tag.
+
+        Example:
+
+            for item in btag.get('foo.bar'):
+                dostuff(item)
+
+        '''
+        vals = self.bytag.get(tag)
+        if vals == None:
+            return ()
+
+        return list(vals)
+
+    def pop(self, item, dval=None):
+        '''
+        Remove an item previously added to the ByTag.
+        '''
+        tags = self.byval.pop(item,None)
+        if tags == None:
+            return
+
+        for tag in tags:
+            for name in iterTagDown(tag):
+                s = self.bytag.get(name)
+                if s == None:
+                    continue
+
+                s.discard(item)
+                if not s:
+                    self.bytag.pop(name,None)
---
+++ synapse/tests/test_tags.py
@@ -0,0 +1,61 @@
+import unittest
+
+import synapse.cortex as s_cortex
+import synapse.lib.tags as s_tags
+
+from synapse.tests.common import *
+
+class TagTest(SynTest):
+
+    def test_aspect_iter_up(self):
+        tags = tuple( s_tags.iterTagUp('foo.bar.baz') )
+        self.assertEqual( tags, ('foo.bar.baz','foo.bar','foo') )
+
+    def test_aspect_iter_down(self):
+        tags = tuple( s_tags.iterTagDown('foo.bar.baz') )
+        self.assertEqual( tags, ('foo','foo.bar','foo.bar.baz') )
+
+    def test_aspect_adddel(self):
+        core = s_cortex.openurl('ram:///')
+        tufo = core.formTufoByProp('foo','bar')
+        tufo = core.addTufoTag(tufo, 'baz.faz.gaz', valu=None)
+
+        tags = s_tags.getTufoTags(tufo)
+
+        self.assertIsNotNone( tags.get('baz') )
+        self.assertIsNotNone( tags.get('baz.faz') )
+        self.assertIsNotNone( tags.get('baz.faz.gaz') )
+
+        tufos = core.getTufosByTag('foo','baz.faz')
+        self.assertEqual( len(tufos), 1 )
+
+        tufo = core.delTufoTag(tufo,'baz.faz')
+
+        tufos = core.getTufosByTag('foo','baz.faz')
+        self.assertEqual( len(tufos), 0 )
+
+        tufos = core.getTufosByTag('foo','baz.faz.gaz')
+        self.assertEqual( len(tufos), 0 )
+
+    def test_aspect_bytag(self):
+        bytag = s_tags.ByTag()
+
+        bytag.put('foo0',('foos.foo0','bar.baz'))
+        bytag.put('foo1',('foos.foo1','bar.faz'))
+
+        vals = tuple( sorted( bytag.get('bar') ) )
+        self.assertEqual( vals, ('foo0','foo1') )
+
+        vals = tuple( sorted( bytag.get('foos') ) )
+        self.assertEqual( vals, ('foo0','foo1') )
+
+        vals = tuple( sorted( bytag.get('foos.foo0') ) )
+        self.assertEqual( vals, ('foo0',) )
+
+        vals = tuple( sorted( bytag.get('newp.foo0') ) )
+        self.assertEqual( vals, () )
+
+        bytag.pop('foo0')
+
+        vals = tuple( sorted( bytag.get('foos') ) )
+        self.assertEqual( vals, ('foo1',) )
