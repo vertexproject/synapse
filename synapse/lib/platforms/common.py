a2c1ab292824b5a2112f6b2fe3088ef36f474fa3
invisigoth <invisigoth.kenshoto@gmail.com>
2016-05-02 11:59:47 -0400

    added getVolInfo() api

--- synapse/lib/platforms/common.py
+++ synapse/lib/platforms/common.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import,unicode_literals
 
+import os
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,6 +10,24 @@ def setProcName(name):
     Set the process title/name for process listing.
     '''
     logger.info('setProcName: %s' % (name,))
+
+def getVolInfo(*paths):
+    '''
+    Retrieve volume usage info for the given path.
+    '''
+    path = os.path.join(*paths)
+    path = os.path.expanduser(path)
+
+    st = os.statvfs(path)
+
+    free = st.f_bavail * st.f_frsize
+    total = st.f_blocks * st.f_frsize
+
+    return {
+        'free':free,
+        'used':total - free,
+        'total':total,
+    }
 
 def initHostInfo():
     return {}
