b2c5623bdb77b3854794490814dd66a6da99f261
invisigoth <invisigoth.kenshoto@gmail.com>
2016-06-07 14:15:03 -0400

    initial cut of new modular types subsystem

---
+++ synapse/lib/types.py
@@ -0,0 +1,495 @@
+from __future__ import absolute_import,unicode_literals
+
+import re
+import time
+import socket
+import struct
+import datetime
+
+import synapse.lib.urlhelp as s_urlhelp
+
+from synapse.common import *
+
+class DataType:
+
+    subprops = ()
+
+    def __init__(self, tlib, name, **info):
+        self.tlib = tlib
+        self.name = name
+        self.info = info
+
+    def _raiseBadValu(self, valu, **info):
+        raise BadTypeValu(name=self.name, valu=valu, **info)
+
+    def subs(self):
+        '''
+        Implement if the presence of a property with this type requires sub props.
+        '''
+        return self.subprops
+
+    def chop(self, valu):
+        '''
+        Returns a tuple of (norm,subs) for the given valu.
+        '''
+        return self.norm(valu),{}
+
+    def extend(self, name, **info):
+        '''
+        Construct a new subtype from this instance.
+        '''
+        for k,v in self.info.items():
+            info.setdefault(k,v)
+
+        return self.__class__(self.tlib, name,**info)
+
+class StrType(DataType):
+
+    def __init__(self, tlib, name, **info):
+        DataType.__init__(self, tlib, name, **info)
+
+        self.regex = None
+
+        regex = info.get('regex')
+        if regex != None:
+            self.regex = re.compile(regex)
+
+    def repr(self, valu):
+        return valu
+
+    def norm(self, valu):
+        if self.info.get('lower'):
+            valu = valu.lower()
+
+        enums = self.info.get('enums')
+        if enums != None and valu not in enums:
+            self._raiseBadValu(valu,enums=enums)
+
+        if self.regex != None and not self.regex.match(valu):
+            self._raiseBadValu(valu,regex=self.info.get('regex'))
+
+        return valu
+
+    def parse(self, valu):
+        return self.norm(valu)
+
+class IntType(DataType):
+
+    def __init__(self, tlib, name, **info):
+        DataType.__init__(self, tlib, name, **info)
+
+        self.fmt = info.get('fmt','%d')
+        #self.modval = info.get('mod',None)
+        self.minval = info.get('min',None)
+        self.maxval = info.get('max',None)
+
+    def repr(self, valu):
+        return self.fmt % valu
+
+    def norm(self, valu):
+
+        if self.minval != None and valu < self.minval:
+            self._raiseBadValu(valu,minval=self.minval)
+
+        if self.maxval != None and valu > self.maxval:
+            self._raiseBadValu(valu,maxval=self.maxval)
+
+        return valu
+
+    def parse(self, valu):
+        try:
+            valu = int(valu,0)
+        except Exception as e:
+            raise self._raiseBadValu(valu)
+
+        return self.norm(valu)
+
+def ipv4str(valu):
+    byts = struct.pack('>I',valu)
+    return socket.inet_ntoa(byts)
+
+def ipv4int(valu):
+    byts = socket.inet_aton(valu)
+    return struct.unpack('>I', byts)[0]
+
+class IPv4Type(DataType):
+
+    def norm(self, valu):
+        return valu & 0xffffffff
+
+    def repr(self, valu):
+        return ipv4str(valu)
+
+    def parse(self, text):
+        return ipv4int(text)
+
+def ipv6norm(text):
+    return socket.inet_ntop( socket.AF_INET6, socket.inet_pton( socket.AF_INET6, text ) )
+
+class IPv6Type(DataType):
+
+    def norm(self, valu):
+        try:
+            return ipv6norm(valu)
+        except Exception as e:
+            self._raiseBadValu(valu)
+
+    def parse(self, text):
+        return self.norm(text)
+
+    def repr(self, valu):
+        return valu
+
+#class HostPort(DataType):
+
+class Srv4Type(DataType):
+    '''
+    Base type for <ipv4>:<port> format.
+    '''
+    subprops = (
+        tufo('port', ptype='inet:port'),
+        tufo('ipv4', ptype='inet:ipv4'),
+    )
+
+    def norm(self, valu):
+        return valu & 0xffffffffffff
+
+    def repr(self, valu):
+        addr = valu >> 16
+        port = valu & 0xffff
+        return '%s:%d' % ( ipv4str(addr), port )
+
+    def chop(self, valu):
+        addr = valu >> 16
+        port = valu & 0xffff
+        return valu,{'port':port,'ipv4':addr}
+
+    def parse(self, text):
+
+        try:
+            astr,pstr = text.split(':')
+        except ValueError as e:
+            raise BadTypeValu(name=self.name,valu=text)
+
+        addr = ipv4int(astr)
+        port = int(pstr,0)
+        return ( addr << 16 ) | port
+
+srv6re = re.compile('^\[([a-f0-9:]+)\]:(\d+)$')
+
+class Srv6Type(DataType):
+    '''
+    Base type for [IPv6]:port format.
+    '''
+    subprops = (
+        tufo('port', ptype='inet:port'),
+        tufo('ipv6', ptype='inet:ipv6'),
+    )
+
+    def norm(self, valu):
+        return self.chop(valu)[0]
+
+    def chop(self, valu):
+
+        valu = valu.lower()
+        m = srv6re.match(valu)
+        if m == None:
+            self._raiseBadValu(valu, ex='[af::2]:80')
+
+        host,portstr = m.groups()
+
+        port = int(portstr,0)
+        if port > 0xffff or port < 0:
+            self._raiseBadValu(valu, port=port)
+
+        try:
+            host = ipv6norm(host)
+        except Exception as e:
+            self._raiseBadValu(valu)
+
+        valu = '[%s]:%d' % (host,port)
+        return valu,{'ipv6':host,'port':port}
+
+    def parse(self, text):
+        return self.norm(text)
+
+    def repr(self, valu):
+        return valu
+
+class EmailType(DataType):
+
+    subprops = (
+        tufo('user',ptype='inet:user'),
+        tufo('fqdn',ptype='inet:fqdn'),
+    )
+
+    def norm(self, valu):
+        try:
+            user,fqdn = valu.split('@',1)
+        except ValueError as e:
+            self._raiseBadValu(valu)
+
+        return '%s@%s' % (user.lower(),fqdn.lower())
+
+    def parse(self, text):
+        return self.norm(text)
+
+    def repr(self, valu):
+        return valu
+
+class UrlType(DataType):
+
+    #subprops = (
+        #tufo('fqdn',ptype='inet:fqdn'),
+        #tufo('ipv4',ptype='inet:ipv4'),
+        #tufo('ipv6',ptype='inet:ipv6'),
+        #tufo('port',ptype='inet:port'),
+    #)
+
+    def parse(self, text):
+        return self.norm(text)
+
+    def norm(self, valu):
+        respath = ''
+        resauth = ''
+
+        if valu.find('://') == -1:
+            raise BadTypeValu(name=self.name,valu=valu)
+
+        scheme,resloc = valu.split('://',1)
+
+        parts = resloc.split('/',1)
+        if len(parts) == 2:
+            resloc,respath = parts
+
+        if resloc.find('@') != -1:
+            resauth,resloc = resloc.split('@',1)
+
+        # FIXME chop sub props from resloc!
+        scheme = scheme.lower()
+        hostpart = resloc.lower()
+
+        if resauth:
+            hostpart = '%s@%s' % (resauth,hostpart)
+
+        return '%s://%s/%s' % (scheme,hostpart,respath)
+
+    def repr(self, valu):
+        return valu
+
+class EpochType(DataType):
+
+    def norm(self, valu):
+        return int(valu)
+
+    def parse(self, text):
+
+        text = text.strip().lower()
+        text = (''.join([ c for c in text if c.isdigit() ]))[:14]
+
+        tlen = len(text)
+        if tlen == 4:
+            st = time.strptime(text, '%Y')
+
+        elif tlen == 6:
+            st = time.strptime(text, '%Y%m')
+
+        elif tlen == 8:
+            st = time.strptime(text, '%Y%m%d')
+
+        elif tlen == 10:
+            st = time.strptime(text, '%Y%m%d%H')
+
+        elif tlen == 12:
+            st = time.strptime(text, '%Y%m%d%H%M')
+
+        elif tlen == 14:
+            st = time.strptime(text, '%Y%m%d%H%M%S')
+
+        else:
+            raise Exception('Unknown time format: %s' % text)
+
+        e = datetime.datetime(1970,1,1)
+        d = datetime.datetime(st.tm_year, st.tm_mon, st.tm_mday)
+
+        epoch = int((d - e).total_seconds())
+        epoch += st.tm_hour*3600
+        epoch += st.tm_min*60
+        epoch += st.tm_sec
+
+        return epoch
+
+    def repr(self, valu):
+        dt = datetime.datetime(1970,1,1) + datetime.timedelta(seconds=int(valu))
+        return '%d/%.2d/%.2d %.2d:%.2d:%.2d' % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+
+class TypeLib:
+    '''
+    An extensible type library for use in cortex data models.
+    '''
+    def __init__(self):
+        self.types = {}
+        self.subtypes = []
+
+        self.addType(IntType(self,'int'))
+        self.addType(StrType(self,'str'))
+
+        self.addSubType('syn:tag','str', regex=r'^([\w]+\.)*[\w]+$', lower=1)
+        self.addSubType('syn:prop','str', regex=r'^([\w]+:)*[\w]+$', lower=1)
+        self.addSubType('syn:type','str', regex=r'^([\w]+:)*[\w]+$', lower=1)
+
+        self.addSubType('text', 'str')
+        self.addSubType('bool', 'int', min=0, max=1)
+
+        self.addSubType('str:lwr', 'str', lower=1)
+
+        self.addSubType('geo:latlong', 'str', regex='^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$')
+
+        self.addSubType('guid', 'str', regex='^[0-9a-f]{32}$', lower=1)
+
+        self.addSubType('hash:md5','str', regex='^[0-9a-f]{32}$', lower=1)
+        self.addSubType('hash:sha1','str', regex='^[0-9a-f]{40}$', lower=1)
+        self.addSubType('hash:sha256','str', regex='^[0-9a-f]{64}$', lower=1)
+        self.addSubType('hash:sha384','str', regex='^[0-9a-f]{96}$', lower=1)
+        self.addSubType('hash:sha512','str', regex='^[0-9a-f]{128}$', lower=1)
+
+        # time types
+        self.addType( EpochType(self,'time:epoch') )
+
+        # inet types
+        self.addType(IPv4Type(self,'inet:ipv4'))
+        self.addType(IPv6Type(self,'inet:ipv6'))
+
+        self.addType(Srv4Type(self,'inet:srv4'))
+        self.addType(Srv6Type(self,'inet:srv6'))
+
+        self.addSubType('inet:tcp4','inet:srv4')
+        self.addSubType('inet:udp4','inet:srv4')
+        self.addSubType('inet:tcp6','inet:srv6')
+        self.addSubType('inet:udp6','inet:srv6')
+
+        #self.addSubType('inet:tcp6','inet:srv6')
+        #self.addSubType('inet:udp6','inet:srv6')
+
+        self.addType(UrlType(self,'inet:url'))
+        self.addType(EmailType(self,'inet:email'))
+
+        self.addSubType('inet:asn', 'int')
+        self.addSubType('inet:user','str')
+        self.addSubType('inet:passwd','str')
+        self.addSubType('inet:filepath','str')
+
+        #self.addBaseType('inet:url','str')
+        #self.addBaseType('inet:user','str')
+        #self.addBaseType('inet:passwd','str')
+
+        #self.addBaseType('time:epoch','int',min=0,max=0xffff)
+
+        self.addSubType('inet:fqdn','str', regex='^[a-z0-9.-_]+$', lower=1)
+        #self.addBaseType('inet:port','int', min=0, max=0xffff)
+        self.addSubType('inet:mac', 'str', regex='^([0-9a-f]{2}[:]){5}([0-9a-f]{2})$', lower=1)
+
+        self.addSubType('inet:port', 'int', min=0, max=0xffff)
+
+    def addSubType(self, name, subof, **info):
+        '''
+        Add a new type which extends from parent type's class.
+
+        Example:
+
+            tlib.addSubType('guid:org', 'guid', doc='guid for an org')
+
+        '''
+        if self.types.get(name) != None:
+            raise DupTypeName(name=name)
+
+        info['subof'] = subof
+        base = self.reqDataType(subof)
+        self.addType( base.extend(name, **info) )
+        self.subtypes.append( (name,info) )
+
+    def getDataType(self, name):
+        '''
+        Return the DataType subclass for the given type name.
+        '''
+        return self.types.get(name)
+
+    def reqDataType(self, name):
+        '''
+        Return a reference to the named DataType or raise NoSuchType.
+        '''
+        item = self.getDataType(name)
+        if item == None:
+            raise NoSuchType(name=name)
+        return item
+
+    def addType(self, item):
+        '''
+        Add a type object which extends from DataType.
+
+        class MyType(DataType):
+            def __init__(self):
+                DataType.__init__(self,'my:type')
+
+            #def repr(self, valu):
+            #def norm(self, valu):
+            #def parse(self, text):
+
+        tlib.addType( MyType() )
+        '''
+        self.types[item.name] = item
+
+    #def addBaseType(self, name, base, **info):
+        #'''
+        #Add a new type by specifying options to a base type.
+
+        #Example:
+
+            #tlib.addBaseType('inet:port', 'int', min=0, max=0xffff)
+            #tlib.addBaseType('inet:mac','str', regex='^([0-9a-f]{2}[:]){5}([0-9a-f]{2})$', lower=1)
+
+        #'''
+        #ctor = self.bases.get(base)
+        #if ctor == None:
+            #raise Exception('No Such Base Type: %s' % (base,))
+
+        #self.addType(ctor(name,**info))
+
+    #def addTypeTufo(self, tufo):
+        #name = tufo[1].get('syn:type')
+        #base = tufo[1].get('syn:type:base')
+        #info = tufoprops(tufo, pref='syn:type:info')
+        #return self.addBaseType(name,base,info)
+
+    def getTypeNorm(self, name, valu):
+        '''
+        Normalize a type specific value in system mode.
+
+        Example:
+
+            fqdn = tlib.getTypeNorm('inet:fqdn','Foo.Com')
+
+        '''
+        return self.reqDataType(name).norm(valu)
+
+    def getTypeRepr(self, name, valu):
+        '''
+        Return the humon readable form of the given type value.
+
+        Example:
+
+            print( tlib.getTypeRepr('inet:ipv4', ipv4addr) )
+
+        '''
+        return self.reqDataType(name).repr(valu)
+
+    def getTypeParse(self, name, text):
+        '''
+        Parse input text for the given type into it's system form.
+
+        Example:
+
+            ipv4 = tlib.getTypeParse('inet:ipv4','1.2.3.4')
+
+        '''
+        return self.reqDataType(name).parse(text)
---
+++ synapse/tests/test_types.py
@@ -0,0 +1,169 @@
+import synapse.lib.types as s_types
+
+from synapse.tests.common import *
+
+class DataTypesTest(SynTest):
+
+    def test_datatype_basics(self):
+
+        tlib = s_types.TypeLib()
+        self.assertTrue( isinstance(tlib.getDataType('inet:url'), s_types.DataType) )
+
+        self.assertIsNone( tlib.getDataType('newp') )
+        self.assertRaises( NoSuchType, tlib.reqDataType, 'newp' )
+
+    def test_datatype_inet_url(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises( BadTypeValu, tlib.getTypeNorm, 'inet:url', 'newp' )
+        self.eq( tlib.getTypeNorm('inet:url','http://WoOt.com/HeHe'), 'http://woot.com/HeHe' )
+        self.eq( tlib.getTypeNorm('inet:url','HTTP://WoOt.com/HeHe'), 'http://woot.com/HeHe' )
+        self.eq( tlib.getTypeNorm('inet:url','HttP://Visi:Secret@WoOt.com/HeHe&foo=10'), 'http://Visi:Secret@woot.com/HeHe&foo=10' )
+
+        self.assertRaises( BadTypeValu, tlib.getTypeParse, 'inet:url', 'newp' )
+        self.eq( tlib.getTypeParse('inet:url','http://WoOt.com/HeHe'), 'http://woot.com/HeHe' )
+        self.eq( tlib.getTypeParse('inet:url','HTTP://WoOt.com/HeHe'), 'http://woot.com/HeHe' )
+        self.eq( tlib.getTypeParse('inet:url','HttP://Visi:Secret@WoOt.com/HeHe&foo=10'), 'http://Visi:Secret@woot.com/HeHe&foo=10' )
+
+        self.eq( tlib.getTypeRepr('inet:url','http://woot.com/HeHe'), 'http://woot.com/HeHe' )
+
+    def test_datatype_inet_ipv4(self):
+        tlib = s_types.TypeLib()
+
+        self.eq( tlib.getTypeNorm('inet:ipv4',0x01020304), 0x01020304 )
+
+        self.eq( tlib.getTypeParse('inet:ipv4','1.2.3.4'), 0x01020304 )
+
+        self.eq( tlib.getTypeRepr('inet:ipv4',0x01020304), '1.2.3.4' )
+
+    def test_datatype_inet_tcp4(self):
+        tlib = s_types.TypeLib()
+
+        self.eq( tlib.getTypeNorm('inet:tcp4',0x010203040002), 0x010203040002 )
+
+        self.assertRaises( BadTypeValu, tlib.getTypeParse, 'inet:tcp4', 'newp' )
+        self.eq( tlib.getTypeParse('inet:tcp4','1.2.3.4:2'), 0x010203040002 )
+
+        self.eq( tlib.getTypeRepr('inet:tcp4',0x010203040002), '1.2.3.4:2' )
+
+    def test_datatype_inet_udp4(self):
+        tlib = s_types.TypeLib()
+
+        self.eq( tlib.getTypeNorm('inet:udp4',0x010203040002), 0x010203040002 )
+
+        self.assertRaises( BadTypeValu, tlib.getTypeParse, 'inet:udp4', 'newp' )
+        self.eq( tlib.getTypeParse('inet:udp4','1.2.3.4:2'), 0x010203040002 )
+
+        self.eq( tlib.getTypeRepr('inet:udp4',0x010203040002), '1.2.3.4:2' )
+
+    def test_datatype_inet_port(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'inet:port', '70000' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'inet:port', 0xffffffff )
+
+        self.eq( tlib.getTypeNorm('inet:port', 20), 20 )
+
+    def test_datatype_inet_mac(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'inet:mac', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'inet:mac', 'newp' )
+
+        self.eq( tlib.getTypeNorm('inet:mac', 'FF:FF:FF:FF:FF:FF'), 'ff:ff:ff:ff:ff:ff' )
+        self.eq( tlib.getTypeParse('inet:mac', 'FF:FF:FF:FF:FF:FF'), 'ff:ff:ff:ff:ff:ff' )
+        self.eq( tlib.getTypeRepr('inet:mac', 'ff:ff:ff:ff:ff:ff'), 'ff:ff:ff:ff:ff:ff' )
+
+    def test_datatype_inet_email(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'inet:email', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'inet:email', 'newp' )
+
+        self.eq( tlib.getTypeParse('inet:email', 'ViSi@Woot.Com'), 'visi@woot.com' )
+
+        self.eq( tlib.getTypeNorm('inet:email', 'ViSi@Woot.Com'), 'visi@woot.com' )
+
+        self.eq( tlib.getTypeRepr('inet:email', 'visi@woot.com'), 'visi@woot.com' )
+
+    def test_datatype_guid(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'guid', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'guid', 'newp' )
+
+        self.eq( tlib.getTypeParse('guid', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeNorm('guid', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeRepr('guid', '000102030405060708090a0b0c0d0e0f'), '000102030405060708090a0b0c0d0e0f')
+
+    def test_datatype_guid_sub(self):
+        tlib = s_types.TypeLib()
+
+        tlib.addSubType('woot','guid')
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'woot', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'woot', 'newp' )
+
+        self.eq( tlib.getTypeParse('woot', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeNorm('woot', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeRepr('guid', '000102030405060708090a0b0c0d0e0f'), '000102030405060708090a0b0c0d0e0f')
+
+    def test_datatype_hash_md5(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'hash:md5', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'hash:md5', 'newp' )
+
+        self.eq( tlib.getTypeParse('hash:md5', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeNorm('hash:md5', '000102030405060708090A0B0C0D0E0F'), '000102030405060708090a0b0c0d0e0f')
+        self.eq( tlib.getTypeRepr('hash:md5', '000102030405060708090a0b0c0d0e0f'), '000102030405060708090a0b0c0d0e0f')
+
+    def test_datatype_inet_ipv6(self):
+        tlib = s_types.TypeLib()
+
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'inet:ipv6', 'newp' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'inet:srv6', 'newp' )
+
+        self.eq( tlib.getTypeParse('inet:ipv6', 'AF:00::02'), 'af::2')
+        self.eq( tlib.getTypeNorm('inet:ipv6', 'AF:00::02'), 'af::2')
+        self.eq( tlib.getTypeRepr('inet:ipv6', 'af::2'), 'af::2')
+
+        self.eq( tlib.getTypeRepr('inet:srv6', '[af::2]:80'), '[af::2]:80')
+        self.eq( tlib.getTypeParse('inet:srv6', '[AF:00::02]:80'), '[af::2]:80')
+        self.eq( tlib.getTypeNorm('inet:srv6', '[AF:00::02]:80'), '[af::2]:80')
+
+    def test_datatype_str_enums(self):
+        tlib = s_types.TypeLib()
+
+        tlib.addSubType('woot','str',enums=('hehe','haha','hoho'), lower=True)
+
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'woot', 'asdf' )
+        self.assertRaises(BadTypeValu, tlib.getTypeParse, 'woot', 'asdf' )
+
+        self.eq( tlib.getTypeNorm('woot','HeHe'), 'hehe' )
+        self.eq( tlib.getTypeParse('woot','HeHe'), 'hehe' )
+
+    def test_datatype_dup(self):
+        tlib = s_types.TypeLib()
+        self.assertRaises(DupTypeName, tlib.addSubType, 'inet:port', 'int' )
+
+    def test_datatype_syn_tag(self):
+
+        tlib = s_types.TypeLib()
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'syn:tag', 'asdf qwer' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'syn:tag', 'foo..bar' )
+
+        self.eq( tlib.getTypeNorm('syn:tag','BAR'), 'bar' )
+        self.eq( tlib.getTypeParse('syn:tag','BAR'), 'bar' )
+        self.eq( tlib.getTypeNorm('syn:tag','foo.BAR'), 'foo.bar' )
+        self.eq( tlib.getTypeParse('syn:tag','foo.BAR'), 'foo.bar' )
+
+    def test_datatype_syn_prop(self):
+        tlib = s_types.TypeLib()
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'syn:prop', 'asdf qwer' )
+        self.assertRaises(BadTypeValu, tlib.getTypeNorm, 'syn:prop', 'foo::bar' )
+
+        self.eq( tlib.getTypeNorm('syn:prop','BAR'), 'bar' )
+        self.eq( tlib.getTypeParse('syn:prop','BAR'), 'bar' )
+        self.eq( tlib.getTypeNorm('syn:prop','foo:BAR'), 'foo:bar' )
+        self.eq( tlib.getTypeParse('syn:prop','foo:BAR'), 'foo:bar' )
