cb56b0b7fef64edbbec78f301bbfa7ee3c633b87
invisig0th <invisigoth.kenshoto@gmail.com>
2016-09-06 14:29:39 -0400

    added SynErr class for BadMesgVers to raise for vers compat

--- synapse/exc.py
+++ synapse/exc.py
@@ -17,10 +17,12 @@ class NoSuchProp(SynErr):pass
 class NoSuchOper(SynErr):pass
 class NoSuchRule(SynErr):pass
 
+
 class BadTypeValu(SynErr):pass
 class DupTypeName(SynErr):pass
 class DupPropName(SynErr):pass
 class BadPropName(SynErr):pass
+class BadMesgVers(SynErr):pass
 
 class BadUrl(Exception):pass
 class BadJson(Exception):pass
