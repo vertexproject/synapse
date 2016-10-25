021a1292e6d223e1cdf9abca704462f637062d7c
whippit <whippit@foo>
2016-10-25 15:41:15 -0400

    initial sqlite and ram dockerize

--- synapse/tests/test_docker.py
+++ synapse/tests/test_docker.py
@@ -4,21 +4,60 @@ from synapse.tests.common import *
 
 class DockerTest(SynTest):
 
-    def test_pg_core(self):
-        dcker = os.getenv('SYN_DOCKER')
+    def test_ram_core(self):
+        dcker = os.getenv('SYN_DOCKER_RAM')
         if dcker == None:
-            raise unittest.SkipTest('no SYN_DOCKER')
+            raise unittest.SkipTest('no SYN_DOCKER_RAM')
 
-        prox = s_telepath.openurl('tcp://127.0.0.1/core', port=47322)
-        
+        prox = s_telepath.openurl('tcp://127.0.0.1/core', port=47320)
+
         job00 = prox.call('formTufoByProp', 'inet:fqdn', 'foo.com')
         job01 = prox.call('formTufoByProp', 'inet:fqdn', 'bar.com')
         tufo00 = prox.syncjob(job00)
         tufo01 = prox.syncjob(job01)
-        
+
         job02 = prox.call('getTufoByProp', 'inet:fqdn', 'foo.com')
         tufo02 = prox.syncjob(job02)
-        
+
         self.assertEqual(tufo00[0], tufo02[0])
 
         prox.fini()
+
+    def test_sqlite_core(self):
+        dcker = os.getenv('SYN_DOCKER_SQLITE')
+        if dcker == None:
+            raise unittest.SkipTest('no SYN_DOCKER_SQLITE')
+
+        prox = s_telepath.openurl('tcp://127.0.0.1/core', port=47321)
+
+        job00 = prox.call('formTufoByProp', 'inet:fqdn', 'foo.com')
+        job01 = prox.call('formTufoByProp', 'inet:fqdn', 'bar.com')
+        tufo00 = prox.syncjob(job00)
+        tufo01 = prox.syncjob(job01)
+
+        job02 = prox.call('getTufoByProp', 'inet:fqdn', 'foo.com')
+        tufo02 = prox.syncjob(job02)
+
+        self.assertEqual(tufo00[0], tufo02[0])
+
+        prox.fini()
+
+    def test_pg_core(self):
+        dcker = os.getenv('SYN_DOCKER_PG')
+        if dcker == None:
+            raise unittest.SkipTest('no SYN_DOCKER_PG')
+
+        prox = s_telepath.openurl('tcp://127.0.0.1/core', port=47322)
+
+        job00 = prox.call('formTufoByProp', 'inet:fqdn', 'foo.com')
+        job01 = prox.call('formTufoByProp', 'inet:fqdn', 'bar.com')
+        tufo00 = prox.syncjob(job00)
+        tufo01 = prox.syncjob(job01)
+
+        job02 = prox.call('getTufoByProp', 'inet:fqdn', 'foo.com')
+        tufo02 = prox.syncjob(job02)
+
+        self.assertEqual(tufo00[0], tufo02[0])
+
+        prox.fini()
+
