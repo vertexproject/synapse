313b149cedf0c8852430fb67ab7c57e5ee73406f
invisig0th <invisigoth.kenshoto@gmail.com>
2016-11-29 15:40:29 -0500

    initial module for loading and calling synapse modules

---
+++ synapse/lib/modules.py
@@ -0,0 +1,59 @@
+'''
+Module which implements the synapse module API/convention.
+'''
+import logging
+
+import synapse.dyndeps as s_dyndeps
+
+logger = logging.getLogger(__name__)
+
+synmods = {}
+def call(name, *args, **kwargs):
+    '''
+    Call the given function on all loaded synapse modules.
+
+    Returns a list of name,ret,exc tuples where each module
+    which implements the given function returns either ret on
+    successful execution or exc in the event of an exception.
+
+    Example:
+
+        import synapse.lib.modules as s_modules
+        for name,ret,exc in s_modules.call('getFooByBar',bar):
+            dostuff()
+
+    '''
+    ret = []
+    for sname,smod in synmods.items():
+        func = getattr(smod,name,None)
+        if func == None:
+            continue
+
+        try:
+            val = func(*args,**kwargs)
+            ret.append( (sname,val,None) )
+
+        except Exception as e:
+            ret.append( (sname,None,exc) )
+
+    #print('call: %r %r %r %r' % (name,args,kwargs,ret))
+    return ret
+
+def load(name):
+    '''
+    Load the given python module path as a synapse module.
+
+    Example:
+
+        import synapse.lib.modules as s_modules
+        s_modules.load('foopkg.barmod')
+
+    '''
+    smod = synmods.get(name)
+    if smod == None:
+        #print('loading: %s' % (name,))
+        logging.info('loading syn mod: %s' % (name,))
+        smod = s_dyndeps.tryDynMod(name)
+        synmods[name] = smod
+    return smod
+
