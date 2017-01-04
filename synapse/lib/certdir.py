2e2090ec1f5e25e242af539830263d0abb21fd04
invisig0th <invisigoth.kenshoto@gmail.com>
2017-01-04 10:23:12 -0500

    modularized certdir handling and added convention for ssl link to resolve certs

---
+++ synapse/lib/certdir.py
@@ -0,0 +1,276 @@
+import os
+
+from OpenSSL import crypto
+
+import synapse.lib.tags as s_tags
+
+from synapse.common import *
+
+defdir = os.getenv('SYN_CERT_DIR')
+if defdir == None:
+    defdir = '~/.syn/certs'
+
+def iterFqdnUp(fqdn):
+    levs = fqdn.split('.')
+    for i in range(len(levs)):
+        yield '.'.join( levs[i:] )
+
+class CertDir:
+
+    def __init__(self, path=None):
+
+        if path == None:
+            path = defdir
+
+        self.certdir = reqdir(path)
+
+    def getPathJoin(self, *paths):
+        return genpath(self.certdir,*paths)
+
+    def getCaCert(self, name):
+        byts = reqbytes( self.getCaCertPath(name) )
+        return crypto.load_certificate(crypto.FILETYPE_PEM, byts)
+
+    def getHostCert(self, name):
+        byts = reqbytes( self.getHostCertPath(name) )
+        return crypto.load_certificate(crypto.FILETYPE_PEM, byts)
+
+    def getUserCert(self, name):
+        byts = reqbytes( self.getUserCertPath(name) )
+        return crypto.load_certificate(crypto.FILETYPE_PEM, byts)
+
+    def getCaKey(self, name):
+        byts = reqbytes( self.getCaKeyPath(name) )
+        return crypto.load_privatekey(crypto.FILETYPE_PEM, byts)
+
+    def getHostKey(self, name):
+        byts = reqbytes( self.getHostKeyPath(name) )
+        return crypto.load_privatekey(crypto.FILETYPE_PEM, byts)
+
+    def getUserKey(self, name):
+        byts = reqbytes( self.getUserKeyPath(name) )
+        return crypto.load_privatekey(crypto.FILETYPE_PEM, byts)
+
+    #def getHostCert(self, name):
+        #byts = reqbytes( self.getHostCertPath(name) )
+        #return crypto.load_certificate(crypto.FILETYPE_PEM, byts)
+
+    #def saveCaCert(self, cert):
+    #def saveUserCert(self, cert):
+    #def saveHostCert(self, cert):
+    #def saveX509Cert(self, cert):
+    #def loadX509Cert(self, path):
+
+    def _genBasePkeyCert(self, name, pkey=None):
+
+        if pkey == None:
+            pkey = crypto.PKey()
+            pkey.generate_key(crypto.TYPE_RSA, 2048)
+
+        cert = crypto.X509()
+        cert.set_pubkey(pkey)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(10*365*24*60*60)
+        cert.set_serial_number( int(time.time()) )
+
+        cert.get_subject().CN = name
+
+        return pkey,cert
+
+    def _saveCertTo(self, cert, *paths):
+        path = self.getPathJoin(*paths)
+        if os.path.isfile(path):
+            raise DupFileName(path=path)
+
+        with genfile(path) as fd:
+            fd.write( crypto.dump_certificate(crypto.FILETYPE_PEM, cert) )
+        return path
+
+    def _savePkeyTo(self, pkey, *paths):
+        path = self.getPathJoin(*paths)
+        if os.path.isfile(path):
+            raise DupFileName(path=path)
+
+        with genfile(path) as fd:
+            fd.write( crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey) )
+        return path
+
+    def genCaCert(self, name, signas=None, outp=None):
+        pkey,cert = self._genBasePkeyCert(name)
+        ext0 = crypto.X509Extension(b'basicConstraints',False,b'CA:TRUE')
+        cert.add_extensions([ext0])
+
+        if signas != None:
+            self.signCertAs(cert,signas)
+        else:
+            self.selfSignCert(cert,pkey)
+
+        keypath = self._savePkeyTo(pkey, 'cas','%s.key' % name)
+        if outp != None:
+            outp.printf('key saved: %s' % (keypath,))
+
+        crtpath = self._saveCertTo(cert, 'cas','%s.crt' % name)
+        if outp != None:
+            outp.printf('cert saved: %s' % (crtpath,))
+
+        return pkey,cert
+
+    def genHostCert(self, name, signas=None, outp=None, pkey=None):
+        pkey,cert = self._genBasePkeyCert(name,pkey=pkey)
+
+        certtype = b'server'
+        extuse = [b'serverAuth']
+        keyuse = [b'digitalSignature',b'keyEncipherment']
+
+        ext0 = crypto.X509Extension(b'nsCertType',False,certtype)
+        ext1 = crypto.X509Extension(b'keyUsage',False,b','.join(keyuse))
+
+        extuse = b','.join(extuse)
+        ext2 = crypto.X509Extension(b'extendedKeyUsage',False,extuse)
+        ext3 = crypto.X509Extension(b'basicConstraints',False,b'CA:FALSE')
+
+        cert.add_extensions([ext0,ext1,ext2,ext3])
+
+        if signas != None:
+            self.signCertAs(cert,signas)
+        else:
+            self.selfSignCert(cert,pkey)
+
+        if not pkey._only_public:
+            keypath = self._savePkeyTo(pkey, 'hosts','%s.key' % name)
+            if outp != None:
+                outp.printf('key saved: %s' % (keypath,))
+
+        crtpath = self._saveCertTo(cert, 'hosts','%s.crt' % name)
+        if outp != None:
+            outp.printf('cert saved: %s' % (crtpath,))
+
+        return pkey,cert
+
+    def genUserCert(self, name, signas=None, outp=None, pkey=None):
+
+        pkey,cert = self._genBasePkeyCert(name, pkey=pkey)
+
+        keyuse = [b'digitalSignature']
+        extuse = [b'clientAuth']
+        certtype = b'client'
+
+        ext0 = crypto.X509Extension(b'nsCertType',False,certtype)
+        ext1 = crypto.X509Extension(b'keyUsage',False,b','.join(keyuse))
+
+        extuse = b','.join(extuse)
+        ext2 = crypto.X509Extension(b'extendedKeyUsage',False,extuse)
+        ext3 = crypto.X509Extension(b'basicConstraints',False,b'CA:FALSE')
+
+        cert.add_extensions([ext0,ext1,ext2,ext3])
+
+        if signas != None:
+            self.signCertAs(cert,signas)
+        else:
+            self.selfSignCert(cert,pkey)
+
+        if not pkey._only_public:
+            keypath = self._savePkeyTo(pkey, 'users','%s.key' % name)
+            if outp != None:
+                outp.printf('key saved: %s' % (keypath,))
+
+        crtpath = self._saveCertTo(cert, 'users','%s.crt' % name)
+        if outp != None:
+            outp.printf('cert saved: %s' % (crtpath,))
+
+        return pkey,cert
+
+    def genUserCsr(self, name, outp=None):
+        return self._genPkeyCsr(name,'users',outp=outp)
+
+    def genHostCsr(self, name, outp=None):
+        return self._genPkeyCsr(name,'hosts',outp=outp)
+
+    def signUserCsr(self, xcsr, signas, outp=None):
+        pkey = xcsr.get_pubkey()
+        name = xcsr.get_subject().CN
+        return self.genUserCert(name, pkey=pkey, signas=signas, outp=outp)
+
+    def signHostCsr(self, xcsr, signas, outp=None):
+        return self.genHostCert(name, pkey=pkey, signas=signas, outp=outp)
+
+    def _genPkeyCsr(self, name, mode, outp=None):
+        pkey = crypto.PKey()
+        pkey.generate_key(crypto.TYPE_RSA, 2048)
+
+        xcsr = crypto.X509Req()
+        xcsr.get_subject().CN = name
+
+        xcsr.set_pubkey(pkey)
+        xcsr.sign(pkey,'sha256')
+
+        keypath = self._savePkeyTo(pkey,mode,'%s.key' % name)
+        if outp != None:
+            outp.printf('key saved: %s' % (keypath,))
+
+        csrpath = self.getPathJoin(mode,'%s.csr' % name)
+        if os.path.isfile(csrpath):
+            raise DupFileName(path=csrpath)
+
+        with genfile(csrpath) as fd:
+            fd.write( crypto.dump_certificate_request(crypto.FILETYPE_PEM, xcsr) )
+
+        if outp != None:
+            outp.printf('csr saved: %s' %( csrpath,))
+
+    def signCertAs(self, cert, signas):
+        cakey = self.getCaKey(signas)
+        cacert = self.getCaCert(signas)
+
+        cert.set_issuer( cacert.get_subject() )
+        cert.sign( cakey, 'sha256' )
+
+    def selfSignCert(self, cert, pkey):
+        cert.set_issuer( cert.get_subject() )
+        cert.sign( pkey, 'sha256' )
+
+    def getUserForHost(self, user, host):
+        for name in iterFqdnUp(host):
+            usercert = '%s@%s' % (user,name)
+            if self.isUserCert(usercert):
+                return usercert
+
+    def getCaCertPath(self, name):
+        return reqpath(self.certdir,'cas','%s.crt' % name)
+
+    def getCaKeyPath(self, name):
+        return reqpath(self.certdir,'cas','%s.key' % name)
+
+    def getHostCertPath(self, name):
+        return reqpath(self.certdir,'hosts','%s.crt' % name)
+
+    def getHostKeyPath(self, name):
+        return reqpath(self.certdir,'hosts','%s.key' % name)
+
+    def getUserCertPath(self, name):
+        return reqpath(self.certdir,'users','%s.crt' % name)
+
+    def getUserKeyPath(self, name):
+        return reqpath(self.certdir,'users','%s.key' % name)
+
+    def getUserCaPath(self, name):
+        cert = self.getUserCert(name)
+        subj = cert.get_issuer()
+
+        capath = self.getPathJoin('cas','%s.crt' % subj.CN)
+        if not os.path.isfile(capath):
+            return None
+
+        return capath
+
+    def isUserCert(self, name):
+        crtpath = self.getPathJoin('users','%s.crt' % name)
+        return os.path.isfile(crtpath)
+
+    def isCaCert(self, name):
+        crtpath = self.getPathJoin('cas','%s.crt' % name)
+        return os.path.isfile(crtpath)
+
+    def isHostCert(self, name):
+        crtpath = self.getPathJoin('hosts','%s.crt' % name)
+        return os.path.isfile(crtpath)
--- synapse/tools/easycert.py
+++ synapse/tools/easycert.py
@@ -6,8 +6,9 @@ import argparse
 from OpenSSL import crypto
 
 import synapse.lib.output as s_output
+import synapse.lib.certdir as s_certdir
 
-from synapse.common import gendir,genfile,reqbytes
+from synapse.common import *
 
 descr = '''
 Command line tool to generate simple x509 certs
@@ -67,115 +68,59 @@ def main(argv, outp=None):
     pars.add_argument('--signas', help='sign the new cert with the given cert name')
     pars.add_argument('--ca', default=False, action='store_true', help='mark the certificate as a CA/CRL signer')
     pars.add_argument('--server', default=False, action='store_true', help='mark the certificate as a server')
-    pars.add_argument('--gen-csr', default=False, action='store_true', help='generate a cert signing request')
+    pars.add_argument('--csr', default=False, action='store_true', help='generate a cert signing request')
     pars.add_argument('--sign-csr', default=False, action='store_true', help='sign a cert signing request')
     pars.add_argument('name', help='common name for the certificate (or filename for CSR signing)')
 
     opts = pars.parse_args(argv)
 
-    cname = opts.name
+    cdir = s_certdir.CertDir(path=opts.certdir)
 
-    pkey = None
-    savepkey = True
+    try:
 
-    if opts.gen_csr:
-        return gencsr(opts,outp)
+        if opts.sign_csr:
 
-    if opts.sign_csr:
+            if opts.signas == None:
+                outp.printf('--sign-csr requires --signas')
+                return -1
 
-        if opts.signas == None:
-            outp.printf('--sign-csr requires --signas')
-            return -1
+            byts = reqbytes(opts.name)
+            xcsr = crypto.load_certificate_request(crypto.FILETYPE_PEM,byts)
 
-        savepkey = False
+            if opts.server:
+                cdir.signHostCsr(xcsr, opts.signas, outp=outp)
+                return 0
 
-        byts = reqbytes(opts.name)
-        xcsr = crypto.load_certificate_request(crypto.FILETYPE_PEM,byts)
+            cdir.signUserCsr(xcsr, opts.signas, outp=outp)
+            return 0
 
-        cname = xcsr.get_subject().CN
-        pkey = xcsr.get_pubkey()
+        if opts.csr:
 
+            if opts.ca:
+                cdir.genCaCsr(opts.name, outp=outp)
+                raise Exception('CSR for CA cert not supporte (yet)')
 
-    if pkey == None:
-        pkey = crypto.PKey()
-        pkey.generate_key(crypto.TYPE_RSA, 2048)
+            if opts.server:
+                cdir.genHostCsr(opts.name, outp=outp)
+                return 0
 
-    certdir = gendir(opts.certdir)
+            cdir.genUserCsr(opts.name, outp=outp)
+            return 0
 
-    pkeypath = os.path.join(certdir,'%s.key' % cname)
-    certpath = os.path.join(certdir,'%s.crt' % cname)
-
-    if savepkey and os.path.exists(pkeypath):
-        outp.printf('key exists: %s' % (pkeypath,))
-        return(-1)
-
-    if os.path.exists(certpath):
-        outp.printf('cert exists: %s' % (certpath,))
-        return(-1)
-
-    cert = crypto.X509()
-    cert.set_pubkey(pkey)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(10*365*24*60*60)
-    cert.set_serial_number( int(time.time()) )
-
-    if opts.ca:
-
-        ext0 = crypto.X509Extension(b'basicConstraints',False,b'CA:TRUE')
-        cert.add_extensions([ext0])
-
-    else:
-
-        keyuse = [b'digitalSignature']
-        extuse = [b'clientAuth']
-        certtype = b'client'
-
+        if opts.ca:
+            cdir.genCaCert(opts.name, signas=opts.signas, outp=outp)
+            return 0
 
         if opts.server:
-            certtype = b'server'
-            extuse = [b'serverAuth']
-            keyuse.append(b'keyEncipherment')
+            cdir.genHostCert(opts.name, signas=opts.signas, outp=outp)
+            return 0
 
-        ext0 = crypto.X509Extension(b'nsCertType',False,certtype)
-        ext1 = crypto.X509Extension(b'keyUsage',False,b','.join(keyuse))
+        cdir.genUserCert(opts.name, signas=opts.signas, outp=outp)
+        return 0
 
-        extuse = b','.join(extuse)
-        ext2 = crypto.X509Extension(b'extendedKeyUsage',False,extuse)
-        ext3 = crypto.X509Extension(b'basicConstraints',False,b'CA:FALSE')
-
-        cert.add_extensions([ext0,ext1,ext2,ext3])
-
-    subj = cert.get_subject()
-    subj.CN = cname
-
-    signcert = cert
-    signpkey = pkey
-
-    if opts.signas:
-        byts = reqbytes(certdir,'%s.key' % (opts.signas,))
-        signpkey = crypto.load_privatekey(crypto.FILETYPE_PEM, byts)
-
-        byts = reqbytes(certdir,'%s.crt' % (opts.signas,))
-        signcert = crypto.load_certificate(crypto.FILETYPE_PEM, byts)
-
-    cert.set_issuer( signcert.get_subject() )
-    cert.sign( signpkey, 'sha256' )
-
-    if savepkey:
-
-        byts = crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey)
-
-        with genfile(pkeypath) as fd:
-            fd.write(byts)
-
-        outp.printf('pkey saved: %s' % (pkeypath,))
-
-    byts = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
-    with genfile(certpath) as fd:
-        fd.write(byts)
-
-    outp.printf('cert saved: %s' % (certpath,))
-    return 0
+    except DupFileName as e:
+        outp.printf('file exists: %s' % (e.errinfo.get('path'),))
+        return -1
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
