59f8e1c8c328ed0e3d0703798a90ccb0fd2d5acb
invisig0th <invisigoth.kenshoto@gmail.com>
2017-01-12 17:42:26 -0500

    added guid generation via hash set

--- synapse/axon.py
+++ synapse/axon.py
@@ -28,7 +28,12 @@ gigabyte = 1024000000
 chunksize = megabyte * 10
 
 class HashSet:
+
     def __init__(self):
+
+        self.size = 0
+
+        # BEWARE ORDER MATTERS FOR guid()
         self.hashes = [
             ('md5',hashlib.md5()),
             ('sha1',hashlib.sha1()),
@@ -36,10 +41,42 @@ class HashSet:
             ('sha512',hashlib.sha512())
         ]
 
+    def guid(self):
+        '''
+        Use elements from this hash set to create a unique
+        (re)identifier.
+        '''
+        iden = hashlib.md5()
+        props = {'size':self.size}
+
+        for name,item in self.hashes:
+            iden.update(item.digest())
+            props[name] = item.hexdigest()
+
+        return iden.hexdigest(),props
+
+    def consume(self, fd):
+        '''
+        Consume all the bytes from a file like object.
+
+        Example:
+
+            hset = HashSet()
+            hset.consume(fd)
+
+        '''
+        byts = fd.read(10000000)
+        while byts:
+            self.update(byts)
+            byts = fd.read(10000000)
+
+        return self.guid()
+
     def update(self, byts):
         '''
         Update all the hashes in the set with the given bytes.
         '''
+        self.size += len(byts)
         [ h[1].update(byts) for h in self.hashes ]
 
     def digests(self):
