279def929e9175ad06c6d787d584b8ea3af51924
invisig0th <invisigoth.kenshoto@gmail.com>
2017-01-14 10:50:55 -0500

    tweaks for scrape tool to upload to axons

--- synapse/tools/scrape.py
+++ synapse/tools/scrape.py
@@ -21,6 +21,7 @@ def main(argv, outp=None):
     pars = argparse.ArgumentParser(prog='scrape', description='Command line tool for scraping data into a cortex')
 
     pars.add_argument('--sync', default=None, help='Sync to an additional cortex')
+    pars.add_argument('--axon', default=None, help='Axon URL to save the file bytes')
     pars.add_argument('--save', default=None, help='Save changes to a .sync file for later loading')
     pars.add_argument('--debug', default=False, action='store_true', help='Drop to interactive prompt to inspect cortex')
     pars.add_argument('--verbose',default=False, action='store_true', help='Show the nodes being scraped')
@@ -36,7 +37,14 @@ def main(argv, outp=None):
     core = s_cortex.openurl('ram://')
     core.setConfOpt('enforce',1)
 
+    axon = None
+    if opts.axon != None:
+        outp.printf('connecting to axon: %s' % (opts.axon,))
+        axon = s_axon.openurl(opts.axon)
+        outp.printf('...success! %r' % (axon,))
+
     if opts.save:
+        outp.printf('adding save file: %s' % (opts.save,))
         fd = genfile(opts.save)
         core.addSyncFd(fd)
 
@@ -47,12 +55,16 @@ def main(argv, outp=None):
 
     for path in opts.files:
 
-        hset = s_axon.HashSet()
         with reqfile(path) as fd:
-            iden,props = hset.consume(fd)
+
+            if axon != None:
+                outp.printf('Sending to Axon %s -> %s' % (path,opts.axon))
+                axon.eatfd(fd)
+
+            hset = s_axon.HashSet()
+            iden,props = hset.eatfd(fd)
 
         props['name'] = os.path.basename(path)
-
         node = core.formTufoByProp('file:bytes',iden,**props)
 
         core.addTufoTags(node,tags)
