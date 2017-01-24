897b6eb5bed0a45d5a162e7c34d5671e52f06f7a
whippit <whippit@vertex.link>
2017-01-24 16:08:18 -0500

    update the docker container names to match

--- synapse/tests/travis/script.py
+++ synapse/tests/travis/script.py
@@ -24,9 +24,9 @@ def main(argv):
             #'docker exec core_ram /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
             #'docker exec core_sqlite /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
             #'docker exec core_pg /bin/bash -c "SYN_TEST_PG_DB=syn_test SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
-            #'docker exec core_27 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
-            #'docker exec core_35 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
-            'docker exec core_36 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
+            #'docker exec synapse_27 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
+            #'docker exec synapse_35 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
+            'docker exec synapse_36 /bin/bash -c "SYN_DOCKER=1 nosetests --verbosity=3 --with-coverage --cover-erase --cover-package=synapse"',
         ]
     for cmd in cmds:
         print('run: %r' % (cmd,))
