6092d66157a3ebeec65ca957aef4f49dc2a6b455
invisig0th <invisigoth.kenshoto@gmail.com>
2017-02-27 23:20:24 -0500

    proper left recursion and operator precedence for gene syntax

--- synapse/tests/test_gene.py
+++ synapse/tests/test_gene.py
@@ -31,8 +31,42 @@ class GeneTest(SynTest):
         self.eq( s_gene.eval('foo:baz(10) > 1', syms=syms), 1 )
         self.eq( s_gene.eval('foo:baz(10) >= 1', syms=syms), 1 )
 
-        # FIXME operator precedence
-        #self.eq( s_gene.eval('foo:bar + 0x0a == 20', syms=syms), 1 )
+        self.eq( s_gene.eval('foo:bar + 0x0a == 20', syms=syms), 1 )
+
+    def test_gene_prec(self):
+        self.eq( s_gene.eval('5 + 3 * 2'), 11 )
+        self.eq( s_gene.eval('(5 + 3) * 2'), 16 )
+        self.eq( s_gene.eval('9 / 3 <= 1 + 2'), 1)
+
+    def test_gene_comp(self):
+        self.eq( s_gene.eval('5 < 4 + 4'), 1 )
+        self.eq( s_gene.eval('5 <= 4 + 4'), 1 )
+        self.eq( s_gene.eval('5 > 8 - 4 - 2'), 1 )
+        self.eq( s_gene.eval('5 >= 8 - 4 - 2'), 1 )
+
+    def test_gene_call(self):
+
+        def foo(x,y,z):
+            return x + y + z
+
+        syms = { 'foo':foo }
+
+        self.eq( s_gene.eval('foo(1,2,3)',syms=syms), 6 )
+        self.eq( s_gene.eval('foo(foo(1,2,3),9+9,3>2)',syms=syms), 25 )
+
+    def test_gene_pow(self):
+        self.eq( s_gene.eval(' 5 + 2**3 '), 13 )
+
+    def test_gene_landlor(self):
+        self.eq( s_gene.eval(' 10 > 3 && 10 + 10 > 19 '), 1 )
+        self.eq( s_gene.eval(' 10 < 3 || 10 + 10 > 19 '), 1 )
+
+        self.eq( s_gene.eval(' 10 < 3 && 10 + 10 > 19 '), 0 )
+        self.eq( s_gene.eval(' 10 < 3 || 10 + 10 < 19 '), 0 )
+
+    def test_gene_shift(self):
+        self.eq( s_gene.eval(' 1 << 2 + 1'), 8 )
+        self.eq( s_gene.eval(' 0x80 >> 0x00000004'), 8 )
 
     def test_gene_lab(self):
         glab = s_gene.GeneLab()
