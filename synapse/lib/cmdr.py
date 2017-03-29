4a79cc2cb9ee99f5f96b2348926174b8f967a65a
invisig0th <invisigoth.kenshoto@gmail.com>
2017-03-29 11:51:57 -0400

    added doc strings.  :D

--- synapse/lib/cmdr.py
+++ synapse/lib/cmdr.py
@@ -11,7 +11,14 @@ s_mixins.addSynMixin('cmdr','synapse.cores.common.Cortex','synapse.cmds.cortex.A
 
 
 def getItemCmdr(item, outp=None, **opts):
+    '''
+    Construct and return a cmdr for the given item.
 
+    Example:
+
+        cmdr = getItemCmdr(foo)
+
+    '''
     cmdr = s_cli.Cli(item,outp=outp)
 
     refl = s_reflect.getItemInfo(item)
@@ -25,5 +32,13 @@ def getItemCmdr(item, outp=None, **opts):
     return cmdr
 
 def runItemCmdr(item, outp=None, **opts):
+    '''
+    Create a cmdr for the given item and run the cmd loop.
+
+    Example:
+
+        runItemCmdr(foo)
+
+    '''
     cmdr = getItemCmdr(item, outp=outp, **opts)
     cmdr.runCmdLoop()
