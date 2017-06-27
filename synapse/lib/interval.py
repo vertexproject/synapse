d3799746612324bed681e75a9658055043e73239
invisig0th <invisigoth.kenshoto@gmail.com>
2017-06-27 13:33:24 -0400

    remove dead code and up coverage

--- synapse/lib/interval.py
+++ synapse/lib/interval.py
@@ -50,38 +50,3 @@ def parsetime(text):
     minv = s_time.parse(mins)
     maxv = s_time.parse(maxs, base=minv)
     return minv,maxv
-
-class TimeIval:
-
-    def __init__(self, *vals):
-
-        self.mint = None
-        self.maxt = None
-
-        vals = [ v for v in vals if v is not None ]
-        if vals:
-            self.mint = min(vals)
-            self.maxt = max(vals)
-
-    def append(self, x):
-        '''
-        Append a value to the list of times used to calculate this interval.
-
-        Args:
-            x (int):    millisecond timestamp since epoch or None
-
-        '''
-        if x is None:
-            return
-
-        if self.mint == None or x < self.mint:
-            self.mint = x
-
-        if self.maxt == None or x > self.maxt:
-            self.maxt = x
-
-    def minmax(self):
-        '''
-        Return the min,max values for the time interval.
-        '''
-        return self.mint,self.maxt
