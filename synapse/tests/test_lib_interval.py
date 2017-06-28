bacd39f5b87787e5ae9ba53ac09fed7c9f1ed5e6
invisig0th <invisigoth.kenshoto@gmail.com>
2017-06-28 11:00:29 -0400

    added test for interval overlap logic

--- synapse/tests/test_lib_interval.py
+++ synapse/tests/test_lib_interval.py
@@ -4,10 +4,24 @@ import synapse.lib.interval as s_interval
 
 class IvalTest(SynTest):
 
-    def test_ival_init(self):
+    def test_ival_fold(self):
         vals = [ None , 100, 20, None ]
         self.eq( s_interval.fold(*vals), (20,100) )
         self.none( s_interval.fold() )
+
+    def test_ival_overlap(self):
+
+        ival0 = s_interval.fold(10,20)
+        ival1 = s_interval.fold(15,30)
+        ival2 = s_interval.fold(30,50)
+        ival3 = s_interval.fold(1,100)
+
+        self.true( s_interval.overlap(ival0,ival1) )
+        self.false( s_interval.overlap(ival1,ival2) )
+
+        self.true( s_interval.overlap(ival0,ival3) )
+        self.true( s_interval.overlap(ival1,ival3) )
+        self.true( s_interval.overlap(ival2,ival3) )
 
     def test_ival_parsetime(self):
         ival = s_interval.parsetime('1970-1980')
