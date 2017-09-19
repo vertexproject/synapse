b93d54b6d0045cd425dbc3bc2ee4fc41f6c7c405
invisig0th <invisigoth.kenshoto@gmail.com>
2017-09-19 11:16:47 -0400

    stupid paren typo

--- synapse/tests/test_lib_auth.py
+++ synapse/tests/test_lib_auth.py
@@ -19,7 +19,7 @@ class AuthTest(SynTest):
         self.true(rulz.allow(('foo:bar', {'baz': 'faz'})))
 
         self.false(rulz.allow(('foo:bar', {'baz': 'wootwoot'})))
-        self.false(rulz.allow(('newp:newp', {}))
+        self.false(rulz.allow(('newp:newp', {})))
 
     def test_auth_runas(self):
         self.eq(s_auth.whoami(), 'root@localhost')
