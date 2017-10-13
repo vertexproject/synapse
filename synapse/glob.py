3918b881d99327201362e1a6db000e66e60a3532
invisig0th <invisigoth.kenshoto@gmail.com>
2017-10-13 16:24:13 -0400

    global pool max threads is now cpu_count() * 8

--- synapse/glob.py
+++ synapse/glob.py
@@ -1,9 +1,13 @@
 import threading
+import multiprocessing
 
 lock = threading.RLock()
 
 import synapse.lib.sched as s_sched
 import synapse.lib.threads as s_threads
 
-pool = s_threads.Pool()
+# go high since they will mostly be IO bound
+tmax = multiprocessing.cpu_count() * 8
+
+pool = s_threads.Pool(maxsize=tmax)
 sched = s_sched.Sched(pool=pool)
