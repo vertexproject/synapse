da8ade09a4dc7d518d43d8d23a011caa7ce6e7b6
invisig0th <invisigoth.kenshoto@gmail.com>
2017-10-27 06:27:28 -0400

    added geospace test module

---
+++ synapse/tests/test_model_geospace.py
@@ -0,0 +1,34 @@
+
+from synapse.tests.common import *
+
+class GeoTest(SynTest):
+
+    def test_model_geospace_types(self):
+
+        with self.getRamCore() as core:
+
+            #self.eq(core.getTypeNorm('geo:lat', '88.12345678')[0], 8812345678)
+            #self.eq(core.getTypeNorm('geo:lat', '-88.12345678')[0], -8812345678)
+
+            #self.eq(core.getTypeRepr('geo:lat', 8812345678), '88.12345678')
+            #self.eq(core.getTypeRepr('geo:lat', -8812345678), '-88.12345678')
+
+            #self.raises(BadTypeValu, core.getTypeNorm, 'geo:lat', '800')
+            #self.raises(BadTypeValu, core.getTypeNorm, 'geo:lat', '-800')
+
+            #self.eq(core.getTypeNorm('geo:long', '100.12345678')[0], 10012345678)
+            #self.eq(core.getTypeNorm('geo:long', '-100.12345678')[0], -10012345678)
+
+            #self.eq(core.getTypeRepr('geo:long', 10012345678), '100.12345678')
+            #self.eq(core.getTypeRepr('geo:long', -10012345678), '-100.12345678')
+
+            #self.raises(BadTypeValu, core.getTypeNorm, 'geo:long', '800')
+            #self.raises(BadTypeValu, core.getTypeNorm, 'geo:long', '-800')
+
+            valu, subs = core.getTypeNorm('geo:latlong', '-88.12345678,101.12345678')
+            self.eq(valu, '-88.12345678,101.12345678')
+            #self.eq(subs.get('lat'), -8812345678)
+            #self.eq(subs.get('long'), 10112345678)
+
+            valu, subs = core.getTypeNorm('geo:latlong', '-88.02000000,101.1100000000')
+            self.eq(valu, '-88.02,101.11')
