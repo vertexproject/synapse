9a9cb67d37eca870fc82f7c9bac2fea629d26b7f
invisig0th <invisigoth.kenshoto@gmail.com>
2017-10-30 11:42:13 -0400

    json2mpk helper tool

---
+++ synapse/tests/test_tools_json2mpk.py
@@ -0,0 +1,31 @@
+#from binascii import hexlify
+
+import synapse.tools.json2mpk as s_json2mpk
+
+from synapse.tests.common import *
+
+class Json2MpkTest(SynTest):
+
+    def test_tools_json2mpk(self):
+
+        with self.getTestDir() as ndir:
+
+            outp = self.getTestOutp()
+
+            path = os.path.join(ndir, 'woot.json')
+            newp = os.path.join(ndir, 'woot.mpk')
+
+            self.false(os.path.isfile(path))
+            self.false(os.path.isfile(newp))
+
+            with open(path, 'wb') as fd:
+                fd.write(b'{"foo":10}\n{"bar":20}\n')
+
+            args = ['--rm', path]
+
+            s_json2mpk.main(args, outp=outp)
+
+            with open(newp, 'rb') as fd:
+                self.eq(fd.read(), b'\x81\xa3foo\n\x81\xa3bar\x14')
+
+            self.false(os.path.isfile(path))
