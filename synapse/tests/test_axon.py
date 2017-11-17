baa2ce7fe140762be3eaca89eb354ca614a0f985
epiphyte <epiphyte@vertex.link>
2017-11-17 15:23:49 -0500

    Fix reference to axon:blob in tests.

--- synapse/tests/test_axon.py
+++ synapse/tests/test_axon.py
@@ -171,9 +171,9 @@ class AxonHostTest(SynTest):
 
             axon = host.axons.get(axfo[0])
 
-            self.true(axon.has('md5', blob[1].get('hash:md5')))
-            self.true(axon.has('sha1', blob[1].get('hash:sha1')))
-            self.true(axon.has('sha256', blob[1].get('hash:sha256')))
+            self.true(axon.has('md5', blob[1].get('axon:blob:md5')))
+            self.true(axon.has('sha1', blob[1].get('axon:blob:sha1')))
+            self.true(axon.has('sha256', blob[1].get('axon:blob:sha256')))
 
             props = {
                 'axon:syncmax': s_axon.megabyte * 10,
@@ -289,21 +289,21 @@ class AxonHostTest(SynTest):
             time.sleep(1)
 
             self.nn(blob)
-            self.true(axon0.has('md5', blob[1].get('hash:md5')))
-            self.true(axon0.has('sha1', blob[1].get('hash:sha1')))
-            self.true(axon0.has('sha256', blob[1].get('hash:sha256')))
+            self.true(axon0.has('md5', blob[1].get('axon:blob:md5')))
+            self.true(axon0.has('sha1', blob[1].get('axon:blob:sha1')))
+            self.true(axon0.has('sha256', blob[1].get('axon:blob:sha256')))
             axonbyts = b''.join(_byts for _byts in axon0.iterblob(blob))
             self.eq(axonbyts, cv)
 
-            self.true(axonc1.has('md5', blob[1].get('hash:md5')))
-            self.true(axonc1.has('sha1', blob[1].get('hash:sha1')))
-            self.true(axonc1.has('sha256', blob[1].get('hash:sha256')))
+            self.true(axonc1.has('md5', blob[1].get('axon:blob:md5')))
+            self.true(axonc1.has('sha1', blob[1].get('axon:blob:sha1')))
+            self.true(axonc1.has('sha256', blob[1].get('axon:blob:sha256')))
             axonbyts = b''.join(_byts for _byts in axonc1.iterblob(blob))
             self.eq(axonbyts, cv)
 
-            self.true(axonc2.has('md5', blob[1].get('hash:md5')))
-            self.true(axonc2.has('sha1', blob[1].get('hash:sha1')))
-            self.true(axonc2.has('sha256', blob[1].get('hash:sha256')))
+            self.true(axonc2.has('md5', blob[1].get('axon:blob:md5')))
+            self.true(axonc2.has('sha1', blob[1].get('axon:blob:sha1')))
+            self.true(axonc2.has('sha256', blob[1].get('axon:blob:sha256')))
             axonbyts = b''.join(_byts for _byts in axonc2.iterblob(blob))
             self.eq(axonbyts, cv)
 
