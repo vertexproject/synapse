b041d0b5f37ee52274b17dc59fbe3ccb2b42918b
vertexmc <vertexmc@users.noreply.github.com>
2017-11-24 09:23:14 -0500

    Adds geospace type tests

--- synapse/tests/test_model_geospace.py
+++ synapse/tests/test_model_geospace.py
@@ -3,7 +3,7 @@ from synapse.tests.common import *
 
 class GeoTest(SynTest):
 
-    def test_model_geospace_types(self):
+    def test_model_geospace_types_latlong(self):
 
         with self.getRamCore() as core:
 
@@ -11,6 +11,7 @@ class GeoTest(SynTest):
             self.raises(BadTypeValu, core.getTypeNorm, 'geo:latlong', '-91,100')
             self.raises(BadTypeValu, core.getTypeNorm, 'geo:latlong', '80,181')
             self.raises(BadTypeValu, core.getTypeNorm, 'geo:latlong', '80,-181')
+            self.raises(BadTypeValu, core.getTypeNorm, 'geo:latlong', 'hehe,hoho')
 
             valu, subs = core.getTypeNorm('geo:latlong', '-88.12345678,101.12345678')
             self.eq(valu, '-88.12345678,101.12345678')
@@ -18,8 +19,20 @@ class GeoTest(SynTest):
             valu, subs = core.getTypeNorm('geo:latlong', '-88.02000000,101.1100000000')
             self.eq(valu, '-88.02,101.11')
 
+            valu, subs = core.getTypeNorm('geo:latlong', '??')
+            self.eq(valu, '??')
+
+    def test_model_geospace_types_dist(self):
+
+        with self.getRamCore() as core:
+
             valu, subs = core.getTypeNorm('geo:dist', '11.2 km')
             self.eq(valu, 11200000)
+
+            valu, subs = core.getTypeNorm('geo:dist', 11200000)
+            self.eq(valu, 11200000)
+
+            self.raises(SyntaxError, core.getTypeNorm, 'geo:dist', '1.3 pc')
 
     def test_model_geospace_nloc(self):
 
