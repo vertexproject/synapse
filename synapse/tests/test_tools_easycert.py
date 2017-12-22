2ba4831e7dc6bee38e8c5aecb5b9a46a2e12c45f
epiphyte <epiphyte@vertex.link>
2017-12-22 09:29:49 -0500

    Rewrite csr workflow test.

--- synapse/tests/test_tools_easycert.py
+++ synapse/tests/test_tools_easycert.py
@@ -68,42 +68,53 @@ class TestEasyCert(SynTest):
             outp = self.getTestOutp()
             argv = ['--csr', '--certdir', path, 'user@test.com']
             self.eq(s_easycert.main(argv, outp=outp), 0)
-            outp.expect('csr saved:')
+            self.true(outp.expect('key saved'))
+            self.true(outp.expect('user@test.com.key'))
+            self.true(outp.expect('csr saved'))
+            self.true(outp.expect('user@test.com.csr'))
 
             # Generate a server CSR
             outp = self.getTestOutp()
             argv = ['--csr', '--certdir', path, 'wwww.vertex.link', '--server']
             self.eq(s_easycert.main(argv, outp=outp), 0)
-            outp.expect('csr saved:')
+            self.true(outp.expect('key saved'))
+            self.true(outp.expect('www.vertex.link.key'))
+            self.true(outp.expect('csr saved'))
+            self.true(outp.expect('www.vertex.link.csr'))
 
             # Ensure that duplicate files won't be overwritten
             outp = self.getTestOutp()
             argv = ['--csr', '--certdir', path, 'user@test.com']
             self.eq(s_easycert.main(argv, outp=outp), -1)
-            outp.expect('file exists:')
+            self.true(outp.expect('file exists:'))
+            self.true(outp.expect('user@test.com.key'))
 
             self.make_testca(path)
 
+            # Sign the user csr
             outp = self.getTestOutp()
             csrpath = os.path.join(path, 'users', 'user@test.com.csr')
             argv = ['--certdir', path, '--signas', 'testca', '--sign-csr', csrpath, ]
             self.eq(s_easycert.main(argv, outp=outp), 0)
-            outp.expect('cert saved:')
+            self.true(outp.expect('cert saved:'))
+            self.true(outp.expect('user@test.com.crt'))
 
             # Ensure we can do server certificate signing
             outp = self.getTestOutp()
             csrpath = os.path.join(path, 'hosts', 'wwww.vertex.link.csr')
             argv = ['--certdir', path, '--signas', 'testca', '--sign-csr', csrpath, '--server']
             self.eq(s_easycert.main(argv, outp=outp), 0)
-            outp.expect('cert saved:')
+            self.true(outp.expect('cert saved:'))
+            self.true(outp.expect('www.vertex.link.crt'))
 
+            # test nonexistent csr
             outp = self.getTestOutp()
             argv = ['--certdir', path, '--signas', 'testca', '--sign-csr', 'lololol', ]
             self.eq(s_easycert.main(argv, outp=outp), -1)
-            outp.expect('csr not found')
+            self.true(outp.expect('csr not found: lololol'))
 
             # Test bad input
             outp = self.getTestOutp()
             argv = ['--certdir', path, '--sign-csr', 'lololol', ]
             self.eq(s_easycert.main(argv, outp=outp), -1)
-            outp.expect('--sign-csr requires --signas')
+            self.true(outp.expect('--sign-csr requires --signas'))
