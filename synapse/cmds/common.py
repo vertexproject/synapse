3a9d7f86049a440c6ac1a4100ebaee6a217f4507
vertexmc <vertexmc@users.noreply.github.com>
2018-01-28 10:15:47 -0500

    Removes unnecessary py cmd from Synapse CLI

--- synapse/cmds/common.py
+++ synapse/cmds/common.py
@@ -17,25 +17,3 @@ class GuidCmd(s_cli.Cmd):
 
     def runCmdOpts(self, opts):
         self.printf('new guid: %r' % (s_common.guid(),))
-
-class PyCmd(s_cli.Cmd):
-    '''
-    Evaluate a line of python code with the cmd item.
-
-    Examples:
-
-        py item.getFooThingByBar('baz')
-
-    '''
-
-    _cmd_name = 'py'
-    _cmd_syntax = (
-        ('expr', {'type': 'glob'}),
-    )
-
-    def runCmdOpts(self, opts):
-        expr = opts.get('expr')
-        item = self.getCmdItem()
-
-        retn = eval(expr, {'item': item, 'cmd': self})
-        self.printf('returned: %r' % (retn,))
