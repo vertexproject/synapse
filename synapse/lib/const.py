6138b50878edc924fc7ffb9a7499263c68863821
vertexmc <vertexmc@users.noreply.github.com>
2018-02-01 16:29:27 -0500

    Add a constants file to replace scattered definitions of byte units

---
+++ synapse/lib/const.py
@@ -0,0 +1,17 @@
+kilobyte = 1000
+megabyte = 1000 * kilobyte
+gigabyte = 1000 * megabyte
+terabyte = 1000 * gigabyte
+petabyte = 1000 * terabyte
+exabyte = 1000 * petabyte
+zettabyte = 1000 * exabyte
+yottabyte = 1000 * zettabyte
+
+kibibyte = 1024
+mebibyte = 1000 * kibibyte
+gibibyte = 1000 * mebibyte
+tebibyte = 1000 * gibibyte
+pebibyte = 1000 * tebibyte
+exbibyte = 1000 * pebibyte
+zebibyte = 1000 * exbibyte
+yobibyte = 1000 * zebibyte
