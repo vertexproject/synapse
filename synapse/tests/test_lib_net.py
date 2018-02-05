103f6f45fbab82ec825bcb9d3a2295cd36f51cc8
vertexmc <vertexmc@users.noreply.github.com>
2018-02-05 13:53:09 -0500

    Adds unit test for receiving an unknown message type

--- synapse/tests/test_lib_net.py
+++ synapse/tests/test_lib_net.py
@@ -54,3 +54,44 @@ class NetTest(SynTest):
             plex.connect(addr, onconn)
 
             steps.waitall(timeout=2)
+
+    def test_lib_net_unknown(self):
+
+        names = ('conn', 'lisn')
+        steps = self.getTestSteps(names)
+        expected_msg = 'unknown message type wat'
+
+        with self.getLoggerStream('synapse.lib.net', expected_msg) as stream:
+
+            class LisnLink(s_net.Link):
+
+                def linked(self):
+                    steps.done('lisn')
+
+            class ConnLink(s_net.Link):
+
+                def linked(self):
+                    steps.done('conn')
+                    self.tx(('wat', {}))
+
+            with s_net.Plex() as plex:
+
+                def onconn(ok, link):
+                    conn = ConnLink(link)
+                    link.onrx(conn.rx)
+                    conn.linked()
+
+                def onlink(link):
+                    lisn = LisnLink(link)
+                    link.onrx(lisn.rx)
+                    lisn.linked()
+
+                addr = plex.listen(('127.0.0.1', 0), onlink)
+                plex.connect(addr, onconn)
+
+                steps.waitall(timeout=2)
+                self.true(stream.wait(10))
+
+            stream.seek(0)
+            mesgs = stream.read()
+            self.isin(expected_msg, mesgs)
