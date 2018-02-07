fbfbaa09f1262a9c933eb258334fd95f08156c06
vertexmc <vertexmc@users.noreply.github.com>
2018-02-07 16:25:08 -0500

    Add WIP test for calling fini on listen link and then sending on connect link

--- synapse/tests/test_lib_net.py
+++ synapse/tests/test_lib_net.py
@@ -177,3 +177,56 @@ class NetTest(SynTest):
             stream.seek(0)
             mesgs = stream.read()
             self.isin(expected_msg, mesgs)
+
+    def test_lib_net_stoplisten(self):
+
+        names = ('conn', 'lisn', 'ping', 'pong')
+
+        steps = self.getTestSteps(names)
+
+        class LisnLink(s_net.Link):
+
+            def linked(self):
+                steps.done('lisn')
+
+            def handlers(self):
+                return {
+                    'ping': self._onRecvPing,
+                }
+
+            def _onRecvPing(self, link, mesg):
+                self.tx(('pong', {}))
+                self.fini()
+                steps.done('ping')
+
+        class ConnLink(s_net.Link):
+
+            def linked(self):
+                self.tx(('ping', {'haha': 2}))
+                steps.done('conn')
+
+            def handlers(self):
+                return {
+                    'pong': self._onRecvPong,
+                }
+
+            def _onRecvPong(self, link, mesg):
+                self.tx(('ping', {'haha': 2}))
+                steps.done('pong')
+
+        with s_net.Plex() as plex:
+
+            def onconn(ok, link):
+                conn = ConnLink(link)
+                link.onrx(conn.rx)
+                conn.linked()
+
+            def onlink(link):
+                lisn = LisnLink(link)
+                link.onrx(lisn.rx)
+                lisn.linked()
+
+            addr = plex.listen(('127.0.0.1', 0), onlink)
+            plex.connect(addr, onconn)
+
+            steps.waitall(timeout=2)
