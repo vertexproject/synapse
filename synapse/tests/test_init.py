753dea6cb1e961fff55c3cbcc6be8b953c76efe0
vertexmc <vertexmc@users.noreply.github.com>
2018-02-14 11:37:57 -0500

    Added test for synapse __init__.py

---
+++ synapse/tests/test_init.py
@@ -0,0 +1,18 @@
+import os
+import imp
+
+import synapse
+from synapse.tests.common import *
+
+class InitTest(SynTest):
+
+    def test_init_modules(self):
+        os.environ['SYN_MODULES'] = 'fakenotrealmod , badnothere,math'
+        msg = 'SYN_MODULES failed: badnothere (NoSuchDyn: name=\'badnothere\')'
+        with self.getLoggerStream('synapse', msg) as stream:
+            imp.reload(synapse)
+            self.true(stream.wait(10))
+
+        stream.seek(0)
+        self.isin(msg, stream.read())
+        self.isin(('math', 2.0, None), synapse.lib.modules.call('sqrt', 4))
