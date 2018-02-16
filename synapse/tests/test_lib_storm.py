f4c4ed0a7bb62dfda2995f998b47df8b053cfe34
invisig0th <invisigoth.kenshoto@gmail.com>
2018-02-16 11:17:05 -0500

    bow before the power of the pycodestyle build phase

--- synapse/tests/test_lib_storm.py
+++ synapse/tests/test_lib_storm.py
@@ -1479,7 +1479,7 @@ class StormTest(SynTest):
         self.len(0, list(quer.iterVarTree('asdf')))
 
         for node, varz in quer.iterVarTree('a'):
-            quer.addTreeVar('b', ['z','q'], tree=node)
+            quer.addTreeVar('b', ['z', 'q'], tree=node)
 
         rets = []
         for node, varz in quer.iterVarTree('a.b'):
