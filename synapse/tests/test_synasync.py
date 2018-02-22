f44054434e7acf6ba9e7e482607957530d93aa24
vertexmc <vertexmc@users.noreply.github.com>
2018-02-22 17:33:51 -0500

    Adds broken s_async.jobret(job) test
    - jobret does not handle RetnErr being raised from within a job

--- synapse/tests/test_synasync.py
+++ synapse/tests/test_synasync.py
@@ -10,6 +10,15 @@ from synapse.tests.common import *
 
 class AsyncTests(SynTest):
 
+    def test_async_jobret(self):
+        job = (32 * '0', {
+            'err': 'RetnErr', 'errmsg': "RetnErr: excn='NotReady'",
+            'errfile': '/afile.py', 'errline': 123456, 'errinfo': {'excn': 'NotReady'}
+        })
+        s_async.jobret(job)
+        # FIXME - broken test demonstrating jobret not handling RetnErr
+        # This should raise RetnErr or NotReady
+
     def test_async_basics(self):
 
         boss = s_async.Boss()
