43948b4a3bd0a74c48f93c7839720e27a698a992
epiphyte <epiphyte@vertex.link>
2018-03-26 12:53:17 -0400

    Bump version: 0.0.48 → 0.0.49

--- synapse/lib/version.py
+++ synapse/lib/version.py
@@ -183,5 +183,5 @@ def parseVersionParts(text, seps=vseps):
 ##############################################################################
 # The following are touched during the release process by bumpversion.
 # Do not modify these directly.
-version = (0, 0, 48)
+version = (0, 0, 49)
 verstring = '.'.join([str(x) for x in version])
