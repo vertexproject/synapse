ff0554e2e69bddac60f8e0f8d2c3f6bf1663a54c
epiphyte <epiphyte@vertex.link>
2018-04-08 13:42:10 -0400

    Bump version: 0.0.49 → 0.0.50

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse',
-    version='0.0.49',
+    version='0.0.50',
     description='Synapse Distributed Key-Value Hypergraph Analysis Framework',
     author='Invisigoth Kenshoto',
     author_email='invisigoth.kenshoto@gmail.com',
