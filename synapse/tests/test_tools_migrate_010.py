f934754c9653d01339f1fd059f9a9a47840be1b2
Nic Watson <github@nicwatson.org>
2018-05-04 12:31:28 -0400

    Improve test coverage.

--- synapse/tests/test_tools_migrate_010.py
+++ synapse/tests/test_tools_migrate_010.py
@@ -1,13 +1,14 @@
 import pathlib
 
-import synapse.lib.msgpack as s_msgpack
+import synapse.common as s_common
 import synapse.lib.iq as s_iq
+import synapse.lib.msgpack as s_msgpack
 
 import synapse.tools.migrate_010 as s_migrate
 
 class Migrate010Test(s_iq.SynTest):
 
-    def get_formfile(self, formname, dirn, expected):
+    def get_formfile(self, formname, dirn):
         fnam = (dirn / formname.replace(':', '_')).with_suffix('.dump')
         nodes = list(s_msgpack.iterfile(fnam))
         return nodes
@@ -46,10 +47,40 @@ class Migrate010Test(s_iq.SynTest):
             core.formTufoByProp('ps:contact', info)
 
             s_migrate.migrateInto010(core, dirn)
-            contact_nodes = self.get_formfile('ps:contact', dirn, [{}])
-            self.eq(len(contact_nodes), 1)
-            node = contact_nodes[0]
+            nodes = self.get_formfile('ps:contact', dirn)
+            self.eq(len(nodes), 1)
+            node = nodes[0]
             self.eq(type(node), tuple)
             self.eq(len(node[0]), 2)
             self.eq(node[0][0], 'ps:contact')
             self.eq(node[1]['props']['title'], 'ceo')
+
+            acct_nodes = self.get_formfile('inet:web:acct', dirn)
+            self.eq(len(acct_nodes), 1)
+            node = acct_nodes[0]
+            self.eq(type(node), tuple)
+            self.eq(len(node[0]), 2)
+            self.eq(node[0], ('inet:web:acct', ('twitter.com', 'ironman')))
+
+            props = {'a': 'WOOT.com/1.002.3.4', 'rcode': 0, 'time': s_common.now(), 'ipv4': '5.5.5.5',
+                     'udp4': '8.8.8.8:80'}
+            core.formTufoByProp('inet:dns:look', '*', **props)
+            s_migrate.migrateInto010(core, dirn)
+            look_nodes = self.get_formfile('inet:dns:look', dirn)
+            self.eq(len(look_nodes), 1)
+            self.eq(look_nodes[0][1]['props']['inet:dns:look:udp4'], 'udp://134744072:80/')
+
+            tufo = core.formTufoByProp('inet:web:logon', '*', acct='vertex.link/pennywise', time=s_common.now(),
+                                       ipv4=0x01020304)
+            core.addTufoTag(tufo, 'test')
+            s_migrate.migrateInto010(core, dirn, tag='test')
+            nodes = self.get_formfile('inet:web:logon', dirn)
+            self.eq(len(nodes), 1)
+            self.eq(nodes[0][1]['props']['client'], 'tcp://16909060/')
+
+            core.formTufoByProp('inet:web:post', ('vertex.link/visi', 'knock knock'), time='20141217010101')
+            s_migrate.migrateInto010(core, dirn)
+            nodes = self.get_formfile('inet:web:post', dirn)
+            self.eq(len(nodes), 1)
+            # Make sure the primary val is a guid
+            self.eq(len(nodes[0][0][1]), 32)
