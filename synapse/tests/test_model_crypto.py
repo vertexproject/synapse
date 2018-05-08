c650b59b64c3aa09a35cfcba22a71e96754409ef
epiphyte <epiphyte@vertex.link>
2018-05-08 10:13:45 -0400

    Fix tests which were directly accessing Node.props

--- synapse/tests/test_model_crypto.py
+++ synapse/tests/test_model_crypto.py
@@ -55,27 +55,22 @@ class CryptoModelTest(s_test.SynTest):
                 # md5
                 node = xact.addNode('hash:md5', TEST_MD5.upper())
                 self.eq(node.ndef, ('hash:md5', TEST_MD5))
-                self.eq(node.props, {})
                 self.raises(s_exc.BadTypeValu, xact.addNode, 'hash:md5', TEST_SHA1)
                 # sha1
                 node = xact.addNode('hash:sha1', TEST_SHA1.upper())
                 self.eq(node.ndef, ('hash:sha1', TEST_SHA1))
-                self.eq(node.props, {})
                 self.raises(s_exc.BadTypeValu, xact.addNode, 'hash:sha1', TEST_SHA256)
                 # sha256
                 node = xact.addNode('hash:sha256', TEST_SHA256.upper())
                 self.eq(node.ndef, ('hash:sha256', TEST_SHA256))
-                self.eq(node.props, {})
                 self.raises(s_exc.BadTypeValu, xact.addNode, 'hash:sha256', TEST_SHA384)
                 # sha384
                 node = xact.addNode('hash:sha384', TEST_SHA384.upper())
                 self.eq(node.ndef, ('hash:sha384', TEST_SHA384))
-                self.eq(node.props, {})
                 self.raises(s_exc.BadTypeValu, xact.addNode, 'hash:sha384', TEST_SHA512)
                 # sha512
                 node = xact.addNode('hash:sha512', TEST_SHA512.upper())
                 self.eq(node.ndef, ('hash:sha512', TEST_SHA512))
-                self.eq(node.props, {})
                 self.raises(s_exc.BadTypeValu, xact.addNode, 'hash:sha512', TEST_MD5)
 
     def test_form_rsakey(self):
