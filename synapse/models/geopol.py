593a4e8f81bced7b83c7b64e3a107c45803a5579
vertexmc <vertexmc@users.noreply.github.com>
2018-05-11 11:31:54 -0400

    WIP model testing ideas

--- synapse/models/geopol.py
+++ synapse/models/geopol.py
@@ -8,25 +8,22 @@ class PolModule(s_module.CoreModule):
 
                 'types': (
 
-                    ('pol:country',
-                        ('guid', {}),
-                        {'doc': 'A GUID for a country'}
-                    ),
+                    ('pol:country', ('guid', {}), {
+                        'doc': 'A GUID for a country',
+                        'ex': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+                    }),
 
-                    ('pol:iso2',
-                        ('str', {'lower': True, 'regex': '^[a-z0-9]{2}$', 'nullval': '??'}),
-                        {'doc': 'The 2 digit ISO country code', 'ex': 'us'}
-                    ),
+                    ('pol:iso2', ('str', {'lower': True, 'regex': '^[a-z0-9]{2}$', 'nullval': '??'}), {
+                        'doc': 'The 2 digit ISO country code', 'ex': 'us'
+                    }),
 
-                    ('pol:iso3',
-                        ('str', {'lower': True, 'regex': '^[a-z0-9]{3}$', 'nullval': '??'}),
-                        {'doc': 'The 3 digit ISO country code', 'ex': 'usa'}
-                    ),
+                    ('pol:iso3', ('str', {'lower': True, 'regex': '^[a-z0-9]{3}$', 'nullval': '??'}), {
+                        'doc': 'The 3 digit ISO country code', 'ex': 'usa'
+                    }),
 
-                    ('pol:isonum',
-                        ('int', {}),
-                        {'doc': 'The ISO integer country code', 'ex': '840'}
-                    ),
+                    ('pol:isonum', ('int', {}), {
+                        'doc': 'The ISO integer country code', 'ex': '840'
+                    }),
 
                 ),
 
