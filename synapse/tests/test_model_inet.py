748d9d466405a178e67c82bc6512cba44b1329fd
vertexmc <vertexmc@users.noreply.github.com>
2018-05-16 19:14:22 -0400

    Add inet:wifi:ap test

--- synapse/tests/test_model_inet.py
+++ synapse/tests/test_model_inet.py
@@ -957,6 +957,19 @@ class InetModelTest(s_t_common.SynTest):
                 node = snap.addNode(formname, valu)
                 self.checkNode(node, (expected_ndef, expected_props))
 
+    def test_wifi_ap(self):
+        formname = 'inet:wifi:ap'
+        valu = ('The Best SSID2 ', '00:11:22:33:44:55')
+        expected_props = {
+            'ssid': valu[0],
+            'bssid': valu[1]
+        }
+        expected_ndef = (formname, valu)
+        with self.getTestCore() as core:
+            with core.snap(write=True) as snap:
+                node = snap.addNode(formname, valu)
+                self.checkNode(node, (expected_ndef, expected_props))
+
 class FIXME:
 
     def test_model_inet_whoisemail(self):
