7b825d860c84eae8b5a74ef16ae8e1da08dde888
epiphyte <epiphyte@vertex.link>
2018-05-24 13:56:48 -0400

    Add -alpha prerelease version to version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse',
-    version='0.1.0',
+    version='0.1.0-alpha',
     description='Synapse Distributed Key-Value Hypergraph Analysis Framework',
     author='Invisigoth Kenshoto',
     author_email='invisigoth.kenshoto@gmail.com',
