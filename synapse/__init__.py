70349f7dd91ca79220fc6b437e9b4f2aafde64cd
vertexmc <vertexmc@users.noreply.github.com>
2018-05-31 23:04:07 -0400

    Remove tornado as nothing else is using it

--- synapse/__init__.py
+++ synapse/__init__.py
@@ -1,27 +1,23 @@
 '''
 The synapse distributed key-value hypergraph analysis framework.
 '''
+# stdlib
 import sys
-import msgpack
-import tornado
 import logging
 import multiprocessing
-
-logger = logging.getLogger(__name__)
+# third party code
+import msgpack
+# custom code
+import synapse.glob as s_glob  # setup glob here to avoid import loops...
+import synapse.lib.plex as s_plex
+import synapse.lib.threads as s_threads
 
 if (sys.version_info.major, sys.version_info.minor) < (3, 6):  # pragma: no cover
     raise Exception('synapse is not supported on Python versions < 3.6')
-
 if msgpack.version < (0, 5, 0):  # pragma: no cover
     raise Exception('synapse requires msgpack >= 0.5.0')
 
-if tornado.version_info < (3, 2, 2):  # pragma: no cover
-    raise Exception('synapse requires tornado >= 3.2.2')
-
-# setup glob here to avoid import loops...
-import synapse.glob as s_glob
-import synapse.lib.plex as s_plex
-import synapse.lib.threads as s_threads
+logger = logging.getLogger(__name__)
 
 tmax = multiprocessing.cpu_count() * 8
 
