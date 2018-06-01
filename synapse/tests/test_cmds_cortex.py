80e87c6ee164c679486271aa4468bdf31a846bf2
vertexmc <vertexmc@users.noreply.github.com>
2018-06-01 13:10:31 -0400

    Another help test

--- synapse/tests/test_cmds_cortex.py
+++ synapse/tests/test_cmds_cortex.py
@@ -1,7 +1,3 @@
-
-
-import regex
-
 import synapse.lib.cmdr as s_cmdr
 import synapse.tests.common as s_test
 
@@ -20,6 +16,11 @@ class CmdCoreTest(s_test.SynTest):
             cmdr = s_cmdr.getItemCmdr(core, outp=outp)
             cmdr.runCmdLine('help ask')
             outp.expect(help_msg)
+
+            outp = self.getTestOutp()
+            cmdr = s_cmdr.getItemCmdr(core, outp=outp)
+            cmdr.runCmdLine('ask help')
+            outp.expect('For detailed help on any command')
 
             outp = self.getTestOutp()
             cmdr = s_cmdr.getItemCmdr(core, outp=outp)
@@ -98,6 +99,7 @@ class CmdCoreTest(s_test.SynTest):
             outp.expect('err')
             outp.expect('NoSuchProp')
 
+# FIXME incorporate these into storm tests
 '''
 class SynCmdCoreTest(s_test.SynTest):
 
