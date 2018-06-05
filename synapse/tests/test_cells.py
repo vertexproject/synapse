9f1964f9f83c493f9bc6e08e2058d1e14ace031f
epiphyte <epiphyte@vertex.link>
2018-06-05 07:54:28 -0400

    Add a test for s_cells.deploy()

--- synapse/tests/test_cells.py
+++ synapse/tests/test_cells.py
@@ -9,3 +9,9 @@ class CellTest(SynTest):
         data = s_cells.getCells()
         data = {k: v for k, v in data}
         self.isin('cortex', data)
+
+    def test_deploy(self):
+        with self.getTestDir() as dirn:
+            s_cells.deploy('cortex', dirn, {'test': 1})
+            d = s_common.yamlload(dirn, 'boot.yaml')
+            self.eq(d, {'type': 'cortex', 'test': 1, })
