c845739767918ba3dbd65c0c817242e91877ffc2
epiphyte <epiphyte@vertex.link>
2018-06-05 08:18:25 -0400

    Add tests for tools.deploy, make tools.deploy use outp and do consisten return codes.

--- synapse/tools/deploy.py
+++ synapse/tools/deploy.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging
 import argparse
 
 import synapse.exc as s_exc
@@ -7,57 +8,51 @@ import synapse.cells as s_cells
 import synapse.common as s_common
 import synapse.dyndeps as s_dyndeps
 
+import synapse.lib.output as s_output
+
 import synapse.tools.dmon as s_dmon
+
+logger = logging.getLogger(__name__)
 
 desc = '''
 Deploy a cell to a daemon directory.
 '''
 
-def main(argv):
+def main(argv, outp=None):
+    if outp is None:  # pragma: no cover
+        outp = s_output.OutPut()
 
-    pars = argparse.ArgumentParser('synapse.tools.deploy', description=desc)
-
-    pars.add_argument('celltype', help='The cell type to deploy by name.')
-    pars.add_argument('cellname', help='The cell type to deploy by name.')
-    pars.add_argument('dmonpath', nargs='?', default=s_dmon.dmonpath,
-                        help='The synapse.tools.dmon directory where the cell will be deployed.')
-
-    pars.add_argument('--listen', action='store', help='URL for the daemon to listen to (only set if the dmon.yaml '
-                                                       'does not exist).')
-    pars.add_argument('--auth', action='store_true', help='Enable the cell auth subsystem.')
-    pars.add_argument('--admin', help='Set the initial <user>:<passwd> as an admin (enables --auth).')
-    pars.add_argument('--module', action='store', help='An additional module to load. This can be used to initialize '
-                                                       'third party module code which may register additional Cells.')
-    pars.add_argument('--cells', action='store_true', help='Print registered cell types and exit.')
-
+    pars = makeargpaser()
     opts = pars.parse_args(argv)
 
     if opts.module:
         mod = s_dyndeps.tryDynMod(opts.module)
-        print(f'Loaded {opts.module}@{mod}')
+        outp.printf(f'Loaded {opts.module}@{mod}')
 
     if opts.cells:
-        print('Registered cells:')
+        outp.printf('Registered cells:')
         for cname, cpath in s_cells.getCells():
-            # print(f'[{cname}] {cpath:^12}')
-            print(f'{cname:<10} {cpath:>10}')
+            outp.printf(f'{cname:<10} {cpath:>10}')
         return 0
 
     dirn = s_common.genpath(opts.dmonpath, 'cells', opts.cellname)
     if os.path.isdir(dirn):
-        print(f'cell directory already exists: {dirn}')
-        return
+        outp.printf(f'cell directory already exists: {dirn}')
+        return 1
 
     dmon = {}
     if opts.listen:
         dmon['listen'] = opts.listen
 
+    if opts.module:
+        dmon['modules'] = [opts.module]
+
     if dmon:
         dmon.setdefault('modules', [])
         dmon_fp = os.path.join(opts.dmonpath, 'dmon.yaml')
         if os.path.exists(dmon_fp):
-            raise s_exc.SynErr(mesg='Cannot smash existing dmon.yaml file.',
-                               file=dmon_fp)
+            outp.printf(f'Cannot overwrite existing dmon.yaml file. [{dmon_fp}]')
+            return 1
         s_common.yamlsave(dmon, dmon_fp)
 
     boot = {
@@ -71,8 +66,30 @@ def main(argv):
         boot['auth:en'] = True
         boot['auth:admin'] = opts.admin
 
-    print(f'Deploying a {opts.celltype} at: {dirn}')
+    outp.printf(f'Deploying a {opts.celltype} at: {dirn}')
     s_cells.deploy(opts.celltype, dirn, boot)
+    return 0
 
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+def makeargpaser():
+    pars = argparse.ArgumentParser('synapse.tools.deploy', description=desc)
+
+    pars.add_argument('celltype', help='The cell type to deploy by name.')
+    pars.add_argument('cellname', help='The cell type to deploy by name.')
+    pars.add_argument('dmonpath', nargs='?', default=s_dmon.dmonpath,
+                      help='The synapse.tools.dmon directory where the cell will be deployed.')
+
+    pars.add_argument('--listen', action='store', help='URL for the daemon to listen to (only set if the dmon.yaml '
+                                                       'does not exist).')
+    pars.add_argument('--auth', action='store_true', help='Enable the cell auth subsystem.')
+    pars.add_argument('--admin', help='Set the initial <user>:<passwd> as an admin (enables --auth).')
+    pars.add_argument('--module', action='store', help='An additional module to load. This can be used to initialize '
+                                                       'third party module code which may register additional Cells.')
+    pars.add_argument('--cells', action='store_true', help='Print registered cell types and exit.')
+    return pars
+
+def _main():  # pragma: no cover
+    s_common.setlogging(logger, 'DEBUG')
+    main(sys.argv[1:])
+
+if __name__ == '__main__':  # pragma: no cover
+    _main()
