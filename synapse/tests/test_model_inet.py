cd5bd12a73c05aa5eab041208741cc4c15be9c75
epiphyte <epiphyte@vertex.link>
2018-08-07 13:08:08 -0400

    Add a bunch of cidr range lifts from 00x to 010 cidr tests.

--- synapse/tests/test_model_inet.py
+++ synapse/tests/test_model_inet.py
@@ -54,6 +54,40 @@ class InetModelTest(s_t_common.SynTest):
             self.len(4, core.eval('inet:ipv4 +inet:ipv4=1.2.3.0/30'))
             self.len(1, core.eval('inet:ipv4 -inet:ipv4=1.2.3.0/30'))
 
+            self.len(256, core.eval('[ inet:ipv4=192.168.1.0/24]'))
+            self.len(256, core.eval('[ inet:ipv4=192.168.2.0/24]'))
+            self.len(256, core.eval('inet:ipv4=192.168.1.0/24'))
+
+            # Seed some nodes for bounds checking
+            pnodes = [(('inet:ipv4', f'10.2.1.{d}'), {}) for d in range(1, 33)]
+            nodes = list(core.addNodes(pnodes))
+
+            nodes = list(core.eval('inet:ipv4=10.2.1.4/32'))
+            self.len(1, nodes)
+
+            nodes = list(core.eval('inet:ipv4=10.2.1.4/31'))
+            self.len(2, nodes)
+
+            # 10.2.1.1/30 is 10.2.1.0 -> 10.2.1.3 but we don't have 10.2.1.0 in the core
+            nodes = list(core.eval('inet:ipv4=10.2.1.1/30'))
+            self.len(3, nodes)
+
+            # 10.2.1.2/30 is 10.2.1.0 -> 10.2.1.3 but we don't have 10.2.1.0 in the core
+            nodes = list(core.eval('inet:ipv4=10.2.1.2/30'))
+            self.len(3, nodes)
+
+            # 10.2.1.1/29 is 10.2.1.0 -> 10.2.1.7 but we don't have 10.2.1.0 in the core
+            nodes = list(core.eval('inet:ipv4=10.2.1.1/29'))
+            self.len(7, nodes)
+
+            # 10.2.1.8/29 is 10.2.1.8 -> 10.2.1.15
+            nodes = list(core.eval('inet:ipv4=10.2.1.8/29'))
+            self.len(8, nodes)
+
+            # 10.2.1.1/28 is 10.2.1.0 -> 10.2.1.15 but we don't have 10.2.1.0 in the core
+            nodes = list(core.eval('inet:ipv4=10.2.1.1/28'))
+            self.len(15, nodes)
+
     def test_addr(self):
         formname = 'inet:addr'
         with self.getTestCore() as core:
