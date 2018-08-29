e7b131e386d114f4b2d40057dcc769ace9dc5ef9
visi <visi@vertex.link>
2018-08-29 12:17:13 -0400

    remove tests for perms at snap layer

--- synapse/tests/test_lib_snap.py
+++ synapse/tests/test_lib_snap.py
@@ -53,27 +53,6 @@ class SnapTest(s_t_common.SynTest):
                 nodes = list(snap.addFeedNodes('test.genr', []))
                 self.len(2, nodes)
 
-    def test_addNode_noperms(self):
-        form = 'teststr'
-        valu = 'cool'
-
-        with self.getTestCore() as core:
-
-            with core.snap() as snap:
-
-                with self.getTestDir() as dirn:
-
-                    with s_auth.Auth(dirn) as auth:
-
-                        user = auth.addUser('hatguy')
-                        snap.setUser(user)
-
-                        self.true(snap.strict)  # Following assertions based on snap.strict being true
-                        self.raises(s_exc.AuthDeny, snap.addNode, form, valu)
-
-                        snap.strict = False
-                        self.none(snap.addNode(form, valu))
-
     def test_addNodes(self):
         with self.getTestCore() as core:
             with core.snap() as snap:
@@ -94,43 +73,6 @@ class SnapTest(s_t_common.SynTest):
                 nodes = list(snap.getNodesBy('teststr', 'hehe'))
                 self.len(1, nodes)
                 self.eq(nodes[0], node)
-
-                with self.getTestDir() as dirn:
-                    with s_auth.Auth(dirn) as auth:
-                        user = auth.addUser('hatguy')
-                        user.addRule((True, ('node:add', 'teststr')))
-                        snap.setUser(user)
-
-                        ndefs = (
-                            (('teststr', 'haha'), {}, ),   # allowed
-                            (('testauto', 'hoho'), {}, ),  # not allowed
-                            (('teststr', 'huhu'), {}, ),   # allowed
-                        )
-
-                        # Demonstrate what happens when individual call to addNode fails during addNodes
-                        with self.raises(s_exc.AuthDeny):
-                            result = list(snap.addNodes(ndefs))
-
-                        self.nn(snap.getNodeByNdef(('teststr', 'haha')))
-                        self.none(snap.getNodeByNdef(('testauto', 'hoho')))
-                        # missing because call fails because in strict mode
-                        self.none(snap.getNodeByNdef(('teststr', 'huhu')))
-
-                        # Try again with strict mode disabled
-                        snap.strict = False
-                        ndefs = (
-                            (('teststr', 'foo'), {}, ),   # allowed
-                            (('testauto', 'bar'), {}, ),  # not allowed, still won't go through even if strict is false
-                            (('testtime', 'baz'), {}, ),   # invalid
-                            (('teststr', 'faz'), {}, ),   # allowed
-                        )
-
-                        result = list(snap.addNodes(ndefs))
-                        self.len(4, result)
-                        self.nn(snap.getNodeByNdef(('teststr', 'foo')))
-                        self.none(snap.getNodeByNdef(('testauto', 'bar')))
-                        self.none(snap.getNodeByNdef(('testtime', 'baz')))
-                        self.nn(snap.getNodeByNdef(('teststr', 'faz')))
 
     @contextlib.contextmanager
     def _getTestCoreMultiLayer(self, first_dirn):
