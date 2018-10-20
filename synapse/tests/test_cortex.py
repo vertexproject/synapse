1f20bbb87335499736be11568cb5ae60b5ae5e19
epiphyte <epiphyte@vertex.link>
2018-10-20 01:36:43 +0000

    Fix test_pivot_inout

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -1151,10 +1151,8 @@ class CortexTest(s_t_utils.SynTest):
             # Setup a propvalu pivot where the secondary prop may fail to norm
             # to the destination prop for some of the inbound nodes.
             await alist(core.eval('[ testcomp=(127,newp) ] [testcomp=(127,127)]'))
-            mesgs = await alist(core.storm('testcomp :haha -> testint'))
+            mesgs = await alist(core.streamstorm('testcomp :haha -> testint'))
 
-            # FIXME: warns are different now somehow
-            return
             warns = [msg for msg in mesgs if msg[0] == 'warn']
             self.len(1, warns)
             emesg = "BadTypeValu ['newp'] during pivot: invalid literal for int() with base 0: 'newp'"
@@ -1170,7 +1168,7 @@ class CortexTest(s_t_utils.SynTest):
                       'pivcomp :lulz <- teststr',
                       'pivcomp :lulz <+- teststr',
                       ]:
-                self.genraises(s_exc.BadStormSyntax, core.eval, q)
+                await self.agenraises(s_exc.BadStormSyntax, core.eval(q))
 
     async def test_node_repr(self):
 
