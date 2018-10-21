2eff9176d4ead9edc02b862ade6ee88ae0b9d82c
epiphyte <epiphyte@vertex.link>
2018-10-21 16:46:08 +0000

    Tear down the socket servers before plex fini - avoids a resourceWarning

--- synapse/tests/test_lib_plex.py
+++ synapse/tests/test_lib_plex.py
@@ -1,9 +1,13 @@
+import logging
 import functools
 import threading
+
 import synapse.glob as s_glob
 import synapse.lib.plex as s_plex
 
 import synapse.tests.utils as s_t_utils
+
+logger = logging.getLogger(__name__)
 
 class PlexTest(s_t_utils.SynTest):
     def test_plex_callLater(self):
@@ -54,6 +58,8 @@ class PlexTest(s_t_utils.SynTest):
         steps = self.getTestSteps(['onlink', 'onrx1', 'client_rx', 'link_fini'])
         with s_plex.Plex() as plex1, s_plex.Plex() as plex2:
 
+            servers = []
+
             async def server_onlink(link):
                 steps.done('onlink')
 
@@ -67,6 +73,7 @@ class PlexTest(s_t_utils.SynTest):
 
             async def do_listen():
                 server = await plex1.listen('127.0.0.1', None, onlink=server_onlink)
+                servers.append(server)
                 _, port = server.sockets[0].getsockname()
                 return port
 
@@ -87,3 +94,10 @@ class PlexTest(s_t_utils.SynTest):
             await steps.asyncwait('onrx1', timeout=1)
             await steps.asyncwait('client_rx', timeout=1)
             await steps.asyncwait('link_fini')
+
+            # Tear down the socket server
+            for server in servers:
+                try:
+                    server.close()
+                except:
+                    logger.exception('Error tearing down socket server.')
