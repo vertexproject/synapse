39c541ce623eb666d2fe81ed954d4496981c018b
epiphyte <epiphyte@vertex.link>
2018-11-20 22:18:15 +0000

    Add a csvtool --test test.

--- synapse/tests/test_tools_csvtool.py
+++ synapse/tests/test_tools_csvtool.py
@@ -47,3 +47,23 @@ class CsvToolTest(s_t_utils.SynTest):
                 await s_coro.executor(s_csvtool.main, argv, outp=outp)
 
                 outp.expect('2 nodes (9 created)')
+
+    async def test_csvtool_local(self):
+        with self.getTestDir() as dirn:
+
+            logpath = s_common.genpath(dirn, 'csvtest.log')
+
+            csvpath = s_common.genpath(dirn, 'csvtest.csv')
+            with s_common.genfile(csvpath) as fd:
+                fd.write(csvfile)
+
+            stormpath = s_common.genpath(dirn, 'csvtest.storm')
+            with s_common.genfile(stormpath) as fd:
+                fd.write(csvstorm)
+
+            argv = ['--csv-header', '--debug', '--test', '--logfile', logpath, stormpath, csvpath]
+            outp = self.getTestOutp()
+
+            await s_coro.executor(s_csvtool.main, argv, outp=outp)
+
+            outp.expect('2 nodes (9 created)')
