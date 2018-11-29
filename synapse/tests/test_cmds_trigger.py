5836a7cdaeb2349428f0fa33987f8184976ee1aa
Nic Watson <github@nicwatson.org>
2018-11-29 10:16:41 -0500

    Improve test case

--- synapse/tests/test_cmds_trigger.py
+++ synapse/tests/test_cmds_trigger.py
@@ -100,5 +100,6 @@ class CmdTriggersTest(s_t_utils.SynTest):
             self.true(outp.expect('BadStormSyntax'))
 
             # (Regression) Just a command as the storm query
-            await cmdr.runCmdLine('trigger add Node:add teststr { spin }')
-
+            await cmdr.runCmdLine('trigger add Node:add teststr {[ testint=99 ] | spin }')
+            await s_common.aspin(await core.eval('sudo | [ teststr=foo4 ]'))
+            await self.agenlen(1, await core.eval('testint=99'))
