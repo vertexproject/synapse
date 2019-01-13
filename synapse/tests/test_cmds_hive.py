7291d952fa8aef18d1ee1d0ce3da062ef89026ce
epiphyte <epiphyte@vertex.link>
2019-01-13 23:34:07 +0000

    Avoid possible outp cross contamination. Use setTstEnvars in Hive EDITOR test. Add VISUAL test.

--- synapse/tests/test_cmds_hive.py
+++ synapse/tests/test_cmds_hive.py
@@ -17,13 +17,16 @@ class CmdHiveTest(s_t_utils.SynTest):
                 await cmdr.runCmdLine('hive ls notadir')
                 self.true(outp.expect('Path not found'))
 
+                outp.clear()
                 await cmdr.runCmdLine('hive edit foo/bar [1,2,3,4]')
                 await cmdr.runCmdLine('hive ls')
                 self.true(outp.expect('foo'))
 
+                outp.clear()
                 await cmdr.runCmdLine('hive get foo/bar')
                 self.true(outp.expect('foo/bar: (1, 2, 3, 4)'))
 
+                outp.clear()
                 await cmdr.runCmdLine('hive ls foo')
                 self.true(outp.expect('bar'))
 
@@ -37,12 +40,18 @@ class CmdHiveTest(s_t_utils.SynTest):
                 with open(fn, 'w') as fh:
                     fh.write('{"foo": 123}')
 
+                outp.clear()
                 await cmdr.runCmdLine(f'hive edit foo/bar2 -f {fn}')
                 await cmdr.runCmdLine('hive get foo/bar2')
                 self.true(outp.expect("foo/bar2: {'foo': 123}"))
 
-                os.environ['EDITOR'] = 'echo [1,2,3] > '
-                await cmdr.runCmdLine(f'hive edit foo/bar3 --editor')
-                await cmdr.runCmdLine('hive get foo/bar3')
-                self.true(outp.expect('foo/bar3: (1, 2, 3)'))
+                outp.clear()
+                with self.setTstEnvars(EDITOR='echo [1,2,3] > ') as nop:
+                    await cmdr.runCmdLine(f'hive edit foo/bar3 --editor')
+                    await cmdr.runCmdLine('hive get foo/bar3')
+                    self.true(outp.expect('foo/bar3: (1, 2, 3)'))
 
+                with self.setTstEnvars(VISUAL='echo [1,2,3] > ') as nop:
+                    await cmdr.runCmdLine(f'hive edit foo/bar3 --editor')
+                    await cmdr.runCmdLine('hive get foo/bar3')
+                    self.true(outp.expect('foo/bar3: (1, 2, 3)'))
