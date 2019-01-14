4b283e42f55e7589a5fcf749635d40a5431c1f2d
epiphyte <epiphyte@vertex.link>
2019-01-14 15:56:23 +0000

    update min/max command examples to include full props; update docs to include fullprop examples.

--- synapse/lib/storm.py
+++ synapse/lib/storm.py
@@ -289,7 +289,9 @@ class MaxCmd(Cmd):
 
     Examples:
 
-        file:bytes +#foo.bar | max size
+        file:bytes +#foo.bar | max :size
+
+        file:bytes +#foo.bar | max file:bytes:size
 
     '''
 
@@ -344,7 +346,9 @@ class MinCmd(Cmd):
 
     Examples:
 
-        file:bytes +#foo.bar | min size
+        file:bytes +#foo.bar | min :size
+
+        file:bytes +#foo.bar | min file:bytes:size
 
     '''
     name = 'min'
