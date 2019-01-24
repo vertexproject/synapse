8248bd0f6dd1db915a07610e30bf96d8b30fb18f
rakuyo <james.gross@vertex.link>
2019-01-24 12:51:00 -0500

    add cleaned up pullfile and associated test

---
+++ synapse/tests/test_tools_pullfile.py
@@ -0,0 +1,52 @@
+import os
+import asyncio
+import hashlib
+
+import synapse.common as s_common
+
+import synapse.lib.scope as s_scope
+import synapse.tests.utils as s_t_utils
+
+import synapse.tools.pushfile as s_pushfile
+import synapse.tools.pullfile as s_pullfile
+
+
+class TestPullFile(s_t_utils.SynTest):
+
+    async def test_pullfile(self):
+
+        async with self.getTestDmonCortexAxon() as dmon:  # NOQA
+
+            coreurl = s_scope.get('coreurl')
+            axonurl = s_scope.get('axonurl')
+
+            def pullfile():
+
+                with self.getTestDir() as dirn:
+                    testpath = os.path.join(dirn, 'test.txt')
+                    visipath = os.path.join(dirn, 'visi.txt')
+                    with s_common.genfile(testpath) as fd:
+                        fd.write(b'test')
+                    with s_common.genfile(visipath) as fd:
+                        fd.write(b'visi')
+
+                    pushargs = ['-a', axonurl,
+                                '-c', coreurl]
+                    outp = self.getTestOutp()
+                    self.eq(0, s_pushfile.main(pushargs + [testpath], outp=outp))
+                    self.eq(0, s_pushfile.main(pushargs + [visipath], outp=outp))
+
+                    testhash = hashlib.sha256(b'test').hexdigest()
+                    visihash = hashlib.sha256(b'visi').hexdigest()
+                    nonehash = hashlib.sha256(b'none').hexdigest()
+                    pullargs = ['-a', axonurl,
+                                '-o', dirn,
+                                '-l', [testhash, visihash, nonehash]]
+                    outp = self.getTestOutp()
+                    self.eq(0, s_pullfile.main(pullargs, outp))
+                    self.true(outp.expect(f'b\'{nonehash}\' not in axon store'))
+                    self.true(outp.expect(f'Fetching {testhash} to file'))
+                    self.true(outp.expect(f'Fetching {visihash} to file'))
+
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(None, pullfile)
---
+++ synapse/tools/pullfile.py
@@ -0,0 +1,70 @@
+'''
+Fetches file details from a cortex and then pulls the associated file from the axon given
+'''
+
+import sys
+import argparse
+import binascii
+
+import synapse.exc as s_exc
+import synapse.glob as s_glob
+import synapse.common as s_common
+import synapse.telepath as s_telepath
+
+import synapse.lib.output as s_output
+
+
+def main(argv, outp=None):
+    pars = setup()
+    opts = pars.parse_args(argv)
+
+    if outp is None:
+        outp = s_output.OutPut()
+
+    if opts.output is None:
+        opts.output = '.'
+
+    s_common.gendir(opts.output)
+
+    axon = s_telepath.openurl(opts.axon)
+    if not axon:
+        raise s_exc.SynErr(mesg='Failed to connect to Axon')
+
+    # reminder: these are the hashes *not* available
+    awants = axon.wants([binascii.unhexlify(h) for h in opts.hashes])
+    for a in awants:
+        outp.printf(f'{binascii.hexlify(a)} not in axon store')
+
+    exists = [h for h in opts.hashes if binascii.unhexlify(h) not in awants]
+
+    for h in exists:
+
+        try:
+            outp.printf(f'Fetching {h} to file')
+            with open(f'{opts.output}/{h}', 'wb') as fd:
+                for b in axon.get(binascii.unhexlify(h)):
+                    fd.write(b)
+            outp.printf(f'Fetched {h} to file')
+        except Exception as e:
+            outp.printf('Error: Hit Exception: %s' % (str(e),))
+            continue
+
+    s_glob.sync(axon.fini())
+    return 0
+
+
+def setup():
+    desc = 'Looks up a file in a cortex and then fetches it from the given axon'
+    pars = argparse.ArgumentParser('pullfile', description=desc)
+    pars.add_argument('-a', '--axon', type=str, dest='axon', required=True,
+                      help='URL to the axon blob store')
+    pars.add_argument('-o', '--output', type=str, dest='output',
+                      help='Directory to output files to')
+    pars.add_argument('-l', '--list-hashes', dest='hashes',
+                      help='List of hashes to pull from axon')
+
+    return pars
+
+
+if __name__ == '__main__':  # pragma: no cover
+    sys.exit(main(sys.argv))
