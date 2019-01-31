ff535d9b0dce4029313c9b2b476228a5bbe24adb
epiphyte <epiphyte@vertex.link>
2019-01-31 02:44:40 +0000

    Add a uniqueness constraint to splice generation.

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -830,6 +830,10 @@ class CortexTest(s_t_utils.SynTest):
                 splice[1].pop('user', None)
                 splice[1].pop('time', None)
                 splices.append(splice)
+
+            # Ensure the splices are unique
+            self.len(len(splices), {s_msgpack.en(s) for s in splices})
+
             # Check to ensure a few expected splices exist
             mesg = ('node:add', {'ndef': ('teststr', 'hello')})
             self.isin(mesg, splices)
