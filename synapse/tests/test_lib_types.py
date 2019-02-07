3ed589a3ab7da7ef9db05586065bc527828e5dec
rakuyo <james.gross@vertex.link>
2019-02-07 13:05:18 -0500

    remove comment

--- synapse/tests/test_lib_types.py
+++ synapse/tests/test_lib_types.py
@@ -404,7 +404,6 @@ class TypesTest(s_t_utils.SynTest):
             await self.agenlen(1, core.eval('teststr:tick@=("now-1day", "?")'))
             await self.agenlen(1, core.eval('teststr:tick@=("now+2days", "-3 day")'))
 
-            # Calls into the range handler
             await self.agenlen(0, core.eval('teststr:tick@=("2011", "2014")'))
             await self.agenlen(1, core.eval('teststr:tick@=("2014", "20140601")'))
 
