63ec65a75ce58d4395f1973b3ef1d99a6ed2d336
epiphyte <epiphyte@vertex.link>
2019-02-13 15:24:37 +0000

    Change a message from info to debug

--- synapse/lib/hive.py
+++ synapse/lib/hive.py
@@ -626,7 +626,7 @@ class HiveAuth(s_base.Base):
         user = await HiveUser.anit(self, node)
 
         self.onfini(user)
-        logger.info(f'Adding user: {user.iden}/{user.name}')
+        logger.debug(f'Adding user: {user.iden}/{user.name}')
         self.usersbyiden[user.iden] = user
         self.usersbyname[user.name] = user
 
