859016205a26e0dbe34ca5c6ff13982aa53f7b74
visi <visi@vertex.link>
2019-02-19 10:59:09 -0500

    default self-signed ssl cert generation

--- synapse/servers/cortex.py
+++ synapse/servers/cortex.py
@@ -11,23 +11,17 @@ import synapse.common as s_common
 import synapse.cortex as s_cortex
 
 import synapse.lib.output as s_output
+import synapse.lib.certdir as s_certdir
 
 logger = logging.getLogger(__name__)
 
 def parse(argv):
 
-    httpport = os.getenv('SYN_CORTEX_HTTP_PORT')
-    httphost = os.getenv('SYN_CORTEX_HTTP_HOST', '0.0.0.0')
-
-    httpsport = os.getenv('SYN_CORTEX_HTTPS_PORT')
-    httpshost = os.getenv('SYN_CORTEX_HTTPS_HOST', socket.gethostname())
+    httpsport = os.getenv('SYN_CORTEX_HTTPS_PORT', '56443')
+    httpshost = os.getenv('SYN_CORTEX_HTTPS_HOST', '0.0.0.0')
 
     insecure = json.loads(os.getenv('SYN_CORTEX_INSECURE', 'false').lower())
 
-    teleport = os.getenv('SYN_CORTEX_PORT', '27492')
-    telehost = os.getenv('SYN_CORTEX_HOST', '127.0.0.1')
-
-    insecure = json.loads(os.getenv('SYN_CORTEX_INSECURE', 'false').lower())
     teleport = os.getenv('SYN_CORTEX_PORT', '27492')
     telehost = os.getenv('SYN_CORTEX_HOST', '127.0.0.1')
 
@@ -35,14 +29,12 @@ def parse(argv):
     pars.add_argument('--port', default=teleport, help='The TCP port to bind for telepath.')
     pars.add_argument('--host', default=telehost, help='The host address to bind telepath.')
 
-    pars.add_argument('--http-port', default=httpport, help='Set port to bind for the HTTP API.')
-    pars.add_argument('--http-host', default=httphost, help='Set the host/addr to bind for the HTTP API.')
-
     pars.add_argument('--https-host', default=httpshost, help='Set the host/addr to bind for the HTTPS API.')
     pars.add_argument('--https-port', default=httpsport, help='Set port to bind for the HTTPS API.')
 
     pars.add_argument('--insecure', default=insecure, action='store_true',
                       help='Start the cortex with all auth bypassed (DANGER!).')
+
     pars.add_argument('coredir', help='The directory for the cortex to use for storage.')
 
     return pars.parse_args(argv)
@@ -59,20 +51,34 @@ async def mainopts(opts, outp=s_output.stdout):
 
     lisn = f'tcp://{opts.host}:{opts.port}/cortex'
 
-    outp.printf('starting cortex at: %s' % (lisn,))
-
     core = await s_cortex.Cortex.anit(opts.coredir)
     core.insecure = opts.insecure
 
     if core.insecure:
         logger.warning('INSECURE MODE ENABLED')
 
-    await core.dmon.listen(lisn)
-
-    if opts.http_port:
-        await core.addHttpPort(int(opts.http_port), host=opts.http_host)
+    outp.printf('starting cortex at: %s' % (lisn,))
 
     await core.dmon.listen(lisn)
+
+    # check for certs or gen self signed
+    keypath = os.path.join(opts.coredir, 'sslkey.pem')
+    certpath = os.path.join(opts.coredir, 'sslcert.pem')
+
+    if not os.path.isfile(certpath):
+        logger.warning('NO CERTIFICATE FOUND! generating self-signed certificate.')
+        with s_common.getTempDir() as dirn:
+            cdir = s_certdir.CertDir(dirn)
+            pkey, cert = cdir.genHostCert('cortex')
+            cdir.savePkeyPem(pkey, keypath)
+            cdir.saveCertPem(cert, certpath)
+
+    outp.printf(f'loading SSL key from: {keypath}')
+    outp.printf(f'loading SSL cert from: {certpath}')
+
+    sslctx = core.initSslCtx(certpath, keypath)
+
+    await core.addHttpPort(int(opts.https_port), host=opts.https_host, ssl=sslctx)
 
     return core
 
