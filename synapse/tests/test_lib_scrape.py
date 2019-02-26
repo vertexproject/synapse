c6009b8e61cc30d62760a3c63a6b6d7f577c9171
epiphyte <epiphyte@vertex.link>
2019-02-26 18:26:39 +0000

    Re-anble synase.lib.scrape tests. Add a optinoal pytpe argument to scrape to limit the amount of work done on a blob of text.

--- synapse/tests/test_lib_scrape.py
+++ synapse/tests/test_lib_scrape.py
@@ -20,26 +20,26 @@ and BOB@WOOT.COM is another
 
 '''
 
-import unittest
-raise unittest.SkipTest('SHOULD WORK. REGEX DEBUGGING?')
-
 class ScrapeTest(s_t_utils.SynTest):
 
     def test_scrape(self):
 
-        nodes = dict(s_scrape.scrape(data0))
+        nodes = set(s_scrape.scrape(data0))
 
-        print(repr(nodes))
+        self.len(10, nodes)
+        nodes.remove(('hash:md5', 'a' * 32))
+        nodes.remove(('inet:ipv4', '1.2.3.4'))
+        nodes.remove(('inet:ipv4', '5.6.7.8'))
+        nodes.remove(('inet:fqdn', 'WOOT.COM'))
+        nodes.remove(('inet:fqdn', 'hehe.taxi'))
+        nodes.remove(('inet:fqdn', 'vertex.link'))
+        nodes.remove(('inet:server', '5.6.7.8:16'))
+        nodes.remove(('inet:email', 'BOB@WOOT.COM'))
+        nodes.remove(('inet:email', 'visi@vertex.link'))
+        self.len(0, nodes)
 
-        nodes.pop(('inet:email', 'visi@vertex.link'))
-
-        nodes.pop(('inet:email', 'BOB@WOOT.COM'))
-        nodes.pop(('inet:fqdn', 'hehe.taxi'))
-
-        nodes.pop(('inet:ipv4', 0x01020304))
-        nodes.pop(('inet:ipv4', 0x05060708))
-        nodes.pop(('inet:tcp4', 0x050607080010))
-
-        nodes.pop(('hash:md5', 'a' * 32))
-
+        nodes = set(s_scrape.scrape(data0, 'inet:email'))
+        self.len(2, nodes)
+        nodes.remove(('inet:email', 'visi@vertex.link'))
+        nodes.remove(('inet:email', 'BOB@WOOT.COM'))
         self.len(0, nodes)
