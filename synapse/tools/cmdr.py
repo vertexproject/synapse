f35075338f03d0491c9a6af92493353c589b1d04
visi <visi@vertex.link>
2019-03-16 20:49:50 -0400

    remove accidental copy pasta

--- synapse/tools/cmdr.py
+++ synapse/tools/cmdr.py
@@ -5,47 +5,18 @@ import synapse.glob as s_glob
 import synapse.telepath as s_telepath
 
 import synapse.lib.cmdr as s_cmdr
+import synapse.lib.output as s_output
 
 async def main(argv):  # pragma: no cover
 
-    if len(argv) != 2:
+    if len(argv) != 1:
         print('usage: python -m synapse.tools.cmdr <url>')
         return -1
 
-    async with s_telepath.openurl(argv[1]) as item:
+    async with await s_telepath.openurl(argv[0]) as item:
 
         cmdr = await s_cmdr.getItemCmdr(item)
-        cmdr.inithist = True
         await cmdr.runCmdLoop()
-
-async def main(argv, outp=s_output.stdout):
-
-    opts = parse(argv)
-
-    s_common.setlogging(logger)
-
-    lisn = f'tcp://{opts.host}:{opts.port}/cortex'
-
-    core = await s_cortex.Cortex.anit(opts.coredir)
-
-    try:
-
-        core.insecure = opts.insecure
-
-        if core.insecure:
-            logger.warning('INSECURE MODE ENABLED')
-
-        outp.printf('starting cortex at: %s' % (lisn,))
-
-        await core.dmon.listen(lisn)
-
-        await core.addHttpsPort(opts.https_port)
-
-        return core
-
-    except Exception as e:
-        await core.fini()
-        raise
 
 if __name__ == '__main__': # pragma: no cover
     asyncio.run(main(sys.argv[1:]))
