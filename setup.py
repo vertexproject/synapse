90d1672f0c812d74073dfcb23bd2688bb8f702c3
vEpiphyte <epiphyte@vertex.link>
2019-03-19 20:39:10 +0000

    Misc (#1165)
    * fixes
    
    * Remove old changelog.md
    
    * Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse',
-    version='0.1.0a10',
+    version='0.1.0',
     description='Synapse Intelligence Analysis Framework',
     author='The Vertex Project LLC',
     author_email='synapse@vertex.link',
