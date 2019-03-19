140e4fd313ec25d70242b6dae30764a77f828549
visi <visi@vertex.link>
2019-03-19 12:25:15 -0400

    docs ready for initial review

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synapse',
-    version='0.1.0',
+    version='0.1.0a10',
     description='Synapse Hypergraph Analysis Framework',
     author='The Vertex Project LLC',
     author_email='synapse@vertex.link',
