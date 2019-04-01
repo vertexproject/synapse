2b8e52b4ad971ebd40889e79fc421e6f541a0545
Nic Watson <github@nicwatson.org>
2019-04-01 16:42:02 -0400

    Remove debug

--- synapse/lib/base.py
+++ synapse/lib/base.py
@@ -32,7 +32,7 @@ def _fini_atexit(): # pragma: no cover
             continue
 
         if not item._fini_atexit:
-            if __debug__ and 0:
+            if __debug__:
                 print(f'At exit: Missing fini for {item}')
                 for depth, call in enumerate(item.call_stack[:-2]):
                     print(f'{depth+1:3}: {call.strip()}')
