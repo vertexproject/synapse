a98f86620e02248cfae69bc9781e0ddb958ddb4c
epiphyte <epiphyte@vertex.link>
2019-05-15 18:53:59 +0000

    Fix formatting

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -2809,8 +2809,7 @@ class CortexBasicTest(s_t_utils.SynTest):
 
                 self.len(1, await core01.eval('[ test:str=core01 ]').list())
                 # Add a lmdb layer with core00's iden
-                await core01.addLayer(iden=iden00,
-                                      )
+                await core01.addLayer(iden=iden00)
                 iden01 = core01.getCellIden()
                 # Set the default view for core01 to have a read layer with
                 # the iden from core00.
@@ -2821,8 +2820,7 @@ class CortexBasicTest(s_t_utils.SynTest):
             # Blow away the old layer at the destination and replace it
             # with our layer from core00
             shutil.rmtree(dst)
-            shutil.copytree(src, dst,
-                            )
+            shutil.copytree(src, dst)
 
             # Ensure data from both layers is present in the cortex
             async with self.getTestCore(dirn=path01) as core01:
