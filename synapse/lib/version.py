59ec72010725da13a58ff9fe855b1f9b4a9edbcc
Nic Watson <github@nicwatson.org>
2019-05-22 17:46:46 -0400

    Bump version: 0.1.7 → 0.1.8

--- synapse/lib/version.py
+++ synapse/lib/version.py
@@ -183,5 +183,5 @@ def parseVersionParts(text, seps=vseps):
 ##############################################################################
 # The following are touched during the release process by bumpversion.
 # Do not modify these directly.
-version = (0, 1, 7)
+version = (0, 1, 8)
 verstring = '.'.join([str(x) for x in version])
