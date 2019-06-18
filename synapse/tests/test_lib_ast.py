562cd1bb66368fe018282d040c23587d3f65aefd
epiphyte <epiphyte@vertex.link>
2019-06-18 14:22:30 +0000

    Update tests to show a second computed case

--- synapse/tests/test_lib_ast.py
+++ synapse/tests/test_lib_ast.py
@@ -7,11 +7,32 @@ class AstTest(s_test.SynTest):
 
         async with self.getTestCore() as core:
 
+            # Show a runtime variable being smashed by a subquery
+            # variable assignment
             q = '''
                 $loc=newp
                 [ test:comp=(10, lulz) ]
                 { -> test:int [ :loc=haha ] $loc=:loc }
                 $lib.print($loc)
             '''
-            msgs = [m async for m in core.streamstorm(q)]
-            self.printed(msgs, 'haha')
+            msgs = await core.streamstorm(q).list()
+            self.stormIsInPrint('haha', msgs)
+
+            # Show that a computed variable being smashed by a
+            # subquery variable assignment with multiple nodes
+            # traveling through a subquery.
+            async with await core.snap() as snap:
+                await snap.addNode('test:comp', (30, 'w00t'))
+                await snap.addNode('test:comp', (40, 'w00t'))
+                await snap.addNode('test:int', 30, {'loc': 'sol'})
+                await snap.addNode('test:int', 40, {'loc': 'mars'})
+
+            q = '''
+                test:comp:haha=w00t
+                { -> test:int $loc=:loc }
+                $lib.print($loc)
+                -test:comp
+            '''
+            msgs = await core.streamstorm(q).list()
+            self.stormIsInPrint('sol', msgs)
+            self.stormIsInPrint('mars', msgs)
