9a536ed1e278a3dc4a7f8d5b5aa0200e88a5d36a
epiphyte <epiphyte@vertex.link>
2019-07-11 15:18:49 +0000

    Bump version: 0.1.15 → 0.1.16

--- synapse/lib/version.py
+++ synapse/lib/version.py
@@ -183,5 +183,5 @@ def parseVersionParts(text, seps=vseps):
 ##############################################################################
 # The following are touched during the release process by bumpversion.
 # Do not modify these directly.
-version = (0, 1, 15)
+version = (0, 1, 16)
 verstring = '.'.join([str(x) for x in version])
