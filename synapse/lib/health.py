9668aed3ab9faf741b9bb7a49a6dc2dd4b72c11c
epiphyte <epiphyte@vertex.link>
2019-08-20 16:45:42 +0000

    WIP - make the components a list intead of a dictionary

--- synapse/lib/health.py
+++ synapse/lib/health.py
@@ -1,20 +1,18 @@
 import logging
-import typing
-
-import synapse.exc as s_exc
 
 logger = logging.getLogger(__name__)
+
 
 class HealthCheck(object):
     def __init__(self, iden: str):
         self.iden = iden
         self._healthy = True
-        self.components = {}
+        self.components = []
 
     def pack(self) -> tuple:
         ret = {'iden': self.iden,
-                'components': self.components,
-                'health': self.healthy,
+               'components': self.components,
+               'health': self.healthy,
                }
         return ret
 
@@ -22,21 +20,19 @@ class HealthCheck(object):
                name: str,
                status: bool,
                mesg: str = '',
-               data: dict =None,
+               data: dict = None,
                ) -> None:
-        if name in self.components:
-            raise s_exc.DataAlreadyExists(mesg='Already updated healthcheck for the component.',
-                                          name=name, status=status)
         # Allow the component a shot at reporting a failure state
         self.healthy = status
         if data is None:
             data = {}
         # record which component failed, any additional message they may have,
         # as well as any additional data they want to report on.
-        self.components[name] = {'status': status,
-                                 'mesg': mesg,
-                                 'data': data,
-                                 }
+        self.components.append({'status': status,
+                                'name': name,
+                                'mesg': mesg,
+                                'data': data,
+                                })
 
     @property
     def healthy(self) -> bool:
