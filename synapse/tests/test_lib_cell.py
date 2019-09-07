6f98d5027fa57180bbeabdea48d73c715bc6420c
epiphyte <epiphyte@vertex.link>
2019-09-07 20:20:14 +0000

    setCellUser test for daemon sessions

--- synapse/tests/test_lib_cell.py
+++ synapse/tests/test_lib_cell.py
@@ -52,6 +52,7 @@ class CellTest(s_t_utils.SynTest):
                     self.true(await proxy.isadmin())
                     self.true(await proxy.allowed('hehe', 'haha'))
 
+                    # Auth data is reflected in the Dmon session
                     resp = await proxy.getDmonSessions()
                     self.len(1, resp)
                     info = resp[0]
@@ -238,6 +239,9 @@ class CellTest(s_t_utils.SynTest):
                 async with cell.getLocalProxy() as prox:
 
                     self.eq('root', (await prox.getCellUser())['name'])
+                    snfo = await prox.getDmonSessions()
+                    self.len(1, snfo)
+                    self.eq(snfo[0].get('user').get('name'), 'root')
 
                     with self.raises(s_exc.NoSuchUser):
                         await prox.setCellUser(s_common.guid())
@@ -246,6 +250,12 @@ class CellTest(s_t_utils.SynTest):
 
                     self.true(await prox.setCellUser(user['iden']))
                     self.eq('visi', (await prox.getCellUser())['name'])
+
+                    # setCellUser propagates his change to the Daemon Sess object.
+                    # But we have to use the daemon directly to get that info
+                    snfo = await cell.dmon.getSessInfo()
+                    self.len(1, snfo)
+                    self.eq(snfo[0].get('user').get('name'), 'visi')
 
                     with self.raises(s_exc.AuthDeny):
                         await prox.setCellUser(s_common.guid())
