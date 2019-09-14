f2368257c397d56a7222f8d3f86c1c83b6073969
Nic Watson <github@nicwatson.org>
2019-09-14 13:36:46 -0400

    Formatting

--- synapse/lib/lmdblayer.py
+++ synapse/lib/lmdblayer.py
@@ -73,7 +73,8 @@ class LmdbLayer(s_layer.Layer):
         self.onfini(self.layrslab.fini)
 
         self.spliceslab = await s_lmdbslab.Slab.anit(splicepath, max_dbs=128, map_size=mapsize, maxsize=maxsize,
-                                                     growsize=growsize, writemap=True, readahead=readahead, map_async=map_async)
+                                                     growsize=growsize, writemap=True, readahead=readahead,
+                                                     map_async=map_async)
         self.onfini(self.spliceslab.fini)
 
         self.dataslab = await s_lmdbslab.Slab.anit(datapath, map_async=True)
@@ -603,7 +604,7 @@ class LmdbLayer(s_layer.Layer):
                     yield (buid,)
                 return
 
-            #pragma: no cover
+            # pragma: no cover
             mesg = f'No such index function for tag props: {iopr[0]}'
             raise s_exc.NoSuchName(name=iopr[0], mesg=mesg)
 
