ab7619f5960ea343fed67b8655b3f769c4cf5695
invisig0th <visi@vertex.link>
2019-10-08 12:29:06 -0400

    model extension for X.509 details (#1374)
    *Iniital X509 model
    
    *update $node.repr()

--- synapse/models/gov/intl.py
+++ synapse/models/gov/intl.py
@@ -5,7 +5,20 @@ class GovIntlModule(s_module.CoreModule):
     def getModelDefs(self):
         modl = {
             'types': (
-                ('gov:intl:un:m49', ('int', {'min': 1, 'max': 999}), {'doc': 'UN M49 Numeric Country Code'}),
+                ('iso:oid', ('str', {'regex': '^([0-2])((\.0)|(\.[1-9][0-9]*))*$'}), {
+                    'doc': 'An ISO Object Identifier string.'}),
+
+                ('gov:intl:un:m49', ('int', {'min': 1, 'max': 999}), {
+                    'doc': 'UN M49 Numeric Country Code'}),
+            ),
+
+            'forms': (
+                ('iso:oid', {}, (
+                    ('descr', ('str', {}), {
+                        'doc': 'A description of the value or meaning of the OID.'}),
+                    ('identifier', ('str', {}), {
+                        'doc': 'The string identifier for the deepest tree element.'}),
+                )),
             ),
         }
         name = 'gov:intl'
