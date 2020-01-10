77553854cb808e2716c67a0d199d01a734b91811
epiphyte <epiphyte@vertex.link>
2020-01-10 21:36:27 +0000

    Add dmon view test

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -3764,6 +3764,41 @@ class CortexBasicTest(s_t_utils.SynTest):
             with self.raises(s_exc.NoSuchUser):
                 await core.runStormDmon(iden, {'user': s_common.guid()})
 
+    async def test_cortex_storm_dmon_view(self):
+
+        async with self.getTestCore() as core:
+            self.len(1, await core.nodes('[test:int=1]'))
+            await core.nodes('$q=$lib.queue.add(dmon)')
+            view2 = await core.view.fork()
+            view2_iden = view2.iden
+
+            async with core.getLocalProxy() as prox:
+                q = '''
+                $lib.dmon.add(${
+                    $q = $lib.queue.get(dmon)
+                     for ($offs, $item) in $q.gets(size=3, wait=12)
+                        {
+                            [ test:int=$item ]
+                            $lib.print("made {ndef}", ndef=$node.ndef())
+                            $q.cull($offs)
+                        }
+                    }, name=viewdmon)
+                '''
+                # Iden is captured from the current snap
+                await core.nodes(q, opts={'view': view2_iden})
+                await asyncio.sleep(0)
+
+                q = '''$q = $lib.queue.get(dmon) $q.puts((1, 3, 5))'''
+                with self.getAsyncLoggerStream('synapse.lib.storm',
+                                               "made ('test:int', 5)") as stream:
+                    await core.nodes(q)
+                    self.true(await stream.wait(6))
+
+                nodes = await core.nodes('test:int', opts={'view': view2_iden})
+                self.len(3, nodes)
+                nodes = await core.nodes('test:int')
+                self.len(1, nodes)
+
     async def test_cortex_storm_cmd_bads(self):
 
         async with self.getTestCore() as core:
