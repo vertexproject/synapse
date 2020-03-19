19038dce87a1c9396f2be77854cf022c0a0e578f
cisphyx <cisphyx@vertex.link>
2020-03-19 10:59:31 -0400

    adjust scanback iternext set_range_dup handling

--- synapse/lib/lmdbslab.py
+++ synapse/lib/lmdbslab.py
@@ -1331,10 +1331,7 @@ class ScanBack(Scan):
                     if self.dupsort:
                         ret = self.curs.set_range_dup(*self.atitem)
                         if ret is False:
-                            if not self.curs.set_key(self.atitem[0]):
-                                raise StopIteration
-
-                            if not self.curs.last_dup():
+                            if not self.curs.last():
                                 raise StopIteration
                     else:
                         ret = self.curs.set_range(self.atitem[0])
