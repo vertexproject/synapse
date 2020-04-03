04f50034c406d0ac26027915c1638d26272f8964
epiphyte <epiphyte@vertex.link>
2020-04-03 13:50:57 +0000

    Docstring on the Spooled base, support for __len__ on spooled set

--- synapse/lib/spooled.py
+++ synapse/lib/spooled.py
@@ -2,10 +2,17 @@ import shutil
 import tempfile
 
 import synapse.lib.base as s_base
+import synapse.lib.const as s_const
 import synapse.lib.msgpack as s_msgpack
 import synapse.lib.lmdbslab as s_lmdbslab
 
 class Spooled(s_base.Base):
+    '''
+    A Base class that can be used to implement objects which fallback to lmdb.
+
+    These objects are intended to fallback from Python to lmbd slabs, which alligns them
+    together. Under memory pressure, these objects have a better shot of getting paged out.
+    '''
 
     async def __anit__(self, size=10000):
 
@@ -29,25 +36,34 @@ class Spooled(s_base.Base):
     async def _initFallBack(self):
         self.fallback = True
         self.slabpath = tempfile.mkdtemp()
-        self.slab = await s_lmdbslab.Slab.anit(self.slabpath)
+        self.slab = await s_lmdbslab.Slab.anit(self.slabpath,
+                                               map_size=s_const.mebibyte * 32)
 
 class Set(Spooled):
     '''
     A minimal set-like implementation that will spool to a slab on large growth.
     '''
+
     async def __anit__(self, size=10000):
         await Spooled.__anit__(self, size=size)
         self.realset = set()
+        self.len = 0
 
     def __contains__(self, valu):
         if self.fallback:
             return self.slab.get(s_msgpack.en(valu)) is not None
         return valu in self.realset
 
+    def __len__(self):
+        if self.fallback:
+            return self.len
+        return len(self.realset)
+
     async def add(self, valu):
 
         if self.fallback:
             self.slab.put(s_msgpack.en(valu), b'\x01')
+            self.len += 1
             return
 
         self.realset.add(valu)
@@ -55,12 +71,16 @@ class Set(Spooled):
         if len(self.realset) >= self.size:
             await self._initFallBack()
             [self.slab.put(s_msgpack.en(valu), b'\x01') for valu in self.realset]
+            self.len = len(self.realset)
             self.realset.clear()
 
     def discard(self, valu):
 
         if self.fallback:
-            self.slab.pop(s_msgpack.en(valu))
+            ret = self.slab.pop(s_msgpack.en(valu))
+            if ret is None:
+                return
+            self.len -= 1
             return
 
         self.realset.discard(valu)
--- synapse/tests/test_lib_spooled.py
+++ synapse/tests/test_lib_spooled.py
@@ -9,19 +9,25 @@ class SpooledTest(s_test.SynTest):
     async def test_spooled_set(self):
 
         sset = await s_spooled.Set.anit(size=2)
+        self.len(0, sset)
 
         await sset.add(10)
         self.true(10 in sset)
+        self.len(1, sset)
         sset.discard(10)
         self.false(10 in sset)
 
+        self.len(0, sset)
         self.false(sset.fallback)
 
         await sset.add(10)
+
+        # Trigger fallback
         await sset.add(20)
         await sset.add(30)
-
         await sset.add(None)
+
+        self.len(4, sset)
 
         self.nn(sset.slab)
         self.nn(sset.slabpath)
@@ -30,6 +36,10 @@ class SpooledTest(s_test.SynTest):
         self.true(10 in sset)
         sset.discard(10)
         self.false(10 in sset)
+        self.len(3, sset)
+
+        sset.discard(10)
+        self.len(3, sset)
 
         self.true(None in sset)
 
