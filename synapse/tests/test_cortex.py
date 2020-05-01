6795648fc87d4c3e947092b65ea1b99ab12d268e
vEpiphyte <epiphyte@vertex.link>
2020-05-01 12:09:53 +0000

    Use the registered cellapi as opposed to hardcoding CoreApi (#1708)
    * Use the registered cellapi as opposed to hardcoding CoreApi
    * asyncify some Layer apis

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -193,7 +193,7 @@ class CortexTest(s_t_utils.SynTest):
                 (s_layer.EDIT_EDGE_DEL, (), ()),
             ))]
 
-            self.eq((), list(core.view.layers[0].makeSplices(0, nodeedits, {})))
+            self.eq((), await alist(core.view.layers[0].makeSplices(0, nodeedits, {})))
 
             # Run multiple nodes through edge creation/deletion ( test coverage for perm caching )
             await core.nodes('inet:ipv4 [ <(test)+ { meta:source:name=test }]')
