3881dc81e93bfa801022b126d5bc168d2f5128a8
epiphyte <epiphyte@vertex.link>
2020-05-11 15:13:29 +0000

    Allow for custom nodeeditlog stores

--- synapse/lib/layer.py
+++ synapse/lib/layer.py
@@ -869,6 +869,8 @@ class Layer(s_nexus.Pusher):
     '''
     The base class for a cortex layer.
     '''
+    nodeeditctor = s_slabseqn.SlabSeqn
+
     def __repr__(self):
         return f'Layer ({self.__class__.__name__}): {self.iden}'
 
@@ -1024,7 +1026,7 @@ class Layer(s_nexus.Pusher):
 
         self.nodeeditlog = None
         if self.logedits:
-            self.nodeeditlog = s_slabseqn.SlabSeqn(self.nodeeditslab, 'nodeedits')
+            self.nodeeditlog = self.nodeeditctor(self.nodeeditslab, 'nodeedits')
 
     def getSpawnInfo(self):
         info = self.pack()
