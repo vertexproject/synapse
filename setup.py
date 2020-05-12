8e2612f25dbfe61f2fd44d9dbe4415aa1fba805f
epiphyte <epiphyte@vertex.link>
2020-05-12 12:35:27 +0000

    Bump for rc15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '0.2.0rc14'
+VERSION = '0.2.0rc15'
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
