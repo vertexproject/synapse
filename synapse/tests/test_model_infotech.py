f6e45f71c1e093f9e1ef45d2befb9c0cec607e07
chompie-vertex <59570216+chompie-vertex@users.noreply.github.com>
2020-07-14 14:49:40 -0500

    Infotech reveng model additions (#1802)
    added reverse engineering elements to the model

--- synapse/tests/test_model_infotech.py
+++ synapse/tests/test_model_infotech.py
@@ -713,28 +713,50 @@ class InfotechModelTest(s_t_utils.SynTest):
         async with self.getTestCore() as core:
 
             baseFile = s_common.ehex(s_common.buid())
+            func = s_common.guid()
             fva = 0x404438
+            rank = 33
+            complexity = 60
+            funccalls = ((baseFile, func), )
             fopt = {'vars': {'file': baseFile,
-                             'func': s_common.guid(),
-                             'fva': fva}}
+                             'func': func,
+                             'fva': fva,
+                             'rank': rank,
+                             'cmplx': complexity,
+                             'funccalls': funccalls}}
             vstr = 'VertexBrandArtisanalBinaries'
-            sopt = {'vars': {'func': fopt['vars']['func'],
+            sopt = {'vars': {'func': func,
                              'string': vstr}}
-            fnode = await core.nodes('[it:reveng:filefunc=($file, $func) :va=$fva]', opts=fopt)
+            name = "FunkyFunction"
+            descrp = "Test Function"
+            impcalls = ("libr.foo", "libr.foo2", "libr.foo3")
+            funcopt = {'vars': {'name': name,
+                                'descrp': descrp,
+                                'impcalls': impcalls}}
+
+            fnode = await core.nodes('[it:reveng:filefunc=($file, $func) :va=$fva :rank=$rank :complexity=$cmplx :funccalls=$funccalls]', opts=fopt)
             snode = await core.nodes('[it:reveng:funcstr=($func, $string)]', opts=sopt)
             self.len(1, fnode)
             self.eq(f'sha256:{baseFile}', fnode[0].get('file'))
             self.eq(fva, fnode[0].get('va'))
+            self.eq(rank, fnode[0].get('rank'))
+            self.eq(complexity, fnode[0].get('complexity'))
+            self.eq((f'sha256:{baseFile}', func), fnode[0].get('funccalls')[0])
 
             self.len(1, snode)
             self.eq(fnode[0].get('function'), snode[0].get('function'))
             self.eq(vstr, snode[0].get('string'))
 
-            funcnode = await core.nodes('it:reveng:function [ :name="FunkyFunction" :description="Test Function" ]')
+            funcnode = await core.nodes('it:reveng:function [ :name=$name :description=$descrp :impcalls=$impcalls ]', opts=funcopt)
             self.len(1, funcnode)
-            self.eq("FunkyFunction", funcnode[0].get('name'))
-            self.eq("Test Function", funcnode[0].get('description'))
+            self.eq(name, funcnode[0].get('name'))
+            self.eq(descrp, funcnode[0].get('description'))
+            self.len(len(impcalls), funcnode[0].get('impcalls'))
+            self.eq(impcalls[0], funcnode[0].get('impcalls')[0])
 
             nodes = await core.nodes(f'file:bytes={baseFile} -> it:reveng:filefunc :function -> it:reveng:funcstr:function')
             self.len(1, nodes)
             self.eq(vstr, nodes[0].get('string'))
+
+            nodes = await core.nodes(f'file:bytes={baseFile} -> it:reveng:filefunc -> it:reveng:function -> it:reveng:impfunc')
+            self.len(len(impcalls), nodes)
