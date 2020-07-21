63df3bf82dac16a1e55789a923743242f75e21fd
epiphyte <epiphyte@vertex.link>
2020-07-21 14:52:25 +0000

    opps

--- docker/images/axon/entrypoint.sh
+++ docker/images/axon/entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 python -O -m synapse.servers.axon /vertex/storage
-
-HEALTHCHECK --start-period=10s --retries=1 --timeout=10s --interval=30s CMD python -m synapse.tools.healthcheck -c cell:///vertex/storage/
