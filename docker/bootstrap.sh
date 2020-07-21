a6bbf3499924d105f3e5023150e5ee3ff74940f2
epiphyte <epiphyte@vertex.link>
2020-07-21 14:40:18 +0000

    Fix the bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd /build/synapse && python setup.py install
-#if [ -d /build/synapse ]; then
-#    cd /build/synapse
-#    python -m pip -v install .
-#fi
+if [ -d /build/synapse ]; then
+    cd /build/synapse
+    python setup.py build bdist_wheel
+    cd dist
+    python -m pip install *.whl
+fi
 
 rm -rf /build
