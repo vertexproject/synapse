3d82ac6d86924d284b8159e5e722ce53a00a0e17
epiphyte <epiphyte@vertex.link>
2020-07-21 14:51:43 +0000

    Add missing healthchecks

--- docker/images/axon/entrypoint.sh
+++ docker/images/axon/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 python -O -m synapse.servers.axon /vertex/storage
+
+HEALTHCHECK --start-period=10s --retries=1 --timeout=10s --interval=30s CMD python -m synapse.tools.healthcheck -c cell:///vertex/storage/
