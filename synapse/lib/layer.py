610bc8a95bd8eaaa206fd8e05dc711a516413fff
mikemoritz <57907149+mikemoritz@users.noreply.github.com>
2020-08-11 11:46:48 -0700

    Use meta to set created prop on node add (#1765)
    use metadata to set created property to avoid mirror divergence

--- synapse/lib/layer.py
+++ synapse/lib/layer.py
@@ -1469,7 +1469,7 @@ class Layer(s_nexus.Pusher):
         results = []
         nexsindx = nexsitem[0]
 
-        for edits in [await self._storNodeEdit(e) for e in nodeedits]:
+        for edits in [await self._storNodeEdit(e, meta) for e in nodeedits]:
             results.extend(edits)
 
         if self.logedits:
@@ -1483,12 +1483,13 @@ class Layer(s_nexus.Pusher):
 
         return results
 
-    async def _storNodeEdit(self, nodeedit):
+    async def _storNodeEdit(self, nodeedit, meta):
         '''
         Execute a series of storage operations for the given node.
 
         Args:
             nodeedit
+            meta
 
         Returns a list of flattened NodeEdits (Tuple[BuidT, str, Sequence]), with top-level and subedits all at the
         top level.  The 3rd element of each item contains only the applied changes.
@@ -1507,9 +1508,21 @@ class Layer(s_nexus.Pusher):
             postedits.extend(changes)
 
             if changes:
+                if any((c[0] == EDIT_NODE_ADD for c in changes)):
+                    if sode is not None:
+                        sode['ndef'] = (form, edit[1][0])
+
+                    if meta is not None:
+                        ctime = meta.get('time')
+                    else:
+                        ctime = s_common.now()
+
+                    created = (EDIT_PROP_SET, ('.created', ctime, None, STOR_TYPE_MINTIME))
+                    postedits.extend(self._editPropSet(buid, form, created, sode))
+
                 subedits = edit[2]
                 for ne in subedits:
-                    subpostnodeedits = await self._storNodeEdit(ne)
+                    subpostnodeedits = await self._storNodeEdit(ne, meta)
                     # Consolidate any changes from subedits to the main edit node into that nodeedit's
                     # edits, otherwise just append an entire nodeedit
                     for subpostnodeedit in subpostnodeedits:
@@ -1567,12 +1580,6 @@ class Layer(s_nexus.Pusher):
         self.formcounts.inc(form)
 
         retn = [(EDIT_NODE_ADD, (valu, stortype), ())]
-
-        if sode is not None:
-            sode['ndef'] = (form, valu)
-
-        created = (EDIT_PROP_SET, ('.created', s_common.now(), None, STOR_TYPE_MINTIME))
-        retn.extend(self._editPropSet(buid, form, created, sode))
 
         return retn
 
