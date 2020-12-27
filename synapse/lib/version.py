0325f203ffc162424a59a4dd0d4b0974f8b49296
visi <visi@vertex.link>
2020-12-27 13:45:45 -0500

    Bump version: 2.18.1 → 2.19.0

--- synapse/lib/version.py
+++ synapse/lib/version.py
@@ -216,5 +216,5 @@ def reqVersion(valu, reqver,
 ##############################################################################
 # The following are touched during the release process by bumpversion.
 # Do not modify these directly.
-version = (2, 18, 1)
+version = (2, 19, 0)
 verstring = '.'.join([str(x) for x in version])
