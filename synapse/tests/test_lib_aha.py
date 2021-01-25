3b98b8d77db7d9153f0a5220d0e69834a87d03c4
cisphyx <cisphyx@vertex.link>
2021-01-25 15:38:58 -0500

    fix test

--- synapse/tests/test_lib_aha.py
+++ synapse/tests/test_lib_aha.py
@@ -187,26 +187,26 @@ class AhaTest(s_test.SynTest):
 
         with self.getTestDir() as dirn:
 
-            conf = {
-                'version': 1,
-                'aha:servers': [
-                    'tcp://root:hehehaha@localhost:9999/',
-                ],
-            }
-
-            path = s_common.genpath(dirn, 'telepath.yaml')
-            s_common.yamlsave(conf, path)
-
-            fini = await s_telepath.loadTeleEnv(path)
-
-            # Should be one uninitialized aha client
-            self.len(1, s_telepath.aha_clients)
-            [info] = s_telepath.aha_clients.values()
-            self.none(info.get('client'))
-
             async with self.getTestAha() as aha:
-                host, port = await aha.dmon.listen('tcp://localhost:9999')
+                host, port = await aha.dmon.listen('tcp://127.0.0.1:0')
                 await aha.auth.rootuser.setPasswd('hehehaha')
+
+                conf = {
+                    'version': 1,
+                    'aha:servers': [
+                        f'tcp://root:hehehaha@127.0.0.1:{port}/',
+                    ],
+                }
+
+                path = s_common.genpath(dirn, 'telepath.yaml')
+                s_common.yamlsave(conf, path)
+
+                fini = await s_telepath.loadTeleEnv(path)
+
+                # Should be one uninitialized aha client
+                self.len(1, s_telepath.aha_clients)
+                [info] = s_telepath.aha_clients.values()
+                self.none(info.get('client'))
 
                 with self.raises(s_exc.NoSuchName):
                     await s_telepath.openurl('aha://visi@foo.bar.com')
