b6f528a17346cbbecdbb27229d9dea0cdf6a3293
epiphyte <epiphyte@vertex.link>
2021-01-25 15:39:03 +0000

    Minimize up the stormhttpapi docs

--- synapse/lib/stormhttp.py
+++ synapse/lib/stormhttp.py
@@ -15,146 +15,53 @@ class LibHttp(s_stormtypes.Lib):
     A Storm Library exposing an HTTP client API.
     '''
     _storm_locals = (
-        {
-            'name': 'get',
-            'desc': 'Get the contents of a given URL.',
-            'type': {
-                'type': 'function',
-                '_funcname': '_httpEasyGet',
-                'args': (
-                    {
-                        'name': 'url',
-                        'type': 'str',
-                        'desc': 'The URL to retrieve.',
-                    },
-                    {
-                        'name': 'headers',
-                        'type': 'dict',
-                        'desc': 'HTTP headers to send with the request.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'ssl_verify',
-                        'type': 'boolean',
-                        'desc': 'Perform SSL/TLS verification.',
-                        'default': True,
-                    },
-                    {
-                        'name': 'params',
-                        'type': 'dict',
-                        'desc': 'Optional parameters which may be passed to the request.',
-                        'default': None,
-                    },
-                ),
-                'returns': {
-                    'type': 'storm:http:resp',
-                    'desc': 'The response object.',
-                }
-            }
-        },
-        {
-            'name': 'post',
-            'desc': 'Post data to a given URL.',
-            'type': {
-                'type': 'function',
-                '_funcname': '_httpPost',
-                'args': (
-                    {
-                        'name': 'url',
-                        'type': 'str',
-                        'desc': 'The URL to post to.',
-                    },
-                    {
-                        'name': 'headers',
-                        'type': 'dict',
-                        'desc': 'HTTP headers to send with the request.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'json',
-                        'type': 'prim',
-                        'desc': 'The data to post, as JSON object.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'body',
-                        'type': 'bytes',
-                        'desc': 'The data to post, as binary object.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'ssl_verify',
-                        'type': 'boolean',
-                        'desc': 'Perform SSL/TLS verification.',
-                        'default': True,
-                    },
-                    {
-                        'name': 'params',
-                        'type': 'dict',
-                        'desc': 'Optional parameters which may be passed to the request.',
-                        'default': None,
-                    },
-                ),
-                'returns': {
-                    'type': 'storm:http:resp',
-                    'desc': 'The response object.',
-                }
-            }
-        },
-        {
-            'name': 'request',
-            'desc': 'Make an HTTP request using the given HTTP method to the url.',
-            'type': {
-                'type': 'function',
-                '_funcname': '_httpRequest',
-                'args': (
-                    {
-                        'name': 'meth',
-                        'type': 'str',
-                        'desc': 'The HTTP method. (ex. PUT)',
-                    },
-                    {
-                        'name': 'url',
-                        'type': 'str',
-                        'desc': 'The URL to send the request to.',
-                    },
-                    {
-                        'name': 'headers',
-                        'type': 'dict',
-                        'desc': 'HTTP headers to send with the request.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'json',
-                        'type': 'prim',
-                        'desc': 'The data to include in the body, as JSON object.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'body',
-                        'type': 'bytes',
-                        'desc': 'The data to include in the body, as binary object.',
-                        'default': None,
-                    },
-                    {
-                        'name': 'ssl_verify',
-                        'type': 'boolean',
-                        'desc': 'Perform SSL/TLS verification.',
-                        'default': True,
-                    },
-                    {
-                        'name': 'params',
-                        'type': 'dict',
-                        'desc': 'Optional parameters which may be passed to the request.',
-                        'default': None,
-                    },
-                ),
-                'returns': {
-                    'type': 'storm:http:resp',
-                    'desc': 'The response object.',
-                }
-            }
-        },
+        {'name': 'get', 'desc': 'Get the contents of a given URL.',
+         'type': {'type': 'function', '_funcname': '_httpEasyGet',
+                  'args': (
+                      {'name': 'url', 'type': 'str', 'desc': 'The URL to retrieve.', },
+                      {'name': 'headers', 'type': 'dict', 'desc': 'HTTP headers to send with the request.',
+                       'default': None, },
+                      {'name': 'ssl_verify', 'type': 'boolean', 'desc': 'Perform SSL/TLS verification.',
+                       'default': True, },
+                      {'name': 'params', 'type': 'dict', 'desc': 'Optional parameters which may be passed to the request.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'storm:http:resp', 'desc': 'The response object.', }}},
+        {'name': 'post', 'desc': 'Post data to a given URL.',
+         'type': {'type': 'function', '_funcname': '_httpPost',
+                  'args': (
+                      {'name': 'url', 'type': 'str', 'desc': 'The URL to post to.', },
+                      {'name': 'headers', 'type': 'dict', 'desc': 'HTTP headers to send with the request.',
+                       'default': None, },
+                      {'name': 'json', 'type': 'prim', 'desc': 'The data to post, as JSON object.',
+                       'default': None, },
+                      {'name': 'body', 'type': 'bytes', 'desc': 'The data to post, as binary object.',
+                       'default': None, },
+                      {'name': 'ssl_verify', 'type': 'boolean', 'desc': 'Perform SSL/TLS verification.',
+                       'default': True, },
+                      {'name': 'params', 'type': 'dict', 'desc': 'Optional parameters which may be passed to the request.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'storm:http:resp', 'desc': 'The response object.', }}},
+        {'name': 'request', 'desc': 'Make an HTTP request using the given HTTP method to the url.',
+         'type': {'type': 'function', '_funcname': '_httpRequest',
+                   'args': (
+                      {'name': 'meth', 'type': 'str', 'desc': 'The HTTP method. (ex. PUT)', },
+                      {'name': 'url', 'type': 'str', 'desc': 'The URL to send the request to.', },
+                      {'name': 'headers', 'type': 'dict', 'desc': 'HTTP headers to send with the request.',
+                       'default': None, },
+                      {'name': 'json', 'type': 'prim', 'desc': 'The data to include in the body, as JSON object.',
+                       'default': None, },
+                      {'name': 'body', 'type': 'bytes', 'desc': 'The data to include in the body, as binary object.',
+                       'default': None, },
+                      {'name': 'ssl_verify', 'type': 'boolean', 'desc': 'Perform SSL/TLS verification.',
+                       'default': True, },
+                      {'name': 'params', 'type': 'dict', 'desc': 'Optional parameters which may be passed to the request.',
+                       'default': None, },
+                   ),
+                  'returns': {'type': 'storm:http:resp', 'desc': 'The response object.', }
+                  }
+         },
     )
     _storm_lib_path = ('inet', 'http')
 
