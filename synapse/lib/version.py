eda9e66f84b104b5f572db42d25a2a2738ee1d82
epiphyte <epiphyte@vertex.link>
2021-02-05 01:57:52 +0000

    Bump version: 2.25.0 → 2.26.0

--- synapse/lib/version.py
+++ synapse/lib/version.py
@@ -216,5 +216,5 @@ def reqVersion(valu, reqver,
 ##############################################################################
 # The following are touched during the release process by bumpversion.
 # Do not modify these directly.
-version = (2, 25, 0)
+version = (2, 26, 0)
 verstring = '.'.join([str(x) for x in version])
