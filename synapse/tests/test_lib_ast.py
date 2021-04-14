b34276e751f496224833590f5aec4032841514b4
Nic Watson <github@nicwatson.org>
2021-04-14 10:06:33 -0400

    Remove duplicate test

--- synapse/tests/test_lib_ast.py
+++ synapse/tests/test_lib_ast.py
@@ -1377,19 +1377,8 @@ class AstTest(s_test.SynTest):
             erfo = [m for m in msgs if m[0] == 'err'][0]
             self.eq(erfo[1][0], 'StormRuntimeError')
 
-            # Can't use a mutable variable as a default
-            q = '$var=$lib.list(1,2,3) function badargs(x=foo, y=$var) {} $badargs()'
-            msgs = await core.stormlist(q)
-            erfo = [m for m in msgs if m[0] == 'err'][0]
-            self.eq(erfo[1][0], 'StormRuntimeError')
-
             # libs are not mutable:  OK to have as default parameter
             self.len(0, await core.nodes('$val=$lib function x(parm1=$val) { return($parm1) }'))
-
-            q = '$var=(1, 2, 3) function badargs(x=foo, y=$var) {} $badargs()'
-            msgs = await core.stormlist(q)
-            erfo = [m for m in msgs if m[0] == 'err'][0]
-            self.eq(erfo[1][0], 'StormRuntimeError')
 
             # bytes are OK
             q = '$val=$lib.base64.decode("dmlzaQ==") function x(parm1=$val) { return($parm1) }'
