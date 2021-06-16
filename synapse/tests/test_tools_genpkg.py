b2a2fa155fd46380470d0d17e1816627264da3e4
epiphyte <epiphyte@vertex.link>
2021-06-16 18:58:07 +0000

    Update tests

--- synapse/tests/test_tools_genpkg.py
+++ synapse/tests/test_tools_genpkg.py
@@ -89,22 +89,16 @@ class GenPkgTest(s_test.SynTest):
             retn = await s_genpkg.main(argv)
             self.eq(1, retn)
 
-# <<<<<<< HEAD
-#             # We'll push the pkddef to savepath if its set
-#             os.unlink(savepath)
-#             argv = ('--no-build', '--save', savepath, yamlpath)
-# =======
             # Invalid:  save with pre-made file
             argv = ('--no-build', '--save', savepath, savepath)
-# >>>>>>> master
+            retn = await s_genpkg.main(argv)
+            self.eq(1, retn)
+
+            # Push a premade yaml
+            argv = ('--push', url, '--no-build', yamlpath)
             retn = await s_genpkg.main(argv)
             self.eq(0, retn)
-            self.eq(pdef, s_common.jsload(savepath))
 
-# <<<<<<< HEAD
-#             # Push a premade yaml
-#             argv = ('--push', url, '--no-build', yamlpath)
-# =======
             # Push a premade json
             argv = ('--no-build', '--push', url, savepath)
             retn = await s_genpkg.main(argv)
