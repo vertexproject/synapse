7c458940b5f320240c74cf135ba869d419c8e115
visi <visi@vertex.link>
2021-06-30 16:07:28 -0400

    Adds lib.storm.eval

---
+++ synapse/tests/test_lib_stormlib_storm.py
@@ -0,0 +1,21 @@
+import synapse.tests.utils as s_test
+
+class LibStormTest(s_test.SynTest):
+
+    async def test_lib_stormlib_storm(self):
+        async with self.getTestCore() as core:
+
+            opts = {'vars': {'text': '(10)'}}
+            self.eq(10, await core.callStorm('return($lib.storm.eval($text))', opts=opts))
+
+            opts = {'vars': {'text': '10'}}
+            self.eq('10', await core.callStorm('return($lib.storm.eval($text))', opts=opts))
+
+            opts = {'vars': {'text': '10'}}
+            self.eq(10, await core.callStorm('return($lib.storm.eval($text, cast=int))', opts=opts))
+
+            opts = {'vars': {'text': 'WOOT.COM'}}
+            self.eq('woot.com', await core.callStorm('return($lib.storm.eval($text, cast=inet:dns:a:fqdn))', opts=opts))
+
+            opts = {'vars': {'text': '(10 + 20)', 'cast': 'inet:port'}}
+            self.eq(30, await core.callStorm('return($lib.storm.eval($text, cast=$cast))', opts=opts))
