69e01f730c0cc866f70a6debba288796d8145326
epiphyte <epiphyte@vertex.link>
2021-07-02 15:04:39 -0400

    Bump version: 2.45.0 → 2.46.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '2.45.0'
+VERSION = '2.46.0'
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
