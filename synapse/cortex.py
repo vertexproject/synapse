67a040058e0dcc5e43a1a2873fb395ac20f1c98d
James Gross <45212823+rakuy0@users.noreply.github.com>
2021-07-29 13:02:25 -0400

    Specify/Move View for Cron (SYN-2037) (#2292)
    Allow users to specify a view to run a cron job against, move a cron job to a new view, and update permission check for adding/moving cron jobs to views.

--- synapse/cortex.py
+++ synapse/cortex.py
@@ -443,7 +443,7 @@ class CoreApi(s_cell.CellApi):
         Get a list of Cortex feed functions.
 
         Notes:
-            Each feed dictinonary has the name of the feed function, the
+            Each feed dictionary has the name of the feed function, the
             full docstring for the feed function, and the first line of
             the docstring broken out in their own keys for easy use.
 
@@ -3297,7 +3297,7 @@ class Cortex(s_cell.Cell):  # type: ignore
         await self.auth.addAuthGate(iden, 'view')
         await user.setAdmin(True, gateiden=iden, logged=False)
 
-        # worldreadable is not get persisted with the view; the state ends up in perms
+        # worldreadable does not get persisted with the view; the state ends up in perms
         worldread = vdef.pop('worldreadable', False)
 
         if worldread:
@@ -4690,6 +4690,11 @@ class Cortex(s_cell.Cell):  # type: ignore
         if not cdef.get('iden'):
             cdef['iden'] = s_common.guid()
 
+        opts = {'user': cdef['creator'], 'view': cdef.get('view')}
+
+        view = self._viewFromOpts(opts)
+        cdef['view'] = view.iden
+
         return await self._push('cron:add', cdef)
 
     @s_nexus.Pusher.onPush('cron:add')
@@ -4709,6 +4714,23 @@ class Cortex(s_cell.Cell):  # type: ignore
         await user.setAdmin(True, gateiden=iden, logged=False)
 
         return cdef
+
+    async def moveCronJob(self, useriden, croniden, viewiden):
+        view = self._viewFromOpts({'view': viewiden, 'user': useriden})
+
+        appt = self.agenda.appts.get(croniden)
+        if appt is None:
+            raise s_exc.NoSuchIden(iden=croniden)
+
+        if appt.view == view.iden:
+            return croniden
+
+        return await self._push('cron:move', croniden, viewiden)
+
+    @s_nexus.Pusher.onPush('cron:move')
+    async def _onMoveCronJob(self, croniden, viewiden):
+        await self.agenda.move(croniden, viewiden)
+        return croniden
 
     @s_nexus.Pusher.onPushAuto('cron:del')
     async def delCronJob(self, iden):
