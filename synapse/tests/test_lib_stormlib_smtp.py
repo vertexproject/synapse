e7f4b564b3b68016b544896a5f5371e640087d65
visi <visi@vertex.link>
2021-08-04 10:03:11 -0400

    mock test

--- synapse/tests/test_lib_stormlib_smtp.py
+++ synapse/tests/test_lib_stormlib_smtp.py
@@ -1,18 +1,23 @@
+from unittest import mock
 import synapse.tests.utils as s_test
 
 class SmtpTest(s_test.SynTest):
 
     async def test_storm_smtp(self):
 
-        async with self.getTestCore() as core:
-            retn = await core.callStorm('''
-                $message = $lib.inet.smtp.message()
-                $message.text = hi
-                $message.sender = visi@vertex.link
-                $message.headers.Subject = woot
-                $message.recipients.append(visi@vertex.link)
-                ($ok, $retn) = $message.send('smtp.gmail.com', port=465, usetls=true)
-                return(($ok, $retn))
-            ''')
-            self.false(retn[0])
-            self.true('Authentication Required' in retn[1])
+        async def send(*args, **kwargs):
+            return
+
+        with mock.patch('aiosmtplib.send', send):
+
+            async with self.getTestCore() as core:
+                retn = await core.callStorm('''
+                    $message = $lib.inet.smtp.message()
+                    $message.text = hi
+                    $message.sender = visi@vertex.link
+                    $message.headers.Subject = woot
+                    $message.recipients.append(visi@vertex.link)
+                    ($ok, $retn) = $message.send('smtp.gmail.com', port=465, usetls=true)
+                    return(($ok, $retn))
+                ''')
+                self.eq(retn, (True, None))
