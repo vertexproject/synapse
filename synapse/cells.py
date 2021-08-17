6eedf4bcb07a8899b982ccba459ff585bf29d8c5
invisig0th <visi@vertex.link>
2021-08-17 11:02:40 -0400

    Added stemcell server (#2328)
    * Added stemcell server and vertexproject/synapse-stemcell container
    
    Co-authored-by: epiphyte <epiphyte@vertex.link>

---
+++ synapse/cells.py
@@ -0,0 +1,12 @@
+import synapse.axon as s_axon
+import synapse.cortex as s_cortex
+import synapse.cryotank as s_cryotank
+
+import synapse.lib.aha as s_aha
+import synapse.lib.jsonstor as s_jsonstor
+
+aha = s_aha.AhaCell
+axon = s_axon.Axon
+cortex = s_cortex.Cortex
+cryotank = s_cryotank.CryoCell
+jsonstor = s_jsonstor.JsonStorCell
