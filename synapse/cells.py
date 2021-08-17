6ab69465f8462f8f72aa7c7cbf8accbdc56da20c
epiphyte <epiphyte@vertex.link>
2021-08-17 09:56:46 -0400

    Implement envar lookup, add sad tests, add cryotank to cells.

--- synapse/cells.py
+++ synapse/cells.py
@@ -1,5 +1,6 @@
 import synapse.axon as s_axon
 import synapse.cortex as s_cortex
+import synapse.cryotank as s_cryotank
 
 import synapse.lib.aha as s_aha
 import synapse.lib.jsonstor as s_jsonstor
@@ -7,4 +8,5 @@ import synapse.lib.jsonstor as s_jsonstor
 aha = s_aha.AhaCell
 axon = s_axon.Axon
 cortex = s_cortex.Cortex
+cryotank = s_cryotank.CryoCell
 jsonstor = s_jsonstor.JsonStorCell
--- synapse/servers/stemcell.py
+++ synapse/servers/stemcell.py
@@ -14,21 +14,27 @@ def getStemCell(dirn):
         mesg = f'Directory {dirn} does not exist!'
         raise s_exc.NoSuchDir(mesg=mesg)
 
+    ctorname = os.getenv('SYN_STEM_CELL_CTOR')
+
     cellyaml = os.path.join(dirn, 'cell.yaml')
-    if not os.path.isfile(cellyaml):
-        mesg = f'No such file: {cellyaml}'
-        raise s_exc.NoSuchFile(mesg=mesg)
 
-    conf = s_common.yamlload(cellyaml)
-    ctorname = conf.get('cell:ctor')
+    if os.path.isfile(cellyaml):
+        conf = s_common.yamlload(cellyaml)
+        ctorname = conf.get('cell:ctor', ctorname)
 
-    return s_dyndeps.getDynLocal(ctorname.strip())
+    if ctorname is not None:
+        ctorname = ctorname.strip()
+        ctor = s_dyndeps.getDynLocal(ctorname)
+        if ctor is None:
+            raise s_exc.NoSuchCtor(mesg=f'Unable to resolve ctor [{ctorname}]', ctor=ctorname)
+        return ctor
 
-#pragma: no cover
-async def main(argv, outp=s_output.stdout):
+    mesg = f'No such file: {cellyaml} and SYN_STEM_CELL_CTOR environmt variable is not set.'
+    raise s_exc.NoSuchFile(mesg=mesg)
+
+async def main(argv, outp=s_output.stdout):  #pragma: no cover
     ctor = getStemCell(argv[0])
     return await ctor.execmain(argv, outp=outp)
 
-#pragma: no cover
-if __name__ == '__main__':
+if __name__ == '__main__':  #pragma: no cover
     sys.exit(asyncio.run(main(sys.argv[1:])))
