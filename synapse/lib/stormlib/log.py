f062ed750d24b86534c07f0abb6a23b9f3c7db48
epiphyte <epiphyte@vertex.link>
2022-02-16 13:42:43 -0500

    Add docs

--- synapse/lib/stormlib/log.py
+++ synapse/lib/stormlib/log.py
@@ -15,9 +15,104 @@ class LoggerLib(s_stormtypes.Lib):
     '''
     A Storm library which implements server side logging.
     '''
-    # _storm_locals = (
-    #     {'name': 'info'}
-    # )
+    _storm_locals = (
+        {'name': 'debug', 'desc': '''
+        Log a message to the Cortex at the debug log level.
+
+        Notes:
+            This requires the ``storm.lib.log.debug`` permission to use.
+
+        Examples:
+            Log a debug message::
+
+                $lib.log.debug('I am a debug message!')
+
+            Log s debug message with extra information::
+
+                $lib.log.debug('Extra information included here.', extra=$lib.dict(key=$valu)''',
+         'type': {'type': 'function', '_funcname': '_logDebug',
+                  'args': (
+                      {'name': 'mesg', 'type': 'str', 'desc': 'The message to log.'},
+                      {'name': 'extra', 'type': 'dict', 'desc': 'Extra key / value pairs to include when structured '
+                                                                'logging is enabled on the Cortex.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'null'},
+                  }
+         },
+        {'name': 'info', 'desc': '''
+            Log a message to the Cortex at the info log level.
+
+            Notes:
+                This requires the ``storm.lib.log.info`` permission to use.
+
+            Examples:
+                Log a info message::
+
+                    $lib.log.info('I am a info message!')
+
+                Log a info message with extra information::
+
+                    $lib.log.info('Extra information included here.', extra=$lib.dict(key=$valu)''',
+         'type': {'type': 'function', '_funcname': '_logInfo',
+                  'args': (
+                      {'name': 'mesg', 'type': 'str', 'desc': 'The message to log.'},
+                      {'name': 'extra', 'type': 'dict', 'desc': 'Extra key / value pairs to include when structured '
+                                                                'logging is enabled on the Cortex.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'null'},
+                  }
+         },
+        {'name': 'warning', 'desc': '''
+            Log a message to the Cortex at the warning log level.
+
+            Notes:
+                This requires the ``storm.lib.log.warning`` permission to use.
+
+            Examples:
+                Log a warning message::
+
+                    $lib.log.warning('I am a warning message!')
+
+                Log a warning message with extra information::
+
+                    $lib.log.warning('Extra information included here.', extra=$lib.dict(key=$valu)''',
+         'type': {'type': 'function', '_funcname': '_logDebug',
+                  'args': (
+                      {'name': 'mesg', 'type': 'str', 'desc': 'The message to log.'},
+                      {'name': 'extra', 'type': 'dict', 'desc': 'Extra key / value pairs to include when structured '
+                                                                'logging is enabled on the Cortex.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'null'},
+                  }
+         },
+        {'name': 'error', 'desc': '''
+            Log a message to the Cortex at the error log level.
+
+            Notes:
+                This requires the ``storm.lib.log.error`` permission to use.
+
+            Examples:
+                Log a error message::
+
+                    $lib.log.error('I am a error message!')
+
+                Log a error message with extra information::
+
+                    $lib.log.error('Extra information included here.', extra=$lib.dict(key=$valu)''',
+         'type': {'type': 'function', '_funcname': '_logDebug',
+                  'args': (
+                      {'name': 'mesg', 'type': 'str', 'desc': 'The message to log.'},
+                      {'name': 'extra', 'type': 'dict', 'desc': 'Extra key / value pairs to include when structured '
+                                                                'logging is enabled on the Cortex.',
+                       'default': None, },
+                  ),
+                  'returns': {'type': 'null'},
+                  }
+         },
+    )
 
     _storm_lib_path = ('log',)
 
