0906d7f9692e9bdbcddfc8963fc370159b2ae255
visi <visi@vertex.link>
2022-03-16 15:04:52 -0400

    wip

--- synapse/lib/stormtypes.py
+++ synapse/lib/stormtypes.py
@@ -531,13 +531,14 @@ class LibDmon(Lib):
                       {'name': 'iden', 'type': 'str', 'desc': 'The GUID of the Dmon to stop.', },
                   ),
                   'returns': {'type': 'boolean',
-                              'desc': 'True if the Dmon is stopped; False if the iden does not exist.', }}},
+                              'desc': '$lib.true unless the dmon does not exist or was already stopped.', }}},
         {'name': 'start', 'desc': 'Start a storm dmon.',
          'type': {'type': 'function', '_funcname': '_libDmonStart',
                   'args': (
                       {'name': 'iden', 'type': 'str', 'desc': 'The GUID of the dmon to start.', },
                   ),
-                  'returns': {'type': 'boolean', 'desc': 'Returns True.', }}},
+                  'returns': {'type': 'boolean',
+                              'desc': '$lib.true unless the dmon does not exist or was already started.', }}},
     )
     _storm_lib_path = ('dmon',)
 
