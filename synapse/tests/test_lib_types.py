395b2a05ea8258aba575923e4503ffd92374a87b
epiphyte <epiphyte@vertex.link>
2022-03-21 09:33:25 -0400

    Update test to account for changing timestamps (SYN-3382)

--- synapse/tests/test_lib_types.py
+++ synapse/tests/test_lib_types.py
@@ -964,7 +964,7 @@ class TypesTest(s_t_utils.SynTest):
             nodes = await core.nodes('test:str:tick=201401*')
             self.eq({node.ndef[1] for node in nodes}, {'a'})
 
-            nodes = await core.nodes('test:str:tick*range=("-3000 days", now)')
+            nodes = await core.nodes('test:str:tick*range=("-4200 days", now)')
             self.eq({node.ndef[1] for node in nodes}, {'a', 'b', 'c', 'd'})
 
             opts = {'vars': {'tick': tick, 'tock': tock}}
