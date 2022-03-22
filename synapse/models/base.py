5d0ca2cefe304b432e7bd4773016edcb7dc6834a
visi <visi@vertex.link>
2022-03-22 17:20:04 -0400

    Adds meta:timeline and meta:event nodes and associated taxonomies

--- synapse/models/base.py
+++ synapse/models/base.py
@@ -21,6 +21,20 @@ class BaseModule(s_module.CoreModule):
                 ('meta:note', ('guid', {}), {
                     'doc': 'An analyst note about nodes linked with -(about)> edges.'}),
 
+                ('meta:timeline', ('guid', {}), {
+                    'doc': 'A curated timeline of analytically relevant events.'}),
+
+                ('meta:timeline:taxonomy', ('taxonomy', {}), {
+                    'interfaces': ('taxonomy',),
+                    'doc': 'A taxonomy of timeline types for a meta:timeline nodes.'}),
+
+                ('meta:event', ('guid', {}), {
+                    'doc': 'An analytically relevant event in a curated timeline.'}),
+
+                ('meta:event:taxonomy', ('taxonomy', {}), {
+                    'interfaces': ('taxonomy',),
+                    'doc': 'A taxonomy of event types for a meta:event nodes.'}),
+
                 ('graph:cluster', ('guid', {}), {
                     'doc': 'A generic node, used in conjunction with Edge types, to cluster arbitrary nodes to a '
                            'single node in the model.'}),
@@ -77,6 +91,37 @@ class BaseModule(s_module.CoreModule):
                     ('created', ('time', {}), {
                         'doc': 'The time the note was created.'}),
                 )),
+
+                ('meta:timeline', {}, (
+                    ('title', ('str', {}), {
+                        'ex': 'The history of the Vertex Project',
+                        'doc': 'A title for the timeline.'}),
+                    ('summary', ('str', {}), {
+                        'disp': {'hint': 'text'},
+                        'doc': 'A prose summary of the timeline.'}),
+                    ('type', ('meta:timeline:taxonomy', {}), {
+                        'doc': 'The type of timeline.'}),
+                )),
+
+                ('meta:timeline:taxonomy', {}, ()),
+
+                ('meta:event', {}, (
+                    ('timeline', ('meta:timeline', {}), {
+                        'doc': 'The timeline containing the event.'}),
+                    ('title', ('str', {}), {
+                        'doc': 'A title for the event.'}),
+                    ('summary', ('str', {}), {
+                        'disp': {'hint': 'text'},
+                        'doc': 'A prose summary of the event.'}),
+                    ('time', ('time', {}), {
+                        'doc': 'The time that the event occurred.'}),
+                    ('duration', ('duration', {}), {
+                        'doc': 'The duration of the event.'}),
+                    ('type', ('meta:event:taxonomy', {}), {
+                        'doc': 'Time type of event.'}),
+                )),
+
+                ('meta:event:taxonomy', {}, ()),
 
                 ('graph:cluster', {}, (
                     ('name', ('str', {'lower': True}), {
