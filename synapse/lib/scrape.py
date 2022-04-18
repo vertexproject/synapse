09053661eb47c2cfc00ce5c0893e29e0414b696e
epiphyte <epiphyte@vertex.link>
2022-04-18 16:05:41 -0400

    Bug - scrape - allow endash when scraping CVE (SYN-3287)

--- synapse/lib/scrape.py
+++ synapse/lib/scrape.py
@@ -70,6 +70,14 @@ def fqdn_check(match: regex.Match):
             return None, {}
     return valu, {}
 
+def cve_check(match: regex.Match):
+    mnfo = match.groupdict()
+    valu = mnfo.get('valu')  # type: str
+    cbfo = {}
+
+    valu = valu.replace('\u2013', '-')
+    return valu, cbfo
+
 # these must be ordered from most specific to least specific to allow first=True to work
 scrape_types = [  # type: ignore
     ('inet:url', r'(?P<prefix>[\\{<\(\[]?)(?P<valu>[a-zA-Z][a-zA-Z0-9]*://(?(?=[,.]+[ \'\"\t\n\r\f\v])|[^ \'\"\t\n\r\f\v])+)',
@@ -81,7 +89,7 @@ scrape_types = [  # type: ignore
     ('hash:md5', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[A-Fa-f0-9]{32})(?:[^A-Za-z0-9]|$))', {}),
     ('hash:sha1', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[A-Fa-f0-9]{40})(?:[^A-Za-z0-9]|$))', {}),
     ('hash:sha256', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[A-Fa-f0-9]{64})(?:[^A-Za-z0-9]|$))', {}),
-    ('it:sec:cve', r'(?:[^a-z0-9]|^)(?P<valu>CVE-[0-9]{4}-[0-9]{4,})(?:[^a-z0-9]|$)', {}),
+    ('it:sec:cve', r'(?:[^a-z0-9]|^)(?P<valu>CVE[-–][0-9]{4}[-–][0-9]{4,})(?:[^a-z0-9]|$)', {'callback': cve_check}),
     ('crypto:currency:address', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[1][a-zA-HJ-NP-Z0-9]{25,39})(?:[^A-Za-z0-9]|$))',
      {'callback': s_coin.btc_base58_check}),
     ('crypto:currency:address', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>3[a-zA-HJ-NP-Z0-9]{33})(?:[^A-Za-z0-9]|$))',
