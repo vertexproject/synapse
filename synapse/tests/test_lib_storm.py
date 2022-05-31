6c9c272c02677e8ed7a1b80c9d39b02ad6e4db32
cisphyx <cisphyx@vertex.link>
2022-05-31 17:05:21 -0400

    allow comma after nonquoteword

--- synapse/tests/test_lib_storm.py
+++ synapse/tests/test_lib_storm.py
@@ -3435,6 +3435,16 @@ class StormTest(s_t_utils.SynTest):
     async def test_storm_queries(self):
         async with self.getTestCore() as core:
 
+            q = 'iden			Jul 17, 2019, 8:14:22 PM		10	 hostname'
+            msgs = await core.stormlist(q)
+            self.stormIsInWarn('Failed to decode iden: [Jul]', msgs)
+            self.stormIsInWarn('Failed to decode iden: [17, ]', msgs)
+            self.stormIsInWarn('Failed to decode iden: [2019, ]', msgs)
+            self.stormIsInWarn('Failed to decode iden: [8:14:22]', msgs)
+            self.stormIsInWarn('Failed to decode iden: [PM]', msgs)
+            self.stormIsInWarn('iden must be 32 bytes [10]', msgs)
+            self.stormIsInWarn('Failed to decode iden: [hostname]', msgs)
+
             q = '''
             [ inet:ipv4=1.2.3.4
                 // add an asn
