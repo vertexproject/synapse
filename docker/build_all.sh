b96dff5869fefa08b5c81237932e442e4ebc28bc
epiphyte <epiphyte@vertex.link>
2022-07-20 14:02:45 -0400

    Make images build from standalone dockerfiles again.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -18,22 +18,15 @@ set -u # undefined variables
 set -o pipefail # pipefail propagate error codes
 set -x # debugging
 
-BASEIMAGE=synbuild:base
-
 TAG=${1:-}
 
 [ ! $TAG ] && echo "Tag not provided, defaulting tag to dev_build" && TAG=dev_build
 
-# Build (or rebuild) the base image
-docker/build_base.sh $BASEIMAGE
-
 # Build target images
+docker build --pull -t vertexproject/synapse:$TAG -f docker/images/synapse/Dockerfile .
 docker/build_image.sh aha $TAG
 docker/build_image.sh axon $TAG
 docker/build_image.sh cortex $TAG
 docker/build_image.sh cryotank $TAG
 docker/build_image.sh jsonstor $TAG
 docker/build_image.sh stemcell $TAG
-
-# Tag the base image as well
-docker tag ${BASEIMAGE} vertexproject/synapse:$TAG
--- docker/build_image.sh
+++ docker/build_image.sh
@@ -22,12 +22,10 @@ set -u # undefined variables
 set -o pipefail # pipefail propagate error codes
 # set -x # debugging
 
-BASEIMAGE=synbuild:base
-
 IMAGE=${1:-}
 if [ ${IMAGE} == "synapse" ]
 then
-    echo "The vertexproject/synapse image is built with docker/build_all.sh"
+    echo "The vertexproject/synapse image is not built with this script."
     false
 fi
 IMAGE_DIR=docker/images/${IMAGE}
@@ -37,15 +35,6 @@ TAG=${2:-}
 
 [ ! $TAG ] && echo "Tag not provided, defaulting tag to dev_build" && TAG=dev_build
 
-# Chuck the syndev:base image if it exists.
-SYNDEVBASE_EXISTS=`docker image ls -q ${BASEIMAGE} | wc -l`
-if [ ${SYNDEVBASE_EXISTS} == "0" ]
-then
-    docker/build_base.sh
-else
-    echo "Using existing ${BASEIMAGE}. Use docker/build_base.sh if you need a updated base image".
-fi
-
 # Build target image
 echo "Building from docker/images/$IMAGE/Dockerfile"
-docker build -t vertexproject/synapse-$IMAGE:$TAG -f docker/images/$IMAGE/Dockerfile .
+docker build --pull -t vertexproject/synapse-$IMAGE:$TAG -f docker/images/$IMAGE/Dockerfile .
