816684007d2d8ddc1c0bae176d941aa455749a95
visi <visi@vertex.link>
2022-07-29 15:52:00 -0400

    WIP: Adds .random.int() to storm

---
+++ synapse/lib/stormlib/random.py
@@ -0,0 +1,30 @@
+import random
+
+import synapse.common as s_common
+import synapse.lib.stormtypes as s_stormtypes
+
+@s_stormtypes.registry.registerLib
+class LibRandom(s_stormtypes.Lib):
+    '''
+    A Storm library for generating random values.
+    '''
+    _storm_locals = (
+        {'name': 'int', 'desc': 'Generate a random integer.',
+         'type': {'type': 'function', '_funcname': '_int',
+                  'args': (
+                      {'name': 'maxval', 'type': 'int', 'desc': 'The maximum random value.'},
+                      {'name': 'minval', 'type': 'int', 'desc': 'The minimum random value.', 'default': 0},
+                  ),
+                  'returns': {'type': 'int', 'desc': 'A random integer in the range min-max inclusive.'}}},
+    )
+    _storm_lib_path = ('random',)
+
+    def getObjLocals(self):
+        return {
+            'int': self._int,
+        }
+
+    async def _int(self, maxval, minval=0):
+        maxval = await s_stormtypes.toint(maxval)
+        minval = await s_stormtypes.toint(minval)
+        return random.randint(minval, maxval)
