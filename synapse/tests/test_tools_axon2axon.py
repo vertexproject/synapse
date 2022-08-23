d016d34998a0dfb244b7e85c39b959bb83d6ee80
visi <visi@vertex.link>
2022-08-23 11:18:07 -0400

    Add axon2axon command

---
+++ synapse/tests/test_tools_axon2axon.py
@@ -0,0 +1,32 @@
+import synapse.lib.base as s_base
+import synapse.lib.output as s_output
+
+import synapse.tests.utils as s_test
+import synapse.tools.axon2axon as s_axon2axon
+
+class Axon2AxonTest(s_test.SynTest):
+
+    async def test_axon2axon(self):
+
+        async with self.getTestAxon() as srcaxon:
+
+            async with self.getTestAxon() as dstaxon:
+
+                srcurl = srcaxon.getLocalUrl()
+                dsturl = dstaxon.getLocalUrl()
+
+                (size, sha256) = await srcaxon.put(b'visi')
+
+                outp = s_output.OutPutStr()
+                await s_axon2axon.main([srcurl, dsturl], outp=outp)
+                self.true(await dstaxon.has(sha256))
+                self.isin('Starting transfer at offset: 0', str(outp))
+                self.isin('[         0] - e45bbb7e03acacf4d1cca4c16af1ec0c51d777d10e53ed3155bd3d8deb398f3f (4)', str(outp))
+
+                (size, sha256) = await srcaxon.put(b'vertex')
+
+                outp = s_output.OutPutStr()
+                await s_axon2axon.main([srcurl, dsturl, '--offset', '1'], outp=outp)
+                self.true(await dstaxon.has(sha256))
+                self.isin('Starting transfer at offset: 1', str(outp))
+                self.isin('[         1] - e1b683e26a3aad218df6aa63afe9cf57fdb5dfaf5eb20cddac14305d67f48a02 (6)', str(outp))
---
+++ synapse/tools/axon2axon.py
@@ -0,0 +1,40 @@
+import logging
+
+import synapse.common as s_common
+import synapse.telepath as s_telepath
+
+import synapse.lib.cmd as s_cmd
+import synapse.lib.base as s_base
+import synapse.lib.output as s_output
+
+logger = logging.getLogger(__name__)
+
+async def main(argv, outp=s_output.stdout):
+
+    pars = s_cmd.Parser(prog='synapse.tools.storm', outp=outp)
+    pars.add_argument('--offset', type=int, default=0, help='An offset within the source axon to start from.')
+    pars.add_argument('src_axon', help='The telepath URL of the source axon.')
+    pars.add_argument('dst_axon', help='The telepath URL of the destination axon.')
+
+    opts = pars.parse_args(argv)
+    if pars.exited:
+        return(-1)
+
+    async with await s_base.Base.anit() as base:
+
+        srcaxon = await base.enter_context(await s_telepath.openurl(opts.src_axon))
+        dstaxon = await base.enter_context(await s_telepath.openurl(opts.dst_axon))
+
+        outp.printf(f'Starting transfer at offset: {opts.offset}')
+
+        async for (offs, (sha256, size)) in srcaxon.hashes(opts.offset):
+            offstext = str(offs).rjust(10)
+            sha2text = s_common.ehex(sha256)
+            outp.printf(f'[{offstext}] - {sha2text} ({size})')
+            async with await dstaxon.upload() as fd:
+                async for byts in srcaxon.get(sha256):
+                    await fd.write(byts)
+                await fd.save()
+
+if __name__ == '__main__':  # pragma: no cover
+    sys.exit(asyncio.run(main(sys.argv[1:])))
