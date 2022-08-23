fd59f474dfa90b3276d75c43a360350a200d504e
vEpiphyte <epiphyte@vertex.link>
2022-08-23 15:50:59 -0400

    Feat axon2axon aha (#2816)
    * Add aha support to axon2axon
    
    * Use outp with s_cmd.Parser, test with testoutp

--- synapse/tools/axon2axon.py
+++ synapse/tools/axon2axon.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 async def main(argv, outp=s_output.stdout):
 
-    pars = s_cmd.Parser(prog='synapse.tools.axon2axon')
+    pars = s_cmd.Parser(prog='synapse.tools.axon2axon', outp=outp)
     pars.add_argument('--offset', type=int, default=0, help='An offset within the source axon to start from.')
     pars.add_argument('src_axon', help='The telepath URL of the source axon.')
     pars.add_argument('dst_axon', help='The telepath URL of the destination axon.')
@@ -24,21 +24,22 @@ async def main(argv, outp=s_output.stdout):
     except s_exc.ParserExit:
         return 1
 
-    async with await s_base.Base.anit() as base:
+    async with s_telepath.withTeleEnv():
+        async with await s_base.Base.anit() as base:
 
-        srcaxon = await base.enter_context(await s_telepath.openurl(opts.src_axon))
-        dstaxon = await base.enter_context(await s_telepath.openurl(opts.dst_axon))
+            srcaxon = await base.enter_context(await s_telepath.openurl(opts.src_axon))
+            dstaxon = await base.enter_context(await s_telepath.openurl(opts.dst_axon))
 
-        outp.printf(f'Starting transfer at offset: {opts.offset}')
+            outp.printf(f'Starting transfer at offset: {opts.offset}')
 
-        async for (offs, (sha256, size)) in srcaxon.hashes(opts.offset):
-            offstext = str(offs).rjust(10)
-            sha2text = s_common.ehex(sha256)
-            outp.printf(f'[{offstext}] - {sha2text} ({size})')
-            async with await dstaxon.upload() as fd:
-                async for byts in srcaxon.get(sha256):
-                    await fd.write(byts)
-                await fd.save()
+            async for (offs, (sha256, size)) in srcaxon.hashes(opts.offset):
+                offstext = str(offs).rjust(10)
+                sha2text = s_common.ehex(sha256)
+                outp.printf(f'[{offstext}] - {sha2text} ({size})')
+                async with await dstaxon.upload() as fd:
+                    async for byts in srcaxon.get(sha256):
+                        await fd.write(byts)
+                    await fd.save()
     return 0
 
 if __name__ == '__main__':  # pragma: no cover
