86b3ce91aba082a94d1a02852dc7335fd9c53ed4
mikemoritz <mike.moritz@vertex.link>
2022-08-25 14:56:05 -0700

    add lib.axon.metrics

--- synapse/lib/stormtypes.py
+++ synapse/lib/stormtypes.py
@@ -1892,6 +1892,17 @@ class LibAxon(Lib):
                   ),
                   'returns': {'name': 'yields', 'type': 'list',
                               'desc': 'A list of strings from the CSV file.'}}},
+        {'name': 'metrics', 'desc': '''
+        Get runtime metrics of the Axon.
+
+        Example:
+            Print the total number of files stored in the Axon::
+
+                $data = $lib.axon.metrics()
+                $lib.print("The Axon has {n} files", n=$data."file:count")
+        ''',
+        'type': {'type': 'function', '_funcname': 'metrics',
+                 'returns': {'type': 'dict', 'desc': 'A dictionary containing runtime data about the Axon.'}}},
     )
     _storm_lib_path = ('axon',)
 
@@ -1906,6 +1917,7 @@ class LibAxon(Lib):
             'readlines': self.readlines,
             'jsonlines': self.jsonlines,
             'csvrows': self.csvrows,
+            'metrics': self.metrics,
         }
 
     def strify(self, item):
@@ -2081,6 +2093,10 @@ class LibAxon(Lib):
         async for item in self.runt.snap.core.axon.csvrows(s_common.uhex(sha256), dialect, **fmtparams):
             yield item
             await asyncio.sleep(0)
+
+    async def metrics(self):
+        self.runt.confirm(('storm', 'lib', 'axon', 'has'))
+        return await self.runt.snap.core.axon.metrics()
 
 @registry.registerLib
 class LibBytes(Lib):
