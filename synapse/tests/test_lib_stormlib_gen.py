03dba36ac318ccea459c61a5a0c0b688f0291ee7
visi <visi@vertex.link>
2022-10-03 09:10:20 -0400

    Add .gen.newsByUrl() and .gen.softByName()

--- synapse/tests/test_lib_stormlib_gen.py
+++ synapse/tests/test_lib_stormlib_gen.py
@@ -19,3 +19,13 @@ class StormLibGenTest(s_test.SynTest):
             nodes01 = await core.nodes('yield $lib.gen.industryByName(intelsoftware)')
             self.eq('intelsoftware', nodes00[0].get('name'))
             self.eq(nodes00[0].ndef, nodes01[0].ndef)
+
+            nodes00 = await core.nodes('yield $lib.gen.newsByUrl(https://vertex.link)')
+            nodes01 = await core.nodes('yield $lib.gen.newsByUrl(https://vertex.link)')
+            self.eq('https://vertex.link', nodes00[0].get('url'))
+            self.eq(nodes00[0].ndef, nodes01[0].ndef)
+
+            nodes00 = await core.nodes('yield $lib.gen.softByName(synapse)')
+            nodes01 = await core.nodes('yield $lib.gen.softByName(synapse)')
+            self.eq('synapse', nodes00[0].get('name'))
+            self.eq(nodes00[0].ndef, nodes01[0].ndef)
