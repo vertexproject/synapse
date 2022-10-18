9fd57d4796f3cea800e738cde5a4595ea691ef3d
epiphyte <epiphyte@vertex.link>
2022-10-18 11:20:00 -0400

    Fix test_syn_model_runts

--- synapse/tests/test_model_syn.py
+++ synapse/tests/test_model_syn.py
@@ -299,7 +299,7 @@ class SynModelTest(s_t_utils.SynTest):
         # Ensure that the model runts are re-populated after a model load has occurred.
         with self.getTestDir() as dirn:
 
-            async with self.getTestCore(dirn=dirn) as core:
+            async with await s_cortex.Cortex.anit(dirn) as core:
 
                 # Lift nodes
                 nodes = await core.nodes('syn:form=syn:tag')
