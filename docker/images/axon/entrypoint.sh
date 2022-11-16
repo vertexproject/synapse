9241dd469debd026437731e535db2e911401ed85
epiphyte <epiphyte@vertex.link>
2022-11-16 09:51:10 -0500

    Slimmed down entrypoint scripts.

--- docker/images/axon/entrypoint.sh
+++ docker/images/axon/entrypoint.sh
@@ -3,35 +3,22 @@
 PREBOOT_SCRIPT=/vertex/preboot/run
 CONCURRENT_SCRIPT=/vertex/concurrent/run
 
-# Preconditions
 if [ -f $PREBOOT_SCRIPT ]
 then
-    if [ -x $PREBOOT_SCRIPT ]
+    echo "Executing $PREBOOT_SCRIPT"
+    ./$PREBOOT_SCRIPT
+    if [ $? -ne 0 ]
     then
-        echo "Executing $PREBOOT_SCRIPT"
-        ./$PREBOOT_SCRIPT
-        PREBOOT_RET=$?
-        if [ $PREBOOT_RET -ne 0 ]
-        then
-            echo "$PREBOOT_SCRIPT script failed with return value $PREBOOT_RET"
-            exit 1
-        fi
-    else
-        echo "$PREBOOT_SCRIPT exists but is not marked executable."
+        echo "$PREBOOT_SCRIPT script failed with return value $?"
         exit 1
     fi
 fi
 
 if [ -f $CONCURRENT_SCRIPT ]
 then
-    if [ -x $CONCURRENT_SCRIPT ]
-    then
-        echo "Executing $CONCURRENT_SCRIPT"
-        ./$CONCURRENT_SCRIPT &
-    else
-        echo "$CONCURRENT_SCRIPT exists but is not marked executable."
-        exit 1
-    fi
+    echo "Executing and backgrounding $CONCURRENT_SCRIPT"
+    ./$CONCURRENT_SCRIPT &
 fi
+
 
 exec python -O -m synapse.servers.axon /vertex/storage
