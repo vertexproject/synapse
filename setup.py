fc02197830191285cd2864f175ebea35fb45e032
vEpiphyte <epiphyte@vertex.link>
2023-01-25 12:40:39 -0500

    python 3.10 cutover (SYN-3525) (#2962)
    * mark 3.10 as a supported python version
    * build images off of debian with 3.10
    * lighten 3.7 builds in from circleci
    * add 3.10 to all circleci release paths

--- setup.py
+++ setup.py
@@ -145,6 +145,7 @@ setup(
 
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
     ],
     cmdclass={
         'verify': VerifyVersionCommand,
