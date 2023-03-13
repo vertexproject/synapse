68524119851cd24553726bd00787a51083cea32b
epiphyte <epiphyte@vertex.link>
2023-03-13 17:04:53 +0000

    proxy tests support python-socks v2.1.0 and v2.2.0 behavior

--- synapse/tests/test_axon.py
+++ synapse/tests/test_axon.py
@@ -901,7 +901,8 @@ bar baz",vv
         async with self.getTestAxon(conf=conf) as axon:
             async with axon.getLocalProxy() as proxy:
                 resp = await proxy.wget('http://vertex.link')
-                self.isin('Could not connect to proxy 127.0.0.1:1', resp.get('mesg', ''))
+                self.false(resp.get('ok'))
+                self.isin('connect to proxy 127.0.0.1:1', resp.get('mesg', ''))
 
     async def test_axon_wput(self):
 
@@ -963,8 +964,8 @@ bar baz",vv
         async with self.getTestAxon(conf=conf) as axon:
             async with axon.getLocalProxy() as proxy:
                 resp = await proxy.postfiles(fields, f'https://127.0.0.1:{port}/api/v1/pushfile', ssl=False)
-                self.false(resp['ok'])
-                self.isin('Could not connect to proxy 127.0.0.1:1', resp.get('err', ''))
+                self.false(resp.get('ok'))
+                self.isin('connect to proxy 127.0.0.1:1', resp.get('err', ''))
 
     async def test_axon_tlscapath(self):
 
