1afd09e1274a09086c014f01260917a4a9744e9f
visi <visi@vertex.link>
2023-05-22 08:56:01 -0400

    Fix perms check on runas to allow elevated runtimes

--- synapse/lib/storm.py
+++ synapse/lib/storm.py
@@ -5765,7 +5765,8 @@ class RunAsCmd(Cmd):
         return pars
 
     async def execStormCmd(self, runt, genr):
-        if not runt.user.isAdmin():
+
+        if not runt.isAdmin():
             mesg = 'The runas command requires admin privileges.'
             raise s_exc.AuthDeny(mesg=mesg, user=self.runt.user.iden, username=self.runt.user.name)
 
