db973160291764e1ef494855f0ed5e41367b735b
Cisphyx <cisphyx@vertex.link>
2023-06-08 10:45:37 -0400

    Migrate file:bytes:pe:imphash stortype (SYN-5556) (#3173)

--- synapse/tests/test_lib_modelrev.py
+++ synapse/tests/test_lib_modelrev.py
@@ -397,3 +397,8 @@ class ModelRevTest(s_tests.SynTest):
         async with self.getRegrCore('model-0.2.20') as core:
             self.len(1, await core.nodes('inet:user="visi@vertex.link" -> inet:url'))
             self.len(1, await core.nodes('inet:passwd="secret@" -> inet:url'))
+
+            md5 = 'e66a62b251fcfbbc930b074503d08542'
+            nodes = await core.nodes(f'hash:md5={md5} -> file:bytes')
+            self.len(1, nodes)
+            self.eq(md5, nodes[0].props.get('mime:pe:imphash'))
