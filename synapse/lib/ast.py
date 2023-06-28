56c42d41587c2df7c4e39046f1eba112afe4bd61
mikemoritz <57907149+mikemoritz@users.noreply.github.com>
2023-06-28 15:50:03 -0700

    Catch divide by zero exceptions (SYN-5566) (#3203)
    * catch div by zero exceptions
    * catch decimal invalidoperation

--- synapse/lib/ast.py
+++ synapse/lib/ast.py
@@ -1,5 +1,6 @@
 import types
 import asyncio
+import decimal
 import fnmatch
 import hashlib
 import logging
@@ -3082,7 +3083,14 @@ class ExprNode(Value):
     async def compute(self, runt, path):
         parm1 = await self.kids[0].compute(runt, path)
         parm2 = await self.kids[2].compute(runt, path)
-        return await self._operfunc(parm1, parm2)
+        try:
+            return await self._operfunc(parm1, parm2)
+        except ZeroDivisionError:
+            exc = s_exc.StormRuntimeError(mesg='Cannot divide by zero')
+            raise self.kids[2].addExcInfo(exc)
+        except decimal.InvalidOperation:
+            exc = s_exc.StormRuntimeError(mesg='Invalid operation on a Number')
+            raise self.addExcInfo(exc)
 
 class ExprOrNode(Value):
     async def compute(self, runt, path):
