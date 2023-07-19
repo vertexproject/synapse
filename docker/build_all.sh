ef9cea258a519144725afa0a1cfa59275be63c0f
epiphyte <epiphyte@vertex.link>
2023-07-19 21:59:08 +0000

    Prune build environments before starting each build.

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -23,6 +23,7 @@ TAG=${1:-}
 [ ! $TAG ] && echo "Tag not provided, defaulting tag to dev_build" && TAG=dev_build
 
 # Build target images
+docker builder prune -a -f
 docker build --no-cache --progress plain --pull -t vertexproject/synapse:$TAG -f docker/images/synapse/Dockerfile .
 docker/build_image.sh aha $TAG
 docker/build_image.sh axon $TAG
