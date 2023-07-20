28a7cad8f5a46c8f159b1ffe47538a5af616a4ed
cisphyx <cisphyx@vertex.link>
2023-07-20 15:55:10 -0400

    remove unnecessary catch blocks

--- synapse/lib/storm.py
+++ synapse/lib/storm.py
@@ -1810,12 +1810,6 @@ class Runtime(s_base.Base):
                     await asyncio.sleep(0)
                 await self.emitq.put((False, None))
 
-            except asyncio.CancelledError: # pragma: no cover
-                raise
-
-            except s_stormctrl.StormStop:
-                await self.emitq.put((False, None))
-
             except Exception as e:
                 await self.emitq.put((False, e))
 
