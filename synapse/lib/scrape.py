d05db718b7e506315a41ba8fbe84b51ae98c2941
blackout <blackout@vertex.link>
2023-07-24 13:56:53 -0400

    Initial commit

--- synapse/lib/scrape.py
+++ synapse/lib/scrape.py
@@ -73,6 +73,21 @@ def fqdn_check(match: regex.Match):
             return None, {}
     return valu, {}
 
+def inet_server_check(match: regex.Match):
+    mnfo = match.groupdict()
+    valu = mnfo.get('valu')
+    port = mnfo.get('port')
+
+    try:
+        port = int(port)
+    except ValueError:
+        return None, {}
+
+    if port < 1 or port > 2**16 - 1:
+        return None, {}
+
+    return valu, {}
+
 def cve_check(match: regex.Match):
     mnfo = match.groupdict()
     valu = mnfo.get('valu')  # type: str
@@ -81,13 +96,49 @@ def cve_check(match: regex.Match):
     valu = s_chop.replaceUnicodeDashes(valu)
     return valu, cbfo
 
+# This excellent IPV6 regex comes from RFC3986. Slightly modified to not capture
+# '::' (any-address)
+ipaddr_define = r'''
+(?(DEFINE)
+  (?<OCTET>25[0-5]|2[0-4]\d|[01]?\d\d?)
+  (?<IPV4>(?:(?&OCTET)\.){3}(?&OCTET))
+  (?<H16>[0-9a-fA-F]{1,4})
+  (?<LS32>(?&H16):(?&H16)|(?&IPV4))
+  (?<IPV6>
+    (?:                                 (?:(?&H16):){6}(?&LS32)) |
+    (?:                              :: (?:(?&H16):){5}(?&LS32)) |
+    (?:(?:                 (?&H16))? :: (?:(?&H16):){4}(?&LS32)) |
+    (?:(?:(?:(?&H16):){0,1}(?&H16))? :: (?:(?&H16):){3}(?&LS32)) |
+    (?:(?:(?:(?&H16):){0,2}(?&H16))? :: (?:(?&H16):){2}(?&LS32)) |
+    (?:(?:(?:(?&H16):){0,3}(?&H16))? :: (?:(?&H16):){1}(?&LS32)) |
+    (?:(?:(?:(?&H16):){0,4}(?&H16))? ::                (?&LS32)) |
+    (?:(?:(?:(?&H16):){0,5}(?&H16))? ::                 (?&H16)) |
+    (?:(?:(?:(?&H16):){0,6}(?&H16))  ::                        )
+  )
+
+  (?<IPV4_ADDR>
+    (?<!\d|\d\.|[0-9a-fA-F:]:)
+    (?&IPV4)
+    (?!\d|\d\.|\.\d)
+  )
+
+  (?<IPV6_ADDR>
+    (?<![0-9a-fA-F]:|:[0-9a-fA-F]|::|\d\.)
+    (?&IPV6)
+    (?![0-9a-fA-F:]|\d\.|\.\d)
+  )
+)
+'''
+
 # these must be ordered from most specific to least specific to allow first=True to work
 scrape_types = [  # type: ignore
     ('inet:url', r'(?P<prefix>[\\{<\(\[]?)(?P<valu>[a-zA-Z][a-zA-Z0-9]*://(?(?=[,.]+[ \'\"\t\n\r\f\v])|[^ \'\"\t\n\r\f\v])+)',
      {'callback': fqdn_prefix_check}),
     ('inet:email', r'(?=(?:[^a-z0-9_.+-]|^)(?P<valu>[a-z0-9_\.\-+]{1,256}@(?:[a-z0-9_-]{1,63}\.){1,10}(?:%s))(?:[^a-z0-9_.-]|[.\s]|$))' % tldcat, {}),
-    ('inet:server', r'(?P<valu>(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):[0-9]{1,5})', {}),
-    ('inet:ipv4', r'(?P<valu>(?<!\d|\d\.)(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?!\d|\.\d))', {}),
+    ('inet:server', ipaddr_define + r'(?P<valu>(?:(?<!\d|\d\.|[0-9a-fA-F:]:)(?P<addr>(?&IPV4)):(?P<port>\d{1,5})(?!\d|\d\.|\.\d)) | (?:\[(?P<addr>(?&IPV6))\]:(?P<port>\d{1,5})(?!\d|\d\.|\.\d)))',
+     {'callback': inet_server_check}),
+    ('inet:ipv4', ipaddr_define + r'(?P<valu>(?&IPV4_ADDR))', {}),
+    ('inet:ipv6', ipaddr_define + r'(?P<valu>(?&IPV6_ADDR))', {}),
     ('inet:fqdn', r'(?=(?:[^\p{L}\p{M}\p{N}\p{S}\u3002\uff0e\uff61_.-]|^|[' + idna_disallowed + '])(?P<valu>(?:((?![' + idna_disallowed + r'])[\p{L}\p{M}\p{N}\p{S}_-]){1,63}[\u3002\uff0e\uff61\.]){1,10}(?:' + tldcat + r'))(?:[^\p{L}\p{M}\p{N}\p{S}\u3002\uff0e\uff61_.-]|[\u3002\uff0e\uff61.]([\p{Z}\p{Cc}]|$)|$|[' + idna_disallowed + r']))', {'callback': fqdn_check}),
     ('hash:md5', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[A-Fa-f0-9]{32})(?:[^A-Za-z0-9]|$))', {}),
     ('hash:sha1', r'(?=(?:[^A-Za-z0-9]|^)(?P<valu>[A-Fa-f0-9]{40})(?:[^A-Za-z0-9]|$))', {}),
@@ -116,7 +167,7 @@ scrape_types = [  # type: ignore
 
 _regexes = collections.defaultdict(list)
 for (name, rule, opts) in scrape_types:
-    blob = (regex.compile(rule, regex.IGNORECASE), opts)
+    blob = (regex.compile(rule, regex.IGNORECASE | regex.VERBOSE), opts)
     _regexes[name].append(blob)
 
 def getForms():
