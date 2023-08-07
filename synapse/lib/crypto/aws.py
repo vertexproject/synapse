e9ee6a4d5ed763a3b3082a17e2a9fe74df63f33b
epiphyte <epiphyte@vertex.link>
2023-08-07 23:26:15 +0000

    Add AWSSigV4 signer; add POC envar credential provider; add stormlib signer

--- synapse/lib/crypto/aws.py
+++ synapse/lib/crypto/aws.py
@@ -1,7 +1,11 @@
+import os
 import json
 import hmac
 import hashlib
 import logging
+import datetime
+
+import urllib.parse as u_parse
 
 import aiohttp
 
@@ -12,6 +16,7 @@ import synapse.common as s_common
 import synapse.lib.base as s_base
 import synapse.lib.time as s_time
 
+import typing
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +26,13 @@ class AwsCredentialError(s_exc.CryptoErr):
     '''
     pass
 
-class AWSProvider(s_base.Base):
+class AwsSigningError(s_exc.CryptoErr):
+    '''
+    An exception raised due to an error signing an AWS requests.
+    '''
+
+class AWSCredentialProvider(s_base.Base):
+
     async def __anit__(self):
         await s_base.Base.__anit__(self)
         self.accesskeyid = None
@@ -31,24 +42,42 @@ class AWSProvider(s_base.Base):
         self.lastupdated = None
         self.expires_after = None
 
-    def storeCredentials(self, creds: dict):
-        self.token = creds.get('Token')
-        self.accesskeyid = creds.get('AccessKeyId')
-        self.secretaccesskey = creds.get('SecretAccessKey')
-        self.expiration = s_time.parse(creds.get('Expiration'))
-        self.lastupdated = s_time.parse(creds.get('LastUpdated'))
-        # Keep credentials alive for 80% of their lifetime before we'll attempt to get new credentials
-        self.expires_after = self.lastupdated + int ((self.expiration - self.lastupdated) * 0.8)
+    def getProvType(self):
+        return self._getProvType()
 
-class AWSEC2Provider(AWSProvider):
-    EC2_METADATA_ENDPOINT = 'http://169.254.169.254'
+    def _getProvType(self):  # pragma: no cover
+        raise s_exc.NoSuchImpl(mesg=f'_getProvType not implemented for {self.__class__.__name__}')
 
-    async def __anit__(self, role=None):
-        await AWSProvider.__anit__(self)
-        self.role = None
-        self._machinetokenlifetime = '21600'
+    async def getCredentials(self):
+        return await self._getCredentials()
+
+    async def _getCredentials(self):  # pragma: no cover
+        raise s_exc.NoSuchImpl(mesg=f'_getCredentials not implemented for {self.__class__.__name__}')
 
     async def updateCredentials(self):
+        '''
+        Refresh the credentials for a given provider.
+
+        Generally uses should use getCredentials()
+        '''
+        return await self._updateCredentials()
+
+    async def _updateCredentials(self):  # pragma: no cover
+        raise s_exc.NoSuchImpl(mesg=f'_updateCredentials not implemented for {self.__class__.__name__}')
+
+class AWSEC2Provider(AWSCredentialProvider):
+    # IDMS provider endpoint
+    EC2_METADATA_ENDPOINT = 'http://169.254.169.254'
+
+    async def __anit__(self, role: str =None):
+        await AWSCredentialProvider.__anit__(self)
+        self.role = role
+        self._machinetokenlifetime = '21600'
+
+    def _getProvType(self):
+        return 'idms'
+
+    async def _updateCredentials(self):
         '''
         Get credentials from the endpoint metadata service
         '''
@@ -76,22 +105,151 @@ class AWSEC2Provider(AWSProvider):
                 text = await resp.text()
                 creds = json.loads(text)
 
+        self.token = creds.get('Token')
+        self.accesskeyid = creds.get('AccessKeyId')
+        self.secretaccesskey = creds.get('SecretAccessKey')
+        self.expiration = s_time.parse(creds.get('Expiration'))
+        self.lastupdated = s_time.parse(creds.get('LastUpdated'))
+        # Keep credentials alive for 80% of their lifetime before we'll attempt to get new credentials
+        self.expires_after = self.lastupdated + int((self.expiration - self.lastupdated) * 0.8)
+
         return creds
 
-    async def getCredentials(self, role=None):
+    async def _getCredentials(self):
         if self.expiration is None:
-            logger.debug(f'Retrieving initial credentials for {self.role}')
             _creds = await self.updateCredentials()
-            self.storeCredentials(_creds)
         elif self.expires_after > s_common.now():
-            # TODO Use ioloop monotonic clock?
-            logger.debug(f'Getting updated credentials for {self.role}')
             _creds = await self.updateCredentials()
-            self.storeCredentials(_creds)
         ret = {
             'token': self.token,
             'accesskeyid': self.accesskeyid,
             'secretaccesskey': self.secretaccesskey,
         }
-
         return ret
+
+class AWSEnvarProvider(AWSCredentialProvider):
+    AWS_TOKEN_VARS = (
+        'AWS_SECURITY_TOKEN',
+        'AWS_SESSION_TOKEN',
+    )
+    async def __anit__(self, region='us-east-1', role=None):
+        await AWSCredentialProvider.__anit__(self)
+        self.role = None
+
+    def _getProvType(self):
+        return 'env'
+
+    async def _getCredentials(self):
+        # TODO: Support refreshableCredentials
+        if self.accesskeyid is None:
+            await self.updateCredentials()
+
+        ret = {
+            'token': self.token,
+            'accesskeyid': self.accesskeyid,
+            'secretaccesskey': self.secretaccesskey,
+        }
+        return ret
+
+    async def _updateCredentials(self):
+        self.accesskeyid = os.environ.get('AWS_ACCESS_KEY_ID')
+        self.secretaccesskey = os.environ.get('AWS_SECRET_ACCESS_KEY')
+        for envar in self.AWS_TOKEN_VARS:
+            valu = os.environ.get(envar)
+            if valu:
+                self.token = valu
+                break
+
+class AWSSigv4Signer:
+    METHODS_NO_BODY = {'DELETE', 'GET', 'HEAD'}
+    EMPTY_SHA256_HASH = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+    AWS4_REQUEST = 'aws4_request'
+    AWS4_ALGORITHM = 'AWS4-HMAC-SHA256'
+    TIMESTAMP_FMT = '%Y%m%dT%H%M%SZ'
+    def __init__(self,
+                 service: str,
+                 region: str,
+                 provider: AWSCredentialProvider,
+                 ):
+        self.service = service
+        self.region = region
+        self.provider = provider
+
+    async def signRequest(self,
+                          method: str,
+                          url: str,
+                          headers: typing.Optional[dict] = None,
+                          payloadhash: typing.Optional[str] = None,
+                          ):
+        cnfo = await self.provider.getCredentials()
+        token = cnfo.get('token')
+
+        secret_key = cnfo.get('secretaccesskey')
+        accesskeyid = cnfo.get('accesskeyid')
+
+        method = method.upper()
+
+        parsed_url = u_parse.urlsplit(url)
+
+        if headers is None:
+            headers = {}
+
+        if payloadhash is None:
+            if method in self.METHODS_NO_BODY:
+                payloadhash = self.EMPTY_SHA256_HASH
+            else:
+                mesg = f'payload hash required when method [{method=}] not in {self.METHODS_NO_BODY}'
+                raise AwsSigningError(mesg=mesg, method=method)
+
+        now = datetime.datetime.utcnow().strftime(self.TIMESTAMP_FMT)
+        additional_headers = {
+            "x-amz-content-sha256": payloadhash,
+            "x-amz-date": now
+        }
+
+        if token:
+            additional_headers['x-amz-security-token'] = token
+
+        canonical_headers = sorted(
+            {
+                'host': parsed_url.netloc,
+                **{key.lower(): valu for key, valu in headers.items()},
+                **{key.lower(): valu for key, valu in additional_headers.items()},
+            }.items()
+        )
+        signed_headers = ';'.join([k for k, _ in canonical_headers])
+
+        canonical_query = u_parse.urlencode(sorted(u_parse.parse_qsl(parsed_url.query)),
+                                            quote_via=u_parse.quote)
+
+        canonical_request = '\n'.join([
+            method,
+            parsed_url.path,
+            canonical_query,
+            '\n'.join('{}:{}'.format(key, valu) for key, valu in canonical_headers),
+            "",
+            signed_headers,
+            payloadhash,
+        ])
+
+        string_to_sign = '\n'.join([
+            self.AWS4_ALGORITHM,
+            now,
+            '/'.join((now[:8], self.region, self.service, self.AWS4_REQUEST)),
+            hashlib.sha256(canonical_request.encode()).hexdigest(),
+        ])
+
+        kdate = hmac.digest(f'AWS4{secret_key}'.encode(), now[:8].encode(), 'sha256')
+        kregion = hmac.digest(kdate, self.region.encode(), 'sha256')
+        kservice = hmac.digest(kregion, self.service.encode(), 'sha256')
+        ksignature = hmac.digest(kservice, self.AWS4_REQUEST.encode(), 'sha256')
+
+        signature = hmac.digest(ksignature, string_to_sign.encode(), 'sha256')
+        signature = s_common.ehex(signature)
+
+        credential = '/'.join((accesskeyid, now[:8], self.region, self.service, self.AWS4_REQUEST))
+
+        auth_header = f'{self.AWS4_ALGORITHM} Credential={credential}, SignedHeaders={signed_headers},' \
+                      f' Signature={signature}'
+
+        return {**additional_headers, 'Authorization': auth_header}
--- synapse/lib/stormlib/aws.py
+++ synapse/lib/stormlib/aws.py
@@ -1,3 +1,4 @@
+import copy
 import hmac
 
 import hashlib
@@ -10,7 +11,7 @@ import synapse.lib.stormtypes as s_stormtypes
 logger = logging.getLogger(__name__)
 
 @s_stormtypes.registry.registerType
-class CredsHelper(s_stormtypes.Prim):
+class AWSCredsHelper(s_stormtypes.Prim):
     _storm_typename = 'aws:creds'
 
     def __init__(self, provider, path=None):
@@ -23,7 +24,34 @@ class CredsHelper(s_stormtypes.Prim):
     async def _getCreds(self):
         return await self.provider.getCredentials()
 
-    # AWS V4 Signing logic goes here?
+@s_stormtypes.registry.registerType
+class AWSSigner(s_stormtypes.Prim):
+    _storm_typename = 'aws:signer'
+    def __init__(self, service, region, provider, path=None):
+        s_stormtypes.Prim.__init__(self, None)
+        self.service = service
+        self.region = region
+        self.signer = s_crypt_aws.AWSSigv4Signer(service, region, provider)
+        self.locls.update({
+            'sign': self._methSignature,
+        })
+
+    async def sign(self, meth, url, headers=None, payloadhash=None):
+        meth = await s_stormtypes.tostr(meth)
+        url = await s_stormtypes.tostr(url)
+        headers = await s_stormtypes.toprim(headers)  # TODO validate dict || none
+        payloadhash = await s_stormtypes.toprim(payloadhash)  # TODO validate str || none
+
+        if headers is None:
+            headers = {}
+        else:
+            headers = copy.deepcopy(headers)
+
+        headers.update(
+            await self.signer.signRequest(meth, url, headers, payloadhash=payloadhash)
+        )
+
+        return headers
 
 @s_stormtypes.registry.registerLib
 class LibAWS(s_stormtypes.Lib):
@@ -31,11 +59,22 @@ class LibAWS(s_stormtypes.Lib):
 
     def getObjLocals(self):
         return {
-            'creds': self._awscreds,
+            'creds': self._methAwsCreds,
+            'signer': self._methAwsSigner,
         }
 
-    async def _awscreds(self):
+    async def _methAwsCreds(self):
         # TODO This should normally come from a Cortex API or runtime API...
+        # TODO dictate parameters about the provider you want!
         provider = await s_crypt_aws.AWSEC2Provider.anit()
         self.runt.onfini(provider)
-        return CredsHelper(provider)
+        return AWSCredsHelper(provider)
+
+    async def _methAwsSigner(self, service, region, provider):
+        region = await s_stormtypes.tostr(region)
+        service = await s_stormtypes.tostr(service)
+        # The provider needs to be a AWSCredsHelper
+        if not isinstance(provider, AWSCredsHelper):
+            raise s_exc.BadArg(mesg=f'provider must be an {AWSCredsHelper._storm_typename} instance.',
+                               name='provider')
+        return AWSSigner(service, region, provider)
