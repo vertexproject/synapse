82e0a9709c7c383368ee5ab05bd00ecb17832ce4
visi <visi@vertex.link>
2023-08-28 14:41:34 -0400

    coverage

--- synapse/tests/test_lib_layer.py
+++ synapse/tests/test_lib_layer.py
@@ -1561,6 +1561,9 @@ class LayerTest(s_t_utils.SynTest):
             readlayr = core.getLayer(readlayrinfo.get('iden'))
             self.true(readlayr.readonly)
 
+            with self.raises(s_exc.IsReadOnly):
+                await readlayr.truncate()
+
     async def test_layer_ro(self):
         with self.getTestDir() as dirn:
             async with self.getTestCore(dirn=dirn) as core:
