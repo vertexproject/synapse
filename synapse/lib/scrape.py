0c44a097e8f06c261b66806153e9db3dbc924bbc
epiphyte <epiphyte@vertex.link>
2023-09-19 00:24:36 +0000

    Remove whitespace

--- synapse/lib/scrape.py
+++ synapse/lib/scrape.py
@@ -223,7 +223,6 @@ ipv6_regex = fr'''
 )
 
 '''
-
 # these must be ordered from most specific to least specific to allow first=True to work
 scrape_types = [  # type: ignore
     ('file:path', linux_path_regex, {'callback': linux_path_check, 'flags': regex.VERBOSE}),
