e5c0789d474f2e566f6edf8a40eb191308dbadbe
cisphyx <cisphyx@vertex.link>
2023-10-09 10:59:31 -0400

    tagprop *cmpr tests

--- synapse/tests/test_lib_ast.py
+++ synapse/tests/test_lib_ast.py
@@ -2589,8 +2589,10 @@ class AstTest(s_test.SynTest):
 
             self.len(2, await core.nodes('test:str +#taga*:score'))
             self.len(1, await core.nodes('test:str +#tagaa:score=5'))
+            self.len(1, await core.nodes('test:str +#tagaa:score*range=(4,6)'))
             self.len(2, await core.nodes('$tag=taga* test:str +#$tag:score'))
             self.len(1, await core.nodes('$tag=tagaa test:str +#$tag:score=5'))
+            self.len(1, await core.nodes('$tag=tagaa test:str +#$tag:score*range=(4,6)'))
 
             with self.raises(s_exc.StormRuntimeError):
                 await core.nodes('test:str +#taga*:score=2023')
