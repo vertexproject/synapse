1fb6fcb35e8b476df7755e44afdcaa45cacb4b24
cisphyx <cisphyx@vertex.link>
2023-10-09 09:36:12 -0400

    add a badtypevalu test

--- synapse/tests/test_cortex.py
+++ synapse/tests/test_cortex.py
@@ -2048,6 +2048,8 @@ class CortexTest(s_t_utils.SynTest):
                 await core.nodes('test:str +test:str:tick*near=newp')
             with self.raises(s_exc.NoSuchCmpr):
                 await core.nodes('test:str +#test*near=newp')
+            with self.raises(s_exc.BadTypeValu):
+                await core.nodes('test:str +#test*in=newp')
             with self.raises(s_exc.BadSyntax):
                 await core.nodes('test:str -> # } limit 10')
             with self.raises(s_exc.BadSyntax):
