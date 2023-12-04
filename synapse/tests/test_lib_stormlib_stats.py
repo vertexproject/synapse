ee9b5c1cde46ed17c66ed3404484d832028bac82
blackout <blackout@vertex.link>
2023-12-04 10:49:26 -0500

    SYN-6479: stats.countby option to sort by name rather than count
    feat: Add ``--by-name`` option to ``stats.countby`` to enable sorting on value name instead of value count

--- synapse/tests/test_lib_stormlib_stats.py
+++ synapse/tests/test_lib_stormlib_stats.py
@@ -4,54 +4,106 @@ import synapse.tests.utils as s_test
 import synapse.lib.stormlib.stats as s_stormlib_stats
 
 chartnorm = '''
-40 | 5 | ##################################################
-30 | 4 | ########################################
-20 | 3 | ##############################
+ 6 | 5 | ##################################################
+13 | 4 | ########################################
+ 3 | 3 | ##############################
 10 | 2 | ####################
+ 0 | 1 | ##########
+'''.strip()
+
+chartnorm_byname = '''
+13 | 4 | ########################################
+10 | 2 | ####################
+ 6 | 5 | ##################################################
+ 3 | 3 | ##############################
  0 | 1 | ##########
 '''.strip()
 
 chartrev = '''
  0 | 1 | ##########
 10 | 2 | ####################
-20 | 3 | ##############################
-30 | 4 | ########################################
-40 | 5 | ##################################################
+ 3 | 3 | ##############################
+13 | 4 | ########################################
+ 6 | 5 | ##################################################
+'''.strip()
+
+chartrev_byname = '''
+ 0 | 1 | ##########
+ 3 | 3 | ##############################
+ 6 | 5 | ##################################################
+10 | 2 | ####################
+13 | 4 | ########################################
 '''.strip()
 
 chartsize = '''
-40 | 5 | ##################################################
-30 | 4 | ########################################
-20 | 3 | ##############################
+ 6 | 5 | ##################################################
+13 | 4 | ########################################
+ 3 | 3 | ##############################
+'''.strip()
+
+chartsize_byname = '''
+13 | 4 | ########################################
+10 | 2 | ####################
+ 6 | 5 | ##################################################
 '''.strip()
 
 chartsizerev = '''
  0 | 1 | ##########
 10 | 2 | ####################
-20 | 3 | ##############################
+ 3 | 3 | ##############################
+'''.strip()
+
+chartsizerev_byname = '''
+0 | 1 | ##########
+3 | 3 | ##############################
+6 | 5 | ##################################################
 '''.strip()
 
 chartwidth = '''
-40 | 5 | ##########
-30 | 4 | ########
-20 | 3 | ######
+ 6 | 5 | ##########
+13 | 4 | ########
+ 3 | 3 | ######
 10 | 2 | ####
  0 | 1 | ##
 '''.strip()
 
+chartwidth_byname = '''
+13 | 4 | ########
+10 | 2 | ####
+ 6 | 5 | ##########
+ 3 | 3 | ######
+ 0 | 1 | ##
+'''.strip()
+
 chartlabelwidth = '''
-4 | 5 | ##################################################
-3 | 4 | ########################################
-2 | 3 | ##############################
+6 | 5 | ##################################################
+1 | 4 | ########################################
+3 | 3 | ##############################
 1 | 2 | ####################
 0 | 1 | ##########
 '''.strip()
 
+chartlabelwidth_byname = '''
+1 | 4 | ########################################
+1 | 2 | ####################
+6 | 5 | ##################################################
+3 | 3 | ##############################
+0 | 1 | ##########
+'''.strip()
+
 chartchar = '''
-40 | 5 | ++++++++++++++++++++++++++++++++++++++++++++++++++
-30 | 4 | ++++++++++++++++++++++++++++++++++++++++
-20 | 3 | ++++++++++++++++++++++++++++++
+ 6 | 5 | ++++++++++++++++++++++++++++++++++++++++++++++++++
+13 | 4 | ++++++++++++++++++++++++++++++++++++++++
+ 3 | 3 | ++++++++++++++++++++++++++++++
 10 | 2 | ++++++++++++++++++++
+ 0 | 1 | ++++++++++
+'''.strip()
+
+chartchar_byname = '''
+13 | 4 | ++++++++++++++++++++++++++++++++++++++++
+10 | 2 | ++++++++++++++++++++
+ 6 | 5 | ++++++++++++++++++++++++++++++++++++++++++++++++++
+ 3 | 3 | ++++++++++++++++++++++++++++++
  0 | 1 | ++++++++++
 '''.strip()
 
@@ -66,7 +118,7 @@ class StatsTest(s_test.SynTest):
             $i = (0)
             for $x in $lib.range(5) {
                 for $y in $lib.range(($x + 1)) {
-                    [ inet:ipv4=$i :asn=($x * 10) ]
+                    [ inet:ipv4=$i :asn=(($x * 10) % 17) ]
                     $i = ($i + 1)
                 }
             }
@@ -76,26 +128,50 @@ class StatsTest(s_test.SynTest):
             msgs = await core.stormlist('inet:ipv4 | stats.countby :asn')
             self.stormIsInPrint(chartnorm, msgs)
 
+            msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --by-name')
+            self.stormIsInPrint(chartnorm_byname, msgs)
+
             msgs = await core.stormlist('inet:ipv4 -> inet:asn | stats.countby')
             self.stormIsInPrint(chartnorm, msgs)
+
+            msgs = await core.stormlist('inet:ipv4 -> inet:asn | stats.countby --by-name')
+            self.stormIsInPrint(chartnorm_byname, msgs)
 
             msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --reverse')
             self.stormIsInPrint(chartrev, msgs)
 
+            msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --reverse --by-name')
+            self.stormIsInPrint(chartrev_byname, msgs)
+
             msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --size 3')
             self.stormIsInPrint(chartsize, msgs)
+
+            msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --size 3 --by-name')
+            self.stormIsInPrint(chartsize_byname, msgs)
 
             msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --size 3 --reverse')
             self.stormIsInPrint(chartsizerev, msgs)
 
+            msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --size 3 --reverse --by-name')
+            self.stormIsInPrint(chartsizerev_byname, msgs)
+
             msgs = await core.stormlist(f'inet:ipv4 | stats.countby :asn --bar-width 10')
             self.stormIsInPrint(chartwidth, msgs)
+
+            msgs = await core.stormlist(f'inet:ipv4 | stats.countby :asn --bar-width 10 --by-name')
+            self.stormIsInPrint(chartwidth_byname, msgs)
 
             msgs = await core.stormlist(f'inet:ipv4 | stats.countby :asn --label-max-width 1')
             self.stormIsInPrint(chartlabelwidth, msgs)
 
+            msgs = await core.stormlist(f'inet:ipv4 | stats.countby :asn --label-max-width 1 --by-name')
+            self.stormIsInPrint(chartlabelwidth_byname, msgs)
+
             msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --char "+"')
             self.stormIsInPrint(chartchar, msgs)
+
+            msgs = await core.stormlist('inet:ipv4 | stats.countby :asn --char "+" --by-name')
+            self.stormIsInPrint(chartchar_byname, msgs)
 
             msgs = await core.stormlist('stats.countby foo')
             self.stormIsInPrint('No values to display!', msgs)
