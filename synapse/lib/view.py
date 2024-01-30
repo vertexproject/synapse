4824230ede4543bf576337d16196b2cf55e9b2a7
rakuyo <james.gross@vertex.link>
2024-01-30 13:45:53 -0500

    tighten that up

--- synapse/lib/view.py
+++ synapse/lib/view.py
@@ -1387,11 +1387,7 @@ class View(s_nexus.Pusher):  # type: ignore
 
         root = await self.core.auth.getUserByName('root')
 
-        # preserve the created timestamp in case of move
-        created = tdef.get('created')
-        if created is None:
-            tdef['created'] = s_common.now()
-
+        tdef.setdefault('created', s_common.now())
         tdef.setdefault('user', root.iden)
         tdef.setdefault('async', False)
         tdef.setdefault('enabled', True)
