6f0c302732aca14ea7062674b7c4a371b2ab7d77
visi <visi@vertex.link>
2024-02-08 09:42:01 -0500

    Model updates 2024-02-08

--- synapse/models/risk.py
+++ synapse/models/risk.py
@@ -23,6 +23,15 @@ class CvssV3(s_types.Str):
             mesg = exc.get('mesg')
             raise s_exc.BadTypeValu(name=self.name, valu=text, mesg=mesg) from None
 
+alertstatus = (
+    (0, 'new'),
+    (10, 'enrichment'),
+    (20, 'todo'),
+    (30, 'analysis'),
+    (40, 'remediation'),
+    (50, 'done'),
+)
+
 class RiskModule(s_module.CoreModule):
 
     def getModelDefs(self):
@@ -281,17 +290,31 @@ class RiskModule(s_module.CoreModule):
 
                 )),
                 ('risk:mitigation', {}, (
+
                     ('vuln', ('risk:vuln', {}), {
                         'doc': 'The vulnerability that this mitigation addresses.'}),
+
                     ('name', ('str', {}), {
                         'doc': 'A brief name for this risk mitigation.'}),
+
                     ('desc', ('str', {}), {
                         'disp': {'hint': 'text'},
                         'doc': 'A description of the mitigation approach for the vulnerability.'}),
+
                     ('software', ('it:prod:softver', {}), {
                         'doc': 'A software version which implements a fix for the vulnerability.'}),
+
                     ('hardware', ('it:prod:hardware', {}), {
                         'doc': 'A hardware version which implements a fix for the vulnerability.'}),
+
+                    ('reporter', ('ou:org', {}), {
+                        'doc': 'The organization reporting on the mitigation.'}),
+
+                    ('reporter:name', ('ou:name', {}), {
+                        'doc': 'The name of the organization reporting on the mitigation.'}),
+
+                    ('tag', ('syn:tag', {}), {
+                        'doc': 'The tag used to annotate nodes which have the mitigation in place.'}),
                 )),
                 ('risk:vulnname', {}, ()),
                 ('risk:vuln:type:taxonomy', {}, ()),
@@ -598,6 +621,9 @@ class RiskModule(s_module.CoreModule):
                         'disp': {'hint': 'text'},
                         'doc': 'A free-form description / overview of the alert.'}),
 
+                    ('status', ('int', {'enums': alertstatus}), {
+                        'doc': 'The status of the alert.'}),
+
                     ('benign', ('bool', {}), {
                         'doc': 'Set to true if the alert has been confirmed benign. Set to false if malicious.'}),
 
@@ -610,6 +636,12 @@ class RiskModule(s_module.CoreModule):
                     ('verdict', ('risk:alert:verdict:taxonomy', {}), {
                         'ex': 'benign.false_positive',
                         'doc': 'A verdict about why the alert is malicious or benign, as a taxonomy entry.'}),
+
+                    ('assignee', ('syn:user', {}), {
+                        'doc': 'The synapse user who is assigned to investigate the alert.'}),
+
+                    ('ext:assignee', ('ps:contact', {}), {
+                        'doc': 'The alert assignee contact information from an external system.'}),
 
                     ('engine', ('it:prod:softver', {}), {
                         'doc': 'The software that generated the alert.'}),
--- synapse/tests/test_model_risk.py
+++ synapse/tests/test_model_risk.py
@@ -285,6 +285,9 @@ class RiskModelTest(s_t_utils.SynTest):
                     :detected=20501217
                     :attack=*
                     :vuln=*
+                    :status=todo
+                    :assignee=$lib.user.iden
+                    :ext:assignee = {[ ps:contact=* :email=visi@vertex.link ]}
                     :url=https://vertex.link/alerts/WOOT-20
                     :ext:id=WOOT-20
                     :engine={[ it:prod:softver=* :name=visiware ]}
@@ -294,6 +297,7 @@ class RiskModelTest(s_t_utils.SynTest):
                 ]
             ''')
             self.len(1, nodes)
+            self.eq(20, nodes[0].get('status'))
             self.eq(40, nodes[0].get('priority'))
             self.eq(50, nodes[0].get('severity'))
             self.eq('bazfaz.', nodes[0].get('type'))
@@ -302,7 +306,9 @@ class RiskModelTest(s_t_utils.SynTest):
             self.eq(2554848000000, nodes[0].get('detected'))
             self.eq('WOOT-20', nodes[0].get('ext:id'))
             self.eq('https://vertex.link/alerts/WOOT-20', nodes[0].get('url'))
+            self.eq(core.auth.rootuser.iden, nodes[0].get('assignee'))
             self.nn(nodes[0].get('host'))
+            self.nn(nodes[0].get('ext:assignee'))
             self.len(1, await core.nodes('risk:alert -> it:host'))
             self.len(1, await core.nodes('risk:alert -> risk:vuln'))
             self.len(1, await core.nodes('risk:alert -> risk:attack'))
@@ -504,9 +510,13 @@ class RiskModelTest(s_t_utils.SynTest):
                     :desc=BazFaz
                     :hardware=*
                     :software=*
+                    :reporter:name=vertex
+                    :reporter = { gen.ou.org vertex }
             ]''')
             self.eq('FooBar', nodes[0].props['name'])
             self.eq('BazFaz', nodes[0].props['desc'])
+            self.eq('vertex', nodes[0].props['reporter:name'])
+            self.nn(nodes[0].get('reporter'))
             self.len(1, await core.nodes('risk:mitigation -> risk:vuln'))
             self.len(1, await core.nodes('risk:mitigation -> it:prod:softver'))
             self.len(1, await core.nodes('risk:mitigation -> it:prod:hardware'))
