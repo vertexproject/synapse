9047f32fd92b7ebe378f642614386cbf1cec0d41
blackout <blackout@vertex.link>
2024-02-08 13:26:00 -0500

    SYN-6798: add aha info to tele:syn response
    feat: Add aha service name to the tele:syn message from the server if present.

--- synapse/telepath.py
+++ synapse/telepath.py
@@ -598,6 +598,7 @@ class Proxy(s_base.Base):
         self.tasks = {}
         self.shares = {}
 
+        self.ahainfo = {}
         self.sharinfo = {}
         self.methinfo = {}
 
@@ -902,6 +903,7 @@ class Proxy(s_base.Base):
             raise s_exc.LinkShutDown(mesg=mesg)
 
         self.sess = self.synack[1].get('sess')
+        self.ahainfo = self.synack[1].get('ahainfo', {})
         self.sharinfo = self.synack[1].get('sharinfo', {})
         self.methinfo = self.sharinfo.get('meths', {})
 
