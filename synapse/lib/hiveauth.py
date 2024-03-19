f4e7821169c52dbd11b1751f5a530f10b1ce2489
epiphyte <epiphyte@vertex.link>
2024-03-19 16:35:16 +0000

    Revert "fix a key issue"
    This reverts commit 58bd719b7110cf4996e5c340f0471af07adb0c48.

--- synapse/lib/hiveauth.py
+++ synapse/lib/hiveauth.py
@@ -996,7 +996,7 @@ class HiveUser(HiveRuler):
         A routine which will return a tuple of (allowed, info).
         '''
         perm = tuple(perm)
-        return self.allowedcache.get(perm)
+        return self.allowedcache.get((perm, default, gateiden))
 
     def _getAllowedReason(self, pkey):
         '''
