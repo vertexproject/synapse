4b49d8174e0cc8126d56bcb30909ddee78961ec2
blackout <blackout@vertex.link>
2024-05-06 15:51:49 -0400

    wip

--- synapse/tests/test_lib_cell.py
+++ synapse/tests/test_lib_cell.py
@@ -2658,11 +2658,11 @@ class CellTest(s_t_utils.SynTest):
 
         stream.seek(0)
         data = stream.read()
-        self.isin('The following sysctl values are not configured with the recommended values:', data)
+        self.isin('The following sysctl parameters are not configured with the recommended values:', data)
         for name, valu in sysvals.items():
             self.isin(f'  - {name}: Expected {valu}, got {valu - 1}.', data)
         self.isin('See https://synapse.docs.vertex.link/en/latest/synapse/devopsguide.html#performance-tuning', data)
-        self.isin('for additional information on each of these sysctl values and their recommended values.', data)
+        self.isin('for additional information on each of these sysctl parameters and their recommended values.', data)
 
         with self.getLoggerStream('synapse.lib.cell') as stream:
             async with self.getTestCell(s_cell.Cell):
@@ -2670,4 +2670,4 @@ class CellTest(s_t_utils.SynTest):
 
         stream.seek(0)
         data = stream.read()
-        self.notin('The following sysctl values are not configured with the recommended values:', data)
+        self.notin('The following sysctl parameters are not configured with the recommended values:', data)
