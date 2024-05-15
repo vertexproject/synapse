da06b97839f47c441512a8c08c262223b5e3e859
epiphyte <epiphyte@vertex.link>
2024-05-15 18:59:56 +0000

    Log svcname / version at startup (SYN-7344)

--- synapse/lib/cell.py
+++ synapse/lib/cell.py
@@ -3724,6 +3724,9 @@ class Cell(s_nexus.Pusher, s_telepath.Aware):
         logconf = s_common.setlogging(logger, defval=opts.log_level,
                                       structlog=opts.structured_logging)
 
+        logger.info(f'Starting {cls.getCellType()} version {cls.VERSTRING}, Synapse version: {s_version.verstring}',
+                    extra={'synapse': {'svc_version': cls.VERSTRING, 'synapse_version': s_version.verstring}})
+
         await cls._initBootRestore(opts.dirn)
 
         try:
