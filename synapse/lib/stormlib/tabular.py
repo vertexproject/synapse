303a09d044ba22b8c71dcee3f5aa64c0e699da4f
mikemoritz <mike.moritz@vertex.link>
2024-07-17 10:57:33 -0700

    more customization

--- synapse/lib/stormlib/tabular.py
+++ synapse/lib/stormlib/tabular.py
@@ -1,3 +1,4 @@
+import copy
 import textwrap
 import itertools
 
@@ -6,19 +7,38 @@ import synapse.common as s_common
 import synapse.lib.config as s_config
 import synapse.lib.stormtypes as s_stormtypes
 
-reqValidColumns = s_config.getJsValidator({
-    'type': 'array',
-    'items': {
-        'type': 'object',
-        'properties': {
-            'name': {'type': 'string'},
-            'width': {'type': 'number', 'exclusiveMinimum': 0},
-            'justify': {'type': 'string', 'default': 'left', 'enum': ['left', 'center', 'right']},
-            'overflow': {'type': 'string', 'default': 'trim', 'enum': ['wrap', 'trim']},
+reqValidTableConf = s_config.getJsValidator(schema := {
+    'type': 'object',
+    'properties': {
+        'separators': {
+            'type': 'object',
+            'properties': {
+                'row:outline': {'type': 'boolean', 'default': False},
+                'column:outline': {'type': 'boolean', 'default': False},
+                'header:row': {'type': 'string', 'default': '='},
+                'data:row': {'type': 'string', 'default': '-'},
+                'column': {'type': 'string', 'default': '|'},
+            },
+            'additionalProperties': False,
         },
-        'required': ['name', 'width'],
-        'minItems': 1,
+        'columns': {
+            'type': 'array',
+            'items': {
+                'type': 'object',
+                'properties': {
+                    'name': {'type': 'string', 'description': 'TODO'},  # todo: descriptions?
+                    'width': {'type': 'number', 'default': None, 'exclusiveMinimum': 0},
+                    'justify': {'type': 'string', 'default': 'left', 'enum': ['left', 'center', 'right']},
+                    'overflow': {'type': 'string', 'default': 'trim', 'enum': ['wrap', 'trim']},
+                },
+                'required': ['name'],
+                'minItems': 1,
+                'additionalProperties': False,
+            },
+        },
     },
+    'required': ['columns'],
+    'additionalProperties': False,
 })
 
 justers = {
@@ -40,20 +60,33 @@ class TabularLib(s_stormtypes.Lib):
     _storm_locals = (
         {'name': 'printer', 'desc': 'Construct a new printer.',
          'type': {'type': 'function', '_funcname': 'printer',
+                  'args': (
+                      {'name': 'conf', 'type': 'list',
+                       'desc': 'TODO'},
+                  ),
                   'returns': {'type': 'tabular:printer',
                               'desc': 'The newly constructed tabular:printer.'}}},
+        {'name': 'schema', 'desc': 'TODO',
+         'type': {'type': 'gtor', '_gtorfunc': '_gtorSchema',
+                  'returns': {'type': 'dict',
+                              'desc': 'TODO'}}},
     )
     _storm_lib_path = ('tabular',)
 
     def getObjLocals(self):
         return {
             'printer': self._methPrinter,
+            'schema': self._methSchema,
         }
 
-    async def _methPrinter(self, columns):
-        columns = await s_stormtypes.toprim(columns)
-        columns = reqValidColumns(columns)
-        return TabularPrinter(self.runt, columns)
+    async def _methSchema(self):
+        return copy.deepcopy(schema)
+
+    async def _methPrinter(self, conf):
+        conf = await s_stormtypes.toprim(conf)
+        conf.setdefault('separators', {})
+        conf = reqValidTableConf(conf)
+        return TabularPrinter(self.runt, conf)
 
 @s_stormtypes.registry.registerType
 class TabularPrinter(s_stormtypes.StormType):
@@ -78,35 +111,40 @@ class TabularPrinter(s_stormtypes.StormType):
          'desc': 'TODO',
          'type': {'type': 'function', '_funcname': 'header',
                   'returns': {'type': 'str', 'desc': 'TODO'}}},
-        {'name': 'separator',
-         'desc': 'TODO',
-         'type': {'type': 'function', '_funcname': 'separator',
-                  'args': (
-                      {'name': 'char', 'type': 'str', 'default': '-',
-                       'desc': 'TODO'},
-                  ),
-                  'returns': {'type': 'str', 'desc': 'TODO'}}},
     )
 
-    def __init__(self, runt, columns):
+    def __init__(self, runt, conf):
         s_stormtypes.StormType.__init__(self, None)
         self.runt = runt
-        self.columns = columns
-        self.colcnt = len(columns)
-        self.fillers = [' ' * coldef['width'] for coldef in columns]
+        self.conf = conf
+
+        self.firstrow = True
+
+        self.seprconf = conf['separators']
+
+        self.colconf = conf['columns']
+        self.colcnt = len(self.colconf)
+        self.colwidths = [coldef.get('width') or len(coldef['name']) for coldef in self.colconf]
 
         self.locls.update({
             'row': self.row,
             'header': self.header,
-            'separator': self.separator,
         })
 
-    def _makeRow(self, values):
+    def _makeRowItems(self, values: list):
+        '''
+        Justify, trim/wrap, and fill cells.
+        Returns list of string lines for each column.
+        '''
 
         items = []
-        for coldef, valu in zip(self.columns, values):
-            width = coldef['width']
+        for coldef, valu in zip(self.colconf, values):
+            width = coldef.get('width')
             valu = str(valu) if valu is not None else ''
+
+            if width is None:
+                items.append([valu])
+                continue
 
             if len(valu) <= width:
                 items.append([justers[coldef['justify']](valu, width)])
@@ -118,20 +156,34 @@ class TabularPrinter(s_stormtypes.StormType):
 
             items.append([justers[coldef['justify']](item, width) for item in textwrap.wrap(valu, width)])
 
-        printrows = []
+        rowitems = []
         for lineitems in itertools.zip_longest(*items, fillvalue=None):
-            printrows.append(f'| {" | ".join(item or self.fillers[i] for i, item in enumerate(lineitems))} |')
+            rowitems.append([item or self.colwidths[i] * ' ' for i, item in enumerate(lineitems)])
 
-        return '\n'.join(printrows)
+        return rowitems
 
-    async def separator(self, char='-'):
-        char = await s_stormtypes.tostr(char)
-        if len(char) != 1:
-            raise s_exc.BadArg(mesg='tabular:printer separator() requires char argument to be a single character')
-        return self._makeRow([None] * self.colcnt).replace(' ', char)
+    def _formatRowLine(self, lineitems, sepr, doends, pad=' '):
+        endstr = sepr if doends else ''
+        return f'{endstr}{pad}{f"{pad}{sepr}{pad}".join(lineitems)}{pad}{endstr}'
+
+    def _separator(self, colsepr, rowsepr, doends):
+        lineitems = (width * rowsepr for width in self.colwidths)
+        return self._formatRowLine(lineitems, colsepr, doends, pad=rowsepr)
 
     async def header(self):
-        return self._makeRow([col['name'] for col in self.columns])
+        colsepr = self.seprconf['column']  # fixme: dont need to pass this around
+        colends = self.seprconf['column:outline']  # fixme: don't need to pass this around
+
+        # fixme: change to _makeRowStrs(values, colsepr, colends)
+        rowitems = self._makeRowItems([col['name'] for col in self.colconf])
+        rowstrs = [self._formatRowLine(lineitems, colsepr, colends) for lineitems in rowitems]
+
+        if rowsepr := self.seprconf['header:row']:
+            rowstrs.append(seprstr := self._separator(colsepr, rowsepr, colends))
+            if self.seprconf['row:outline']:
+                rowstrs.insert(0, seprstr)
+
+        return '\n'.join(rowstrs)
 
     async def row(self, data):
         data = await s_stormtypes.toprim(data)
@@ -143,4 +195,20 @@ class TabularPrinter(s_stormtypes.StormType):
             mesg = 'tabular:printer row() requires data length to equal column count'
             raise s_exc.BadArg(mesg=mesg, length=len(data), column_length=self.colcnt)
 
-        return self._makeRow(data)
+        colsepr = self.seprconf['column']  # fixme: don't need to pass this around
+        colends = self.seprconf['column:outline']  # fixme: don't need to pass this around
+
+        # fixme: change to _makeRowStrs(values, colsepr, colends)
+        rowitems = self._makeRowItems(data)
+        rowstrs = [self._formatRowLine(lineitems, colsepr, colends) for lineitems in rowitems]
+
+        if rowsepr := self.seprconf['data:row']:
+            seprstr = self._separator(colsepr, rowsepr, colends)
+            if self.seprconf['row:outline']:
+                rowstrs.append(seprstr)
+            elif self.firstrow:
+                self.firstrow = False
+            else:
+                rowstrs.insert(0, seprstr)
+
+        return '\n'.join(rowstrs)
--- synapse/tests/test_lib_stormlib_tabular.py
+++ synapse/tests/test_lib_stormlib_tabular.py
@@ -21,67 +21,150 @@ class TabularTest(s_test.SynTest):
                 },
             }
 
-            # single line
+            # get schema
+
+            mesgs = await core.stormlist('$lib.pprint($lib.tabular.schema())')
+            self.stormHasNoWarnErr(mesgs)
+            # todo
+
+            # single line defaults
 
             mesgs = await core.stormlist('''
-                $printer = $lib.tabular.printer(([
-                    {"name": "Year", "width": 6, "justify": "right"},
-                    {"name": "Author", "width": 20, "justify": "center"},
-                    {"name": "Title", "width": 12},
-                ]))
+                $printer = $lib.tabular.printer(({
+                    "columns": [
+                        {"name": "Year", "width": 6, "justify": "right"},
+                        {"name": "Author", "width": 20, "justify": "center"},
+                        {"name": "Title", "width": 12},
+                    ]
+                }))
 
                 $lib.print($printer.header())
-                $lib.print($printer.separator())
                 for $row in $rows {
                     $lib.print($printer.row($row))
                 }
             ''', opts=opts)
             self.stormHasNoWarnErr(mesgs)
             self.eq([
-                '|   Year |        Author        | Title        |',
-                '|--------|----------------------|--------------|',
-                '|   1973 |     Issac Asimov     | The Gods ... |',
-                '|   1974 |   Arthur C. Clarke   | Rendezvou... |',
-                '|   1975 |  Ursula K. Le Guin   | The Dispo... |',
-                '|   1976 |     Joe Haldeman     | The Forev... |',
+                '   Year |        Author        | Title        ',
+                '========|======================|==============',
+                '   1973 |     Issac Asimov     | The Gods ... ',
+                '--------|----------------------|--------------',
+                '   1974 |   Arthur C. Clarke   | Rendezvou... ',
+                '--------|----------------------|--------------',
+                '   1975 |  Ursula K. Le Guin   | The Dispo... ',
+                '--------|----------------------|--------------',
+                '   1976 |     Joe Haldeman     | The Forev... ',
+            ], printlines(mesgs))
+
+            # custom separators
+
+            mesgs = await core.stormlist('''
+                $printer = $lib.tabular.printer(({
+                    "separators": {
+                        "row:outline": true,
+                        "column:outline": true,
+                        "header:row": "#",
+                        "data:row": "*",
+                        "column": "+",
+                    },
+                    "columns": [
+                        {"name": "Year", "width": 6, "justify": "right"},
+                        {"name": "Author", "width": 20, "justify": "center"},
+                        {"name": "Title", "width": 12},
+                    ]
+                }))
+
+                $lib.print($printer.header())
+                for $row in $rows {
+                    $lib.print($printer.row($row))
+                }
+            ''', opts=opts)
+            self.stormHasNoWarnErr(mesgs)
+            self.eq([
+                '+########+######################+##############+',
+                '+   Year +        Author        + Title        +',
+                '+########+######################+##############+',
+                '+   1973 +     Issac Asimov     + The Gods ... +',
+                '+********+**********************+**************+',
+                '+   1974 +   Arthur C. Clarke   + Rendezvou... +',
+                '+********+**********************+**************+',
+                '+   1975 +  Ursula K. Le Guin   + The Dispo... +',
+                '+********+**********************+**************+',
+                '+   1976 +     Joe Haldeman     + The Forev... +',
+                '+********+**********************+**************+',
+            ], printlines(mesgs))
+
+            # no col separators or end width
+
+            mesgs = await core.stormlist('''
+                $printer = $lib.tabular.printer(({
+                    "separators": {
+                        "header:row": "",
+                        "data:row": "",
+                        "column": "",
+                    },
+                    "columns": [
+                        {"name": "Year", "width": 6, "justify": "right"},
+                        {"name": "Author", "width": 20, "justify": "center"},
+                        {"name": "Title"},
+                    ]
+                }))
+
+                $lib.print($printer.header())
+                for $row in $rows {
+                    $lib.print($printer.row($row))
+                }
+            ''', opts=opts)
+            self.stormHasNoWarnErr(mesgs)
+            self.eq([
+                '   Year         Author         Title ',
+                '   1973      Issac Asimov      The Gods Themselves ',
+                '   1974    Arthur C. Clarke    Rendezvous with Rama ',
+                '   1975   Ursula K. Le Guin    The Dispossessed ',
+                '   1976      Joe Haldeman      The Forever War ',
             ], printlines(mesgs))
 
             # wraps
 
             mesgs = await core.stormlist('''
-                $printer = $lib.tabular.printer(([
-                    {"name": "Year", "width": 6, "justify": "right"},
-                    {"name": "Author", "width": 10, "justify": "center", "overflow": "wrap"},
-                    {"name": "Title", "width": 10, "overflow": "wrap"},
-                ]))
+                $printer = $lib.tabular.printer(({
+                    "columns": [
+                        {"name": "Year", "width": 6, "justify": "right"},
+                        {"name": "Author", "width": 10, "justify": "center", "overflow": "wrap"},
+                        {"name": "Title", "width": 10, "overflow": "wrap"},
+                    ]
+                }))
 
                 $lib.print($printer.header())
-                $lib.print($printer.separator(char="="))
                 for $row in $rows {
                     $lib.print($printer.row($row))
                 }
             ''', opts=opts)
             self.stormHasNoWarnErr(mesgs)
             self.eq([
-                '|   Year |   Author   | Title      |',
-                '|========|============|============|',
-                '|   1973 |   Issac    | The Gods   |',
-                '|        |   Asimov   | Themselves |',
-                '|   1974 | Arthur C.  | Rendezvous |',
-                '|        |   Clarke   | with Rama  |',
-                '|   1975 | Ursula K.  | The Dispos |',
-                '|        |  Le Guin   | sessed     |',
-                '|   1976 |    Joe     | The        |',
-                '|        |  Haldeman  | Forever    |',
-                '|        |            | War        |',
+                '   Year |   Author   | Title      ',
+                '========|============|============',
+                '   1973 |   Issac    | The Gods   ',
+                '        |   Asimov   | Themselves ',
+                '--------|------------|------------',
+                '   1974 | Arthur C.  | Rendezvous ',
+                '        |   Clarke   | with Rama  ',
+                '--------|------------|------------',
+                '   1975 | Ursula K.  | The Dispos ',
+                '        |  Le Guin   | sessed     ',
+                '--------|------------|------------',
+                '   1976 |    Joe     | The        ',
+                '        |  Haldeman  | Forever    ',
+                '        |            | War        ',
             ], printlines(mesgs))
 
-            # other cases
+            # reprs
 
             mesgs = await core.stormlist('''
-                $printer = $lib.tabular.printer(([
-                    {"name": "Foo", "width": 20},
-                ]))
+                $printer = $lib.tabular.printer(({
+                    "separators": {"header:row": "", "data:row": ""},
+                    "columns": [{"name": "Foo", "width": 20}],
+                }))
 
                 $lib.print($printer.row(($lib.null,)))
                 $lib.print($printer.row((({"bar": "baz"}),)))
@@ -89,40 +172,30 @@ class TabularTest(s_test.SynTest):
             ''', opts=opts)
             self.stormHasNoWarnErr(mesgs)
             self.eq([
-                "|                      |",
-                "| {'bar': 'baz'}       |",
-                "| ps:name=cool         |",
+                "                      ",
+                " {'bar': 'baz'}       ",
+                " ps:name=cool         ",
             ], printlines(mesgs))
 
             # sad
 
             with self.raises(s_exc.SchemaViolation):
-                await core.nodes('$lib.tabular.printer(([{"name": "newp", "width": -43}]))')
+                await core.nodes('$lib.tabular.printer(({"columns": [{"name": "newp", "width": -43}]}))')
 
             with self.raises(s_exc.BadArg) as ecm:
                 await core.nodes('''
-                    $printer = $lib.tabular.printer(([
-                        {"name": "Foo", "width": 20},
-                    ]))
-                    $printer.separator(char="newp")
-                ''')
-                expmesg = 'tabular:printer separator() requires char argument to be a single character'
-                self.eq(expmesg, ecm.exception.errinfo['mesg'])
-
-            with self.raises(s_exc.BadArg) as ecm:
-                await core.nodes('''
-                    $printer = $lib.tabular.printer(([
-                        {"name": "Foo", "width": 20},
-                    ]))
+                    $printer = $lib.tabular.printer(({
+                        "columns": [{"name": "Foo", "width": 20}],
+                    }))
                     $printer.row(({"yeah": "no"}))
                 ''')
             self.eq('tabular:printer row() requires a list argument', ecm.exception.errinfo['mesg'])
 
             with self.raises(s_exc.BadArg) as ecm:
                 await core.nodes('''
-                    $printer = $lib.tabular.printer(([
-                        {"name": "Foo", "width": 20},
-                    ]))
+                    $printer = $lib.tabular.printer(({
+                        "columns": [{"name": "Foo", "width": 20}],
+                    }))
                     $printer.row((too, many, items))
                 ''')
             self.eq('tabular:printer row() requires data length to equal column count', ecm.exception.errinfo['mesg'])
