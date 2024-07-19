a33e875b7321da8aa3b947171acc8050ad217284
blackout <blackout@vertex.link>
2024-07-19 13:03:47 -0400

    wip

--- synapse/lib/spooled.py
+++ synapse/lib/spooled.py
@@ -185,7 +185,3 @@ class Dict(Spooled):
 
         for item in list(self.realdict.items()):
             yield item
-
-    async def update(self, other):
-        for key, valu in other.items():
-            await self.set(key, valu)
