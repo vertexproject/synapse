f8ee82afc1bcd9bfcfda21f39080bbfd5a9b2915
blackout <blackout@vertex.link>
2024-08-01 12:29:59 -0400

    SYN-7689, SYN-7684: `biz:deal:id` and `inet:service:message:type` (#3832)

--- synapse/models/inet.py
+++ synapse/models/inet.py
@@ -1576,6 +1576,10 @@ class InetModule(s_module.CoreModule):
                     ('inet:service:message:attachment', ('guid', {}), {
                         'doc': 'A file attachment included within a message.'}),
 
+                    ('inet:service:message:type:taxonomy', ('taxonomy', {}), {
+                        'interfaces': ('meta:taxonomy',),
+                        'doc': 'A message type taxonomy.'}),
+
                     ('inet:service:access', ('guid', {}), {
                         'interfaces': ('inet:service:action',),
                         'doc': 'Represents a user access request to a service resource.'}),
@@ -3645,6 +3649,7 @@ class InetModule(s_module.CoreModule):
                         # TODO ndef based auth proto details
                     )),
 
+                    ('inet:service:message:type:taxonomy', {}, ()),
                     ('inet:service:message', {}, (
 
                         ('account', ('inet:service:account', {}), {
@@ -3704,6 +3709,9 @@ class InetModule(s_module.CoreModule):
 
                         ('file', ('file:bytes', {}), {
                             'doc': 'The raw file that the message was extracted from.'}),
+
+                        ('type', ('inet:service:message:type:taxonomy', {}), {
+                            'doc': 'The type of message.'}),
                     )),
 
                     ('inet:service:message:link', {}, (
