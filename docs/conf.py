336f44a89b429c0fd4f0e461c9ed77b74c84f0fb
epiphyte <epiphyte@vertex.link>
2024-08-05 19:06:06 +0000

    Remove RTD change

--- docs/conf.py
+++ docs/conf.py
@@ -194,15 +194,6 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-# Define the canonical URL if you are using a custom domain on Read the Docs
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
-
-# Tell Jinja2 templates the build is running on Read the Docs
-if os.environ.get("READTHEDOCS", "") == "True":
-    if "html_context" not in globals():
-        html_context = {}
-    html_context["READTHEDOCS"] = True
-
 # Our magic
 def run_apidoc(_):
     from sphinx.ext.apidoc import main
