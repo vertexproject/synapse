e7ac75d5ee2ffda03d9e909dd2a44299036c3495
cisphyx <cisphyx@vertex.link>
2024-08-14 13:02:12 -0400

    add test

--- synapse/tests/test_lib_types.py
+++ synapse/tests/test_lib_types.py
@@ -1607,8 +1607,11 @@ class TypesTest(s_t_utils.SynTest):
             core.getLayer()._testAddPropArrayIndx(buid, 'test:int', '_hehe', ('newp' * 100,))
             self.len(0, await core.nodes('test:int:_hehe*[~=newp]'))
 
-    async def test_typehash(self):
+    async def test_types_typehash(self):
         async with self.getTestCore() as core:
             self.true(core.model.form('inet:fqdn').type.typehash is core.model.prop('inet:dns:a:fqdn').type.typehash)
             self.true(core.model.form('it:prod:softname').type.typehash is core.model.prop('it:network:name').type.typehash)
             self.true(core.model.form('inet:asn').type.typehash is not core.model.prop('inet:proto:port').type.typehash)
+
+            self.true(s_common.isguid(core.model.form('inet:fqdn').type.typehash))
+            self.true(s_common.isguid(core.model.form('inet:fqdn').typehash))
