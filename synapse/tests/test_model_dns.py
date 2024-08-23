fc7e48ffe933a734f0443a8ec0f9a7ba3ceefc78
epiphyte <epiphyte@vertex.link>
2024-08-23 14:06:48 +0000

    Add non-enum example

--- synapse/tests/test_model_dns.py
+++ synapse/tests/test_model_dns.py
@@ -110,6 +110,11 @@ class DnsModelTest(s_t_utils.SynTest):
 
             nodes = await core.nodes('[inet:dns:request=* :reply:code=NXDOMAIN]')
             self.eq(nodes[0].get('reply:code'), 3)
+            self.eq(nodes[0].repr('reply:code'), 'NXDOMAIN')
+
+            nodes = await core.nodes('[inet:dns:request=* :reply:code=1138]')
+            self.eq(nodes[0].get('reply:code'), 1138)
+            self.eq(nodes[0].repr('reply:code'), '1138')
 
             nodes = await core.nodes('[inet:dns:query=("tcp://1.2.3.4", 4.3.2.1.in-addr.arpa, 255)]')
             self.len(1, nodes)
