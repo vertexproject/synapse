d99c69b9a4643ebdb55bd75a13c3b9e36884451c
epiphyte <epiphyte@vertex.link>
2024-09-05 14:17:08 +0000

    Add new assets to package data, protect against directory traversal and non-existent requests

--- synapse/assets/__init__.py
+++ synapse/assets/__init__.py
@@ -1,7 +1,9 @@
 import os
+import logging
 
 import synapse.common as s_common
 
+logger = logging.getLogger(__name__)
 dirname = os.path.dirname(__file__)
 
 def getStorm(*names):
@@ -17,6 +19,18 @@ def getStorm(*names):
         text = storm.get('migrations', 'model-0.2.28.storm')
         await core.callStorm(text)
     '''
-    with s_common.genfile(dirname, 'storm', *names) as fp:
-        text = fp.read()
+    fp = getAssetPath('storm', *names)
+    with s_common.genfile(fp) as fd:
+        text = fd.read()
     return text.decode('utf8')
+
+def getAssetPath(*names):
+    fp = s_common.genpath(dirname, *names)
+    absfp = os.path.abspath(fp)
+    if not absfp.startswith(dirname):
+        logger.error(f'{absfp} is not in {dirname}')
+        raise ValueError(f'Path escaping detected for {names}')
+    if not os.path.isfile(absfp):
+        logger.error('{} does not exist'.format(absfp))
+        raise ValueError(f'Asset does not exist for {names}')
+    return absfp
