4a0bf5457c5b010976b8ddb3398060829d251578
epiphyte <epiphyte@vertex.link>
2024-10-08 16:55:47 +0000

    Add $lib.random.seed() (SYN-8077)

--- synapse/lib/stormlib/random.py
+++ synapse/lib/stormlib/random.py
@@ -5,6 +5,48 @@ import synapse.lib.stormtypes as s_stormtypes
 
 randinst = random.SystemRandom()
 
+
+@s_stormtypes.registry.registerType
+class Random(s_stormtypes.StormType):
+    '''
+    A seeded random number generator object.
+    '''
+    _storm_typename = 'random'
+    _storm_locals = (
+        {'name': 'seed', 'desc': 'The seed used to make the generator.', 'type': 'str', },
+        {'name': 'int', 'desc': 'Generate a random integer.',
+         'type': {'type': 'function', '_funcname': '_methInt',
+                  'args': (
+                      {'name': 'maxval', 'type': 'int', 'desc': 'The maximum random value.'},
+                      {'name': 'minval', 'type': 'int', 'desc': 'The minimum random value.', 'default': 0},
+                  ),
+                  'returns': {'type': 'int', 'desc': 'A random integer in the range min-max inclusive.'}}},
+    )
+    _ismutable = False
+
+    def __init__(self, runt, seed: str):
+        s_stormtypes.StormType.__init__(self)
+        self.runt = runt
+        self._seed = seed
+        self.robj = random.Random()
+        self.robj.seed(self._seed, version=2)
+        self.locls.update(self.getObjLocals())
+
+    def getObjLocals(self):
+        return {
+            'int': self._methInt,
+            'seed': self._seed,
+        }
+
+    @s_stormtypes.stormfunc(readonly=True)
+    async def _methInt(self, maxval, minval=0):
+        maxval = await s_stormtypes.toint(maxval)
+        minval = await s_stormtypes.toint(minval)
+        if minval > maxval:
+            raise s_exc.BadArg(mesg=f'Minval must be less than or equal to maxval, minval={minval}, maxval={maxval}',
+                               minval=minval, maxval=maxval)
+        return self.robj.randint(minval, maxval)
+
 @s_stormtypes.registry.registerLib
 class LibRandom(s_stormtypes.Lib):
     '''
@@ -12,22 +54,38 @@ class LibRandom(s_stormtypes.Lib):
     '''
     _storm_locals = (
         {'name': 'int', 'desc': 'Generate a random integer.',
-         'type': {'type': 'function', '_funcname': '_int',
+         'type': {'type': 'function', '_funcname': '_methInt',
                   'args': (
                       {'name': 'maxval', 'type': 'int', 'desc': 'The maximum random value.'},
                       {'name': 'minval', 'type': 'int', 'desc': 'The minimum random value.', 'default': 0},
                   ),
                   'returns': {'type': 'int', 'desc': 'A random integer in the range min-max inclusive.'}}},
+        {'name': 'seed', 'desc': 'Make a random generator with a given seed.',
+         'type': {'type': 'function', '_funcname': '_methSeed',
+                  'args': (
+                      {'name': 'valu', 'type': 'str', 'desc': 'The seed value used for the random generator.'},
+                  ),
+                  'returns': {'type': 'random', 'desc': 'The random generator object.'}}
+        }
     )
     _storm_lib_path = ('random',)
 
     def getObjLocals(self):
         return {
-            'int': self._int,
+            'int': self._methInt,
+            'seed': self._methSeed,
         }
 
+    def _storm_copy(self):
+        pass
+
     @s_stormtypes.stormfunc(readonly=True)
-    async def _int(self, maxval, minval=0):
+    async def _methSeed(self, valu):
+        valu = await s_stormtypes.tostr(valu)
+        return Random(self.runt, valu)
+
+    @s_stormtypes.stormfunc(readonly=True)
+    async def _methInt(self, maxval, minval=0):
         maxval = await s_stormtypes.toint(maxval)
         minval = await s_stormtypes.toint(minval)
         if minval > maxval:
--- synapse/tests/test_lib_stormlib_random.py
+++ synapse/tests/test_lib_stormlib_random.py
@@ -20,3 +20,38 @@ class TestLibStormRandom(s_test.SynTest):
 
             with self.raises(s_exc.BadArg):
                 await core.callStorm('return($lib.random.int(maxval=0, minval=1))')
+
+    async def test_stormlib_random_seed(self):
+        async with self.getTestCore() as core:
+            q = '''$r=$lib.random.seed(myCoolTestSeed)
+            return(($r.int(10), $r.int(10), $r.int(10)))
+            '''
+            valu = await core.callStorm(q)
+            self.eq(valu, (5, 9, 1))
+
+            new_valu = await core.callStorm(q)
+            self.eq(valu, new_valu)
+
+            q = '''$r=$lib.random.seed(myCoolTestSeed)
+            $r2 = $lib.random.seed($r.seed)
+            return(($r.int(10), $r2.int(10)))
+            '''
+            valu = await core.callStorm(q)
+            self.eq(valu, (5, 5))
+
+            q = '''return($lib.vars.type($lib.random.seed(x)))'''
+            valu = await core.callStorm(q)
+            self.eq(valu, 'random')
+
+            # Seeds are stringified
+            q = '''$r=$lib.random.seed(1234567890)
+            $r2 = $lib.random.seed((1234567890))
+            return(($r.int(10), $r2.int(10), $r.seed, $r2.seed))
+            '''
+            valu = await core.callStorm(q)
+            self.eq(valu, (5, 5, '1234567890', '1234567890'))
+
+            # Empty string value is still a str
+            q = '''$r=$lib.random.seed('') return ($r.int(10))'''
+            valu = await core.callStorm(q)
+            self.eq(valu, 7)
