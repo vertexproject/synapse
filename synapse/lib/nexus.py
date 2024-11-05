5e3167657cd17512ca4d7d2a538bd44e9068b66b
cisphyx <cisphyx@vertex.link>
2024-11-04 21:02:16 -0500

    move to eat()

--- synapse/lib/nexus.py
+++ synapse/lib/nexus.py
@@ -341,6 +341,12 @@ class NexsRoot(s_base.Base):
         if meta is None:
             meta = {}
 
+        if (nexus := self._nexskids.get(nexsiden)) is None:
+            raise s_exc.NoSuchName(mesg=f'No Nexus Pusher with iden {nexsiden}.', name=nexsiden)
+
+        if event not in nexus._nexshands:
+            raise s_exc.NoSuchName(mesg=f'No Nexus handler for event {event}.', name=event)
+
         async with self.cell.nexslock:
             self.reqNotReadOnly()
             # Keep a reference to the shielded task to ensure it isn't GC'd
@@ -379,13 +385,6 @@ class NexsRoot(s_base.Base):
     async def _eat(self, item, indx=None):
 
         if self.donexslog:
-            nexsiden, event = item[:2]
-            if (nexus := self._nexskids.get(nexsiden)) is None:
-                raise s_exc.NoSuchName(mesg=f'No Nexus Pusher with iden {nexsiden}.', name=nexsiden)
-
-            if event not in nexus._nexshands:
-                raise s_exc.NoSuchName(mesg=f'No Nexus handler for event {event}.', name=event)
-
             saveindx = await self.nexslog.add(item, indx=indx)
             [dist.update() for dist in tuple(self._mirrors)]
 
