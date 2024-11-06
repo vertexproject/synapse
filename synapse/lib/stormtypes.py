aa04d5132e73b763622a5b7cf48a4a939332dbe5
cisphyx <cisphyx@vertex.link>
2024-11-06 14:04:39 -0500

    Stringify iindividual list values in HTTP request param dicts rather than the whole list (SYN-7650)

--- synapse/lib/stormtypes.py
+++ synapse/lib/stormtypes.py
@@ -60,6 +60,19 @@ def confirmEasyPerm(item, perm, mesg=None):
 def allowedEasyPerm(item, perm):
     return s_scope.get('runt').allowedEasyPerm(item, perm)
 
+def strifyHttpArg(item, multi=False):
+    if isinstance(item, (list, tuple)):
+        return [(str(k), str(v)) for (k, v) in item]
+    elif isinstance(item, dict):
+        retn = {}
+        for name, valu in item.items():
+            if isinstance(valu, (list, tuple)) and multi:
+                retn[str(name)] = [str(v) for v in valu]
+            else:
+                retn[str(name)] = str(valu)
+        return retn
+    return item
+
 class StormTypesRegistry:
     # The following types are currently undefined.
     base_undefined_types = (
@@ -2306,13 +2319,6 @@ class LibAxon(Lib):
             'hashset': self.hashset,
         }
 
-    def strify(self, item):
-        if isinstance(item, (list, tuple)):
-            return [(str(k), str(v)) for (k, v) in item]
-        elif isinstance(item, dict):
-            return {str(k): str(v) for k, v in item.items()}
-        return item
-
     @stormfunc(readonly=True)
     async def readlines(self, sha256, errors='ignore'):
         if not self.runt.allowed(('axon', 'get')):
@@ -2384,8 +2390,8 @@ class LibAxon(Lib):
         if proxy is not None:
             self.runt.confirm(('storm', 'lib', 'inet', 'http', 'proxy'))
 
-        params = self.strify(params)
-        headers = self.strify(headers)
+        params = strifyHttpArg(params, multi=True)
+        headers = strifyHttpArg(headers)
 
         await self.runt.snap.core.getAxon()
 
@@ -2423,8 +2429,8 @@ class LibAxon(Lib):
         timeout = await toprim(timeout)
         ssl_opts = await toprim(ssl_opts)
 
-        params = self.strify(params)
-        headers = self.strify(headers)
+        params = strifyHttpArg(params, multi=True)
+        headers = strifyHttpArg(headers)
 
         if proxy is not None:
             self.runt.confirm(('storm', 'lib', 'inet', 'http', 'proxy'))
