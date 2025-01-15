74d8150eb038f864be0ad63987b44462d23288a9
blackout <blackout@vertex.link>
2025-01-15 02:00:58 -0500

    wip

--- conftest.py
+++ conftest.py
@@ -1,31 +1,34 @@
 import os
 import sys
-import json
-import pathlib
+import warnings
 
-_events = {}
+# Most of these bind to a random port but because it's not zero, it would emit
+# the warning so just ignore these
+allowed = [
+    'synapse/tests/test_lib_aha.py::AhaTest::test_aha_clone',
+    'synapse/tests/test_lib_aha.py::AhaTest::test_aha_restart',
+    'synapse/tests/test_lib_aha.py::AhaTest::test_lib_aha_offon',
+    'synapse/tests/test_lib_storm.py::StormTest::test_storm_pushpull',
+    'synapse/tests/test_lib_stormsvc.py::StormSvcTest::test_storm_svc_restarts',
+
+    # This test checks binding on port 27492 on purpose
+    'synapse/tests/test_telepath.py::TeleTest::test_telepath_default_port',
+]
 
 def audithook(event, args):
     if event == 'socket.bind':
-        testname = os.environ.get('PYTEST_CURRENT_TEST')
-        _events.setdefault(testname, [])
-        sock, addr = args
-        if isinstance(addr, (list, tuple)) and (port := addr[1]) != 0:
-            raise RuntimeError(f'socket.bind() to port {port}')
-        _events[testname].append(addr)
+        _, addr = args
+        if isinstance(addr, tuple) and (port := addr[1]) != 0:
+
+            if (testname := os.environ.get('PYTEST_CURRENT_TEST')) is None:
+                testname = '<unknown>'
+
+            else:
+                testname = testname.split(' ')[0]
+                if testname in allowed:
+                    return
+
+            warnings.warn(f'Synapse tests should not bind to fixed ports: {testname=} {port=}')
 
 def pytest_sessionstart(session):
     sys.addaudithook(audithook)
-
-def pytest_sessionfinish(session, exitstatus):
-
-    dirn = pathlib.Path('test-reports')
-    dirn.mkdir(exist_ok=True)
-
-    if (workerid := os.environ.get('PYTEST_XDIST_WORKER')) is not None:
-        filename = dirn / f'socket.bind.{workerid}.json'
-    else:
-        filename = dirn / 'socket.bind.json'
-
-    with filename.open('w') as fp:
-        json.dump(_events, fp)
