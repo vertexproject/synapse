a3dd8d24db56c68762d21cdf657a0bbc5caafeb6
invisig0th <visi@vertex.link>
2025-02-04 13:10:34 -0500

    Update synapse/lib/stormlib/index.py
    Co-authored-by: Cisphyx <cisphyx@vertex.link>

--- synapse/lib/stormlib/index.py
+++ synapse/lib/stormlib/index.py
@@ -11,8 +11,8 @@ stormcmds = (
                 // Display the number of file:path:ext properties in the view.
                 index.count.prop file:path:ext
 
-                # display the number of file:path:ext properties with the value
-                # "exe" in the view.
+                // Display the number of file:path:ext properties with the value
+                // "exe" in the view.
                 index.count.prop file:path:ext --value exe
 
         ''',
