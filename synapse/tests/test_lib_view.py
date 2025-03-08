224ae7371d857607e92222dbbab23039c78070e7
blackout <blackout@vertex.link>
2025-03-08 15:16:46 -0500

    wip

--- synapse/tests/test_lib_view.py
+++ synapse/tests/test_lib_view.py
@@ -906,6 +906,13 @@ class ViewTest(s_t_utils.SynTest):
             with self.raises(s_exc.BadState):
                 await core.callStorm('return($lib.view.get().insertParentFork().iden)')
 
+            vdef = await view01.insertParentFork(visi.iden)
+            self.eq(vdef.get('name'), 'fork of default')
+
+            piden = view03.parent.iden
+            vdef = await view03.insertParentFork(visi.iden)
+            self.eq(vdef.get('name'), f'fork of {piden}')
+
     async def test_view_children(self):
 
         async with self.getTestCore() as core:
