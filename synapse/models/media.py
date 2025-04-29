8c44dd9ebc83733d3abe30ef2cbc12556870bb07
mikemoritz <mike.moritz@vertex.link>
2025-04-29 10:49:37 -0700

    add media:hashtag for inet:service:message

--- synapse/models/media.py
+++ synapse/models/media.py
@@ -74,6 +74,8 @@ class MediaModule(s_module.CoreModule):
                 ('desc', ('str', {}), {
                     'doc': 'A brief description of the topic.'}),
             )),
+
+            ('media:hashtag', {}, ()),
         )
 
         types = (
@@ -87,6 +89,14 @@ class MediaModule(s_module.CoreModule):
 
             ('media:topic', ('str', {'lower': True, 'onespace': True}), {
                 'doc': 'A topic string.'}),
+
+            ('media:hashtag', ('str', {'lower': True, 'strip': True, 'regex': r'^#[^\p{Z}#]+$'}), {
+                # regex explanation:
+                # - starts with pound
+                # - one or more non-whitespace/non-pound character
+                # The minimum hashtag is a pound with a single non-whitespace character
+                'doc': 'A hashtag used in a media item.',
+            }),
         )
 
         modldef = (name, {
