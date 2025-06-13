0865ff1cb49adccd8a7bee3a1b1dfd37ed235f1f
OCBender <181250370+OCBender@users.noreply.github.com>
2025-06-13 11:27:55 -0400

    updates

--- synapse/tools/healthcheck.py
+++ synapse/tools/healthcheck.py
@@ -44,7 +44,7 @@ async def main(argv, outp=s_output.stdout):
 
             prox = await s_common.wait_for(s_telepath.openurl(url),
                                            timeout=opts.timeout)
-    except (FileNotFoundError, ConnectionError, socket.gaierror) as e:
+    except (s_exc.BadCellPath, socket.gaierror) as e:
         mesg = f'Unable to connect to cell @ {sanitized_url}.'
         ret = {'status': 'failed',
                'iden': opts.cell,
