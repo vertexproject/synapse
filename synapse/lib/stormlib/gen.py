6bf02083cd66451731da78e9dd4b40c7139e6b66
visi <visi@vertex.link>
2025-06-16 16:13:18 -0400

    WIP: Next model changes

--- synapse/lib/stormlib/gen.py
+++ synapse/lib/stormlib/gen.py
@@ -134,7 +134,7 @@ class LibGen(s_stormtypes.Lib):
          'desc': 'Returns a file:bytes node by SHA256, adding the node if it does not exist.',
          'type': {'type': 'function', '_funcname': '_storm_query',
                   'args': (
-                      {'name': 'sha256', 'type': ['str', 'hash:sha256'], 'desc': 'The SHA256 fingerprint for the file:bytes node.'},
+                      {'name': 'sha256', 'type': ['str', 'crypto:hash:sha256'], 'desc': 'The SHA256 fingerprint for the file:bytes node.'},
                       {'name': 'try', 'type': 'boolean', 'default': False,
                        'desc': 'Type normalization will fail silently instead of raising an exception.'},
                   ),
@@ -143,7 +143,7 @@ class LibGen(s_stormtypes.Lib):
          'desc': 'Returns a crypto:x509:cert node by SHA256, adding the node if it does not exist.',
          'type': {'type': 'function', '_funcname': '_storm_query',
                   'args': (
-                      {'name': 'sha256', 'type': ['str', 'hash:sha256'], 'desc': 'The SHA256 fingerprint for the certificate.'},
+                      {'name': 'sha256', 'type': ['str', 'crypto:hash:sha256'], 'desc': 'The SHA256 fingerprint for the certificate.'},
                       {'name': 'try', 'type': 'boolean', 'default': False,
                        'desc': 'Type normalization will fail silently instead of raising an exception.'},
                   ),
@@ -153,7 +153,7 @@ class LibGen(s_stormtypes.Lib):
          'type': {'type': 'function', '_funcname': '_storm_query',
                   'args': (
                       {'name': 'server', 'type': ['str', 'inet:server'], 'desc': 'The server associated with the x509 certificate.'},
-                      {'name': 'sha256', 'type': ['str', 'hash:sha256'], 'desc': 'The SHA256 fingerprint for the certificate.'},
+                      {'name': 'sha256', 'type': ['str', 'crypto:hash:sha256'], 'desc': 'The SHA256 fingerprint for the certificate.'},
                       {'name': 'try', 'type': 'boolean', 'default': False,
                        'desc': 'Type normalization will fail silently instead of raising an exception.'},
                   ),
--- synapse/models/auth.py
+++ synapse/models/auth.py
@@ -1,62 +1,11 @@
 modeldefs = (
     ('auth', {
-        'types': (
-            ('auth:creds', ('guid', {}), {
-                'doc': 'A unique set of credentials used to access a resource.',
-            }),
-            ('auth:access', ('guid', {}), {
-                'doc': 'An instance of using creds to access a resource.',
+        'types': (),
+        'interfaces': (
+            ('auth:credential', {
+                'doc': 'An interface inherited by authentication credential forms.',
             }),
         ),
-        'forms': (
-            ('auth:creds', {}, (
-
-                ('email', ('inet:email', {}), {
-                    'doc': 'The email address used to identify the user.'}),
-
-                ('user', ('inet:user', {}), {
-                    'doc': 'The user name used to identify the user.'}),
-
-                ('phone', ('tel:phone', {}), {
-                    'doc': 'The phone number used to identify the user.'}),
-
-                ('passwd', ('inet:passwd', {}), {
-                    'doc': 'The password used to authenticate.'}),
-
-                ('passwdhash', ('it:auth:passwdhash', {}), {
-                    'doc': 'The password hash used to authenticate.'}),
-
-                ('account', ('it:host:account', {}), {
-                    'doc': 'The account that the creds allow access to.'}),
-
-                ('website', ('inet:url', {}), {
-                    'doc': 'The base URL of the website that the credentials allow access to.'}),
-
-                ('host', ('it:host', {}), {
-                    'doc': 'The host that the credentials allow access to.'}),
-
-                ('wifi:ssid', ('inet:wifi:ssid', {}), {
-                    'doc': 'The WiFi SSID that the credentials allow access to.'}),
-
-                ('service:account', ('inet:service:account', {}), {
-                    'doc': 'The service account that the credentials allow access to.'}),
-                # TODO x509, rfid, mat:item locks/keys
-            )),
-
-            ('auth:access', {}, (
-                ('creds', ('auth:creds', {}), {
-                    'doc': 'The credentials used to attempt access.',
-                }),
-                ('time', ('time', {}), {
-                    'doc': 'The time of the access attempt.',
-                }),
-                ('success', ('bool', {}), {
-                    'doc': 'Set to true if the access was successful.',
-                }),
-                ('person', ('ps:person', {}), {
-                    'doc': 'The person who attempted access.',
-                }),
-            )),
-        ),
+        'forms': (),
     }),
 )
