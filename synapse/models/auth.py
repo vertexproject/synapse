7372d4e88e312b807926aaf0e9dc437ede54fa4f
cisphyx <cisphyx@vertex.link>
2025-07-17 10:55:20 -0400

    Update type norming to include norminfo for subs

--- synapse/models/auth.py
+++ synapse/models/auth.py
@@ -4,14 +4,20 @@ import synapse.lib.types as s_types
 
 class Passwd(s_types.Str):
 
+    def postTypeInit(self):
+        s_types.Str.postTypeInit(self)
+        self.md5 = self.modl.type('crypto:hash:md5')
+        self.sha1 = self.modl.type('crypto:hash:sha1')
+        self.sha256 = self.modl.type('crypto:hash:sha256')
+
     async def norm(self, valu, view=None):
         retn = await s_types.Str.norm(self, valu)
         retn[1].setdefault('subs', {})
         byts = retn[0].encode('utf8')
         retn[1]['subs'].update({
-            'md5': hashlib.md5(byts, usedforsecurity=False).hexdigest(),
-            'sha1': hashlib.sha1(byts, usedforsecurity=False).hexdigest(),
-            'sha256': hashlib.sha256(byts).hexdigest(),
+            'md5': (self.md5.typehash, hashlib.md5(byts, usedforsecurity=False).hexdigest(), {}),
+            'sha1': (self.sha1.typehash, hashlib.sha1(byts, usedforsecurity=False).hexdigest(), {}),
+            'sha256': (self.sha256.typehash, hashlib.sha256(byts).hexdigest(), {}),
         })
         return retn
 
