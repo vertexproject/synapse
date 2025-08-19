3be7f7c1506bca87a56e66f1795837d78c5efea8
OCBender <181250370+OCBender@users.noreply.github.com>
2025-08-19 09:06:50 -0400

    updates

--- synapse/daemon.py
+++ synapse/daemon.py
@@ -213,7 +213,7 @@ async def t2call(link, meth, args, kwargs):
         return
 
     except (asyncio.CancelledError, Exception) as e:
-        if isinstance(e, Exception) and not isinstance(e, asyncio.CancelledError):
+        if not isinstance(e, asyncio.CancelledError):
             logger.exception(f'error during task: {meth.__name__} {e}')
         if not link.isfini:
             retn = s_common.retnexc(e)
