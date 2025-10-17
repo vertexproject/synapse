b499f6cb07350d7d3563a7e498e606e0fd1f8f4a
Cisphyx <cisphyx@vertex.link>
2025-10-17 15:12:48 -0400

    Mark lib/node/path as reserved vars in Storm (SYN-9741) (#4539)

--- synapse/tests/test_lib_ast.py
+++ synapse/tests/test_lib_ast.py
@@ -1944,28 +1944,6 @@ class AstTest(s_test.SynTest):
             self.stormIsInPrint('arg1 is 445', msgs)
             self.stormIsInPrint('retn is 445', msgs)
 
-            # make sure we can't override the base lib object
-            q = '''
-            function wat(arg1) {
-                $lib.print($arg1)
-                $lib.print("We should have inherited the one true lib")
-                return ("Hi :)")
-            }
-            function override() {
-                $lib = "The new lib"
-                $retn = $wat($lib)
-                return ($retn)
-            }
-
-            $lib.print($override())
-            $lib.print("NO OVERRIDES FOR YOU")
-            '''
-            msgs = await core.stormlist(q)
-            self.stormIsInPrint('The new lib', msgs)
-            self.stormIsInPrint('We should have inherited the one true lib', msgs)
-            self.stormIsInPrint('Hi :)', msgs)
-            self.stormIsInPrint('NO OVERRIDES FOR YOU', msgs)
-
             # yields across an import boundary
             q = '''
             $test = $lib.import(yieldsforever)
@@ -2159,18 +2137,6 @@ class AstTest(s_test.SynTest):
             nodes = await core.nodes('[ inet:ip=1.2.3.4 +#visi ] | foocmd')
             self.eq(nodes[0].ndef, ('test:str', 'visi'))
             self.eq(nodes[1].ndef, ('inet:ip', (4, 0x01020304)))
-
-            msgs = await core.stormlist('''
-                function lolol() {
-                    $lib = "pure lulz"
-                    $lolol = "don't do this"
-                    return ($lolol)
-                }
-                $neato = 0
-                $myvar = $lolol()
-                $lib.print($myvar)
-            ''')
-            self.stormIsInPrint("don't do this", msgs)
 
     async def test_ast_setitem(self):
 
