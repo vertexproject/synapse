9163cfdc95e65e2be75acb280334d71850e6a5fd
blackout <blackout@vertex.link>
2025-11-12 15:29:41 -0500

    Update synapse/lib/stormtypes.py
    Co-authored-by: vEpiphyte <epiphyte@vertex.link>

--- synapse/lib/stormtypes.py
+++ synapse/lib/stormtypes.py
@@ -1883,7 +1883,7 @@ class LibDict(Lib):
                 mesg = '$lib.dict.fromlist() keys must be str or int types.'
                 raise s_exc.BadArg(mesg=mesg)
 
-        return s_msgpack.deepcopy(dict(valu), use_list=True)
+        return {k, s_msgpack.deepcopy(v, use_list=True) for (k, v) in valu}
 
     @stormfunc(readonly=True)
     async def _pop(self, valu, key, default=undef):
