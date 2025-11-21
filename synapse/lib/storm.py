145736bcb5da253e22176637695f46542ac5c763
blackout <blackout@vertex.link>
2025-11-21 14:33:33 -0500

    wip

--- synapse/lib/storm.py
+++ synapse/lib/storm.py
@@ -2412,7 +2412,8 @@ class Cmd:
     def getDescr(self):
         return self.__class__.__doc__
 
-    def getArgParser(self, cdef=None):
+    def getArgParser(self):
+        cdef = getattr(self, 'cdef', None)
         return Parser(prog=self.getName(), descr=self.getDescr(), model=self.runt.model, cdef=cdef)
 
     async def setArgv(self, argv):
@@ -2501,7 +2502,7 @@ class PureCmd(Cmd):
 
     def getArgParser(self):
 
-        pars = Cmd.getArgParser(self, cdef=self.cdef)
+        pars = Cmd.getArgParser(self)
         for name, opts in self.cdef.get('cmdargs', ()):
             pars.add_argument(name, **opts)
 
