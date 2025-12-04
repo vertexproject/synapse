d42284d869896306e98a40ebfbea0b509233f1b8
blackout <blackout@vertex.link>
2025-12-04 09:53:41 -0500

    wip

--- synapse/common.py
+++ synapse/common.py
@@ -7,7 +7,9 @@ import http
 import stat
 import time
 import heapq
+import queue
 import types
+import atexit
 import base64
 import shutil
 import struct
@@ -31,6 +33,7 @@ import collections
 
 import http.cookies
 import tornado.escape
+import logging.handlers
 
 import yaml
 import regex
@@ -826,13 +829,25 @@ def setlogging(mlogger, defval=None, structlog=None, log_setup=True, datefmt=Non
 
         log_level = normLogLevel(log_level)
 
+        # See https://docs.python.org/3/howto/logging-cookbook.html#blocking-handlers for info on this configuration
+        logq = queue.Queue()
+        handler = logging.handlers.QueueHandler(logq)
+        stream = logging.StreamHandler()
+
+        listener = logging.handlers.QueueListener(logq, stream)
+        listener.start()
+        atexit.register(listener.stop)
+
         if log_struct:
-            handler = logging.StreamHandler()
             formatter = s_structlog.JsonFormatter(datefmt=datefmt)
-            handler.setFormatter(formatter)
-            logging.basicConfig(level=log_level, handlers=(handler,))
         else:
-            logging.basicConfig(level=log_level, format=s_const.LOG_FORMAT, datefmt=datefmt)
+            formatter = logging.Formatter(fmt=s_const.LOG_FORMAT, datefmt=datefmt)
+
+        stream.setFormatter(formatter)
+
+        logging.root.setLevel(log_level)
+        logging.root.addHandler(handler)
+
         if log_setup:
             mlogger.info('log level set to %s', s_const.LOG_LEVEL_INVERSE_CHOICES.get(log_level))
 
