74c1d83e7980baa3c12f2ecffa9becd5f9e60ef6
epiphyte <epiphyte@vertex.link>
2026-02-17 10:34:45 -0500

    Changelog for deprecation.

--- synapse/lib/structlog.py
+++ synapse/lib/structlog.py
@@ -7,7 +7,7 @@ import synapse.lib.json as s_json
 class JsonFormatter(logging.Formatter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        s_common.deprecated('synapse.lib.structlog.JsonFormatter')
+        s_common.deprdate('synapse.lib.structlog.JsonFormatter', '2026-04-01')
 
     def format(self, record: logging.LogRecord):
 
