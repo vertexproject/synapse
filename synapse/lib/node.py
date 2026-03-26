3c947bd3c0eed5ed6e3ef9ac6982868f179d6f5b
cisphyx <cisphyx@vertex.link>
2026-03-26 17:13:03 -0400

    docs fix

--- synapse/lib/node.py
+++ synapse/lib/node.py
@@ -1705,6 +1705,8 @@ def reprProp(pode, prop):
         return None
     propvalu = pode[1].get('reprs', {}).get(prop)
     if propvalu is None:
+        if not opropvalu:
+            return str(opropvalu)
         if isinstance(opropvalu[0], str):
             return str(opropvalu[1])
         return tuple(str(v[1]) for v in opropvalu)
