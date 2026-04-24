3bd4c99bd9d789c2b24fd7d72ac45d171c72d22f
OCBender <181250370+OCBender@users.noreply.github.com>
2026-04-24 13:35:54 -0400

    feedback

--- synapse/lib/stormtypes.py
+++ synapse/lib/stormtypes.py
@@ -9435,22 +9435,22 @@ class LibCron(Lib):
             h, m = timepart.split(':')
             if h:
                 try:
-                    reqs['hour'] = int(h, 10)
+                    reqs['hour'] = int(h)
                 except ValueError:
                     mesg = f'Invalid hour value: {h}'
                     raise s_exc.BadTime(mesg=mesg)
             if m:
                 try:
                     if ',' in m:
-                        reqs['minute'] = [int(v, 10) for v in m.split(',')]
+                        reqs['minute'] = [int(v) for v in m.split(',')]
                     else:
-                        reqs['minute'] = int(m, 10)
+                        reqs['minute'] = int(m)
                 except ValueError:
                     mesg = f'Invalid minute value: {m}'
                     raise s_exc.BadTime(mesg=mesg)
         else:
             try:
-                reqs['hour'] = int(timepart, 10)
+                reqs['hour'] = int(timepart)
             except ValueError:
                 mesg = f'Invalid hour value: {timepart}'
                 raise s_exc.BadTime(mesg=mesg)
