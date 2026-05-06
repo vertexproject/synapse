06b6bc810521a2f3af617644d7dc01f96f387f21
OCBender <181250370+OCBender@users.noreply.github.com>
2026-05-06 14:00:52 -0400

    feedback

--- synapse/utils/stormcov/plugin.py
+++ synapse/utils/stormcov/plugin.py
@@ -61,7 +61,10 @@ class StormPlugin(coverage.CoveragePlugin, coverage.FileTracer):
                                    f'be reported on first instance in {pname} at line {pline + 1}')
                     continue
 
-                self.subq_map[subg] = (path, node.children[0].line - 1)
+                if rule == 'embedquery':
+                    self.subq_map[subg] = (path, node.children[0].line - 1)
+                else:
+                    self.subq_map[subg] = (path, subq.meta.line - 1)
 
     def file_tracer(self, filename):
         if filename.endswith('synapse/lib/ast.py'):
