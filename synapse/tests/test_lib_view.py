aed6235c7768db105dbaf0db9fd8bac41aa22b9f
OCBender <181250370+OCBender@users.noreply.github.com>
2026-08-19 16:45:07 -0400

    updates

--- synapse/tests/test_lib_view.py
+++ synapse/tests/test_lib_view.py
@@ -5,7 +5,6 @@ import synapse.exc as s_exc
 import synapse.common as s_common
 
 import synapse.lib.time as s_time
-import synapse.lib.layer as s_layer
 
 import synapse.tests.utils as s_t_utils
 from synapse.tests.utils import alist
